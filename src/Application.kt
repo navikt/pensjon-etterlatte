511b1e15cf2aed670099f9f61aa1ab45ea986d28
Lars Erik Dahl <lars.erik.dahl@nav.no>
2021-02-10 12:43:57 +0100

    Changed loglevel to INFO

--- src/Application.kt
+++ src/Application.kt
@@ -10,6 +10,7 @@ import io.ktor.jackson.*
 import io.ktor.features.*
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
+import org.slf4j.event.Level
 import java.net.InetAddress
 import java.time.Duration
 import java.util.*
@@ -24,6 +25,9 @@ fun Application.module(testing: Boolean = false) {
             enable(SerializationFeature.INDENT_OUTPUT)
         }
     }
+    install(CallLogging) {
+        level = Level.INFO
+    }
     val env = System.getenv()
     val kafkaConfig = KafkaConfig(
         bootstrapServers = "b27apvl00045.preprod.local:8443,b27apvl00046.preprod.local:8443,b27apvl00047.preprod.local:8443",
@@ -33,7 +37,9 @@ fun Application.module(testing: Boolean = false) {
         password = env.get("srvpwd"),
         //truststore = env["NAV_TRUSTSTORE_PATH"],
         //truststorePassword = env["NAV_TRUSTSTORE_PASSWORD"],
-        autoCommit = env["KAFKA_AUTO_COMMIT"]?.let { "true" == it.toLowerCase() }
+        autoCommit = env["KAFKA_AUTO_COMMIT"]?.let { "true" == it.toLowerCase()},
+        maxRecords = 1
+
     )
 
 
@@ -45,7 +51,7 @@ fun Application.module(testing: Boolean = false) {
         get("/kafka") {
             val consumer = KafkaConsumer(kafkaConfig.consumerConfig(), ByteArrayDeserializer(), ByteArrayDeserializer())
             consumer.subscribe(listOf("aapen-person-pdl-leesah-v1"))
-            val antallMeldingerLest = consumer.poll(Duration.ofSeconds(60)).count()
+            val antallMeldingerLest = consumer.poll(Duration.ofSeconds(5)).count()
             consumer.close()
             call.respondText("har lest $antallMeldingerLest meldinger", contentType = ContentType.Text.Plain)
         }
