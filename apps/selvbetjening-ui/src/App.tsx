d34209cdbfc270fd8e590a46ea2bee9a3bd2f120
NN <nicolas.nordhagen@nav.no>
2021-03-10 09:04:17 +0100

    fikse frontend import-feil

--- apps/selvbetjening-ui/src/App.tsx
+++ apps/selvbetjening-ui/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router";
+import { Route } from "react-router";
 import "./App.less";
 import SoknadDialog from "./soknad/SoknadDialog";
 
