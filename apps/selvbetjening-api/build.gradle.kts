b8f601c7f9b8b05e034c98d4071c93796850e5ad
Lars Erik Dahl <lars.erik.dahl@nav.no>
2021-03-15 09:06:32 +0100

    Added ny app selvbetjening-api

---
+++ apps/selvbetjening-api/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    application
+    kotlin("jvm")
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:1.4.3")
+    implementation("io.ktor:ktor-server-netty:1.4.3")
+    implementation("io.ktor:ktor-metrics-micrometer:1.4.3")
+    implementation("io.ktor:ktor-jackson:1.4.3")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.6.1")
+    implementation("com.fasterxml:jackson-xml-databind:0.6.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.0")
+    implementation("ch.qos.logback:logback-classic:1.2.1")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    testImplementation("io.ktor:ktor-server-tests:1.4.3")
+    testImplementation("no.nav:kafka-embedded-env:2.7.0")
+
+}
+
+tasks.named<Jar>("jar") {
+    archiveBaseName.set("app")
+
+    manifest {
+        attributes["Main-Class"] = "no.nav.etterlatte.ApplicationKt"
+        attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+            it.name
+        }
+    }
+
+    doLast {
+        configurations.runtimeClasspath.get().forEach {
+            val file = File("$buildDir/libs/${it.name}")
+            if (!file.exists())
+                it.copyTo(file)
+        }
+    }
+
+}
---
+++ apps/selvbetjening-api/src/main/kotlin/Application.kt
@@ -0,0 +1,54 @@
+package no.nav.etterlatte
+
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.route
+import io.ktor.routing.routing
+import io.ktor.server.engine.applicationEngineEnvironment
+import io.ktor.server.engine.connector
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+
+fun main() {
+    embeddedServer(Netty, environment = applicationEngineEnvironment {
+        module {
+            install(ContentNegotiation) {
+                jackson()
+            }
+
+            routing {
+                route("internal") {
+                    get("isalive") {
+                        call.respondText("JADDA!", contentType = ContentType.Text.Plain)
+                    }
+                    get("isready") {
+                        call.respondText("JADDA!", contentType = ContentType.Text.Plain)
+                    }
+                }
+                route("api") {
+                    get {
+                        call.respond(HttpStatusCode.OK, Pair("navn", "Ola Nordmann"))
+                    }
+                }
+            }
+        }
+
+        connector {
+            port = 8080
+        }
+    }
+    ).apply {
+        Runtime.getRuntime().addShutdownHook(Thread {
+            stop(3000, 3000)
+        })
+    }.start(true)
+
+}
