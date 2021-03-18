3df4c9854dc2b289703100b40dff59c7d910d581
NN <nicolas.nordhagen@nav.no>
2021-03-18 15:44:46 +0100

    Teste innlogging frontend

--- apps/selvbetjening-ui/server/decorator.js
+++ apps/selvbetjening-ui/server/decorator.js
@@ -1,19 +1,20 @@
 const { injectDecoratorServerSide } = require("@navikt/nav-dekoratoren-moduler/ssr");
 
-const getDecorator = (filePath) =>
-    injectDecoratorServerSide({
-        env: "dev",
-        context: "privatperson",
-        filePath: filePath,
-        // enforceLogin: true,
-        // level: "Level4",
-        breadcrumbs: [
-            {
-                url: "https://www.nav.no/person/familie/soknad",
-                title: "Søknad om etterlatteytelser",
-                handleInApp: true,
-            },
-        ],
-    });
+const props = {
+    env: "dev",
+    context: "privatperson",
+    enforceLogin: true,
+    redirectToApp: true,
+    level: "Level4",
+    breadcrumbs: [
+        {
+            url: "https://www.nav.no/person/familie/soknad",
+            title: "Søknad om etterlatteytelser",
+            handleInApp: true,
+        },
+    ],
+};
+
+const getDecorator = (filePath) => injectDecoratorServerSide({ ...props, filePath });
 
 module.exports = getDecorator;
