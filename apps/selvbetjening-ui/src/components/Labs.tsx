5e29e5e968f85800f12928da55909605e0726a6d
NN <nicolas.nordhagen@nav.no>
2021-03-19 10:43:04 +0100

    Oppdatere labs (frontend)

--- apps/selvbetjening-ui/src/components/Labs.tsx
+++ apps/selvbetjening-ui/src/components/Labs.tsx
@@ -36,13 +36,23 @@ const hentPerson = () => {
         });
 };
 
+const secure = () => {
+    api.get("/secure")
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
+
 const Labs: FC = () => {
     return (
         <>
             <h1>Selvbetjening API</h1>
             <button onClick={isReady}>isready</button>
             <button onClick={isAlive}>isalive</button>
-            <button onClick={hentPerson}>hentPerson</button>
+            <button onClick={secure}>secure</button>
         </>
     );
 };
