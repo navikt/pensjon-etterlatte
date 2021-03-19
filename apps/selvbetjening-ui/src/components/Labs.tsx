c6928c1cb33326cf95d2a4124676fc07c924f710
NN <nicolas.nordhagen@nav.no>
2021-03-19 10:46:34 +0100

    Fikse ubrukt const i front

--- apps/selvbetjening-ui/src/components/Labs.tsx
+++ apps/selvbetjening-ui/src/components/Labs.tsx
@@ -52,6 +52,7 @@ const Labs: FC = () => {
             <h1>Selvbetjening API</h1>
             <button onClick={isReady}>isready</button>
             <button onClick={isAlive}>isalive</button>
+            <button onClick={hentPerson}>hentPerson</button>
             <button onClick={secure}>secure</button>
         </>
     );
