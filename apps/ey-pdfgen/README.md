26443f9c5f14ca61d9d73194287f84b4c2ceeb33
Janbu <helge.janbu@nav.no>
2021-03-24 11:47:53 +0100

    Lagt til ny app ey-pdfgen

---
+++ apps/ey-pdfgen/README.md
@@ -0,0 +1,7 @@
+# su-pdfgen
+PDF generator for supplerende stønad. Kjør lokalt docker image med  `./run_development.sh`
+
+PDFene kan testes lokalt på `http://localhost:8081/api/v1/genpdf/<application>/<template>`, f.eks.
+http://localhost:8081/api/v1/genpdf/supdfgen/vedtakInnvilgelse
+
+Templatene vil bruke flettedata fra json-fil med samme navn som template i `data/supdfgen`
---
+++ apps/ey-pdfgen/run_development.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+
+docker pull navikt/pdfgen
+docker run \
+        -v $CURRENT_PATH/templates:/app/templates \
+        -v $CURRENT_PATH/fonts:/app/fonts \
+        -v $CURRENT_PATH/data:/app/data \
+        -v $CURRENT_PATH/resources:/app/resources \
+        -p 8081:8080 \
+        -e DISABLE_PDF_GET=false \
+        -it \
+        --rm \
+        navikt/pdfgen
