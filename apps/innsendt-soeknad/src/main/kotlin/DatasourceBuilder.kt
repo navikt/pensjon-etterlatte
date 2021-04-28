db5f117265b383a4351f960b20814cff88204f9e
Lars Erik Dahl <lars.erik.dahl@nav.no>
2021-04-28 11:03:25 +0200

    Database-URL from nais uses "postgres" to identify postressql, but postgressql is expected in jdbc url

--- apps/innsendt-soeknad/src/main/kotlin/DatasourceBuilder.kt
+++ apps/innsendt-soeknad/src/main/kotlin/DatasourceBuilder.kt
@@ -7,12 +7,28 @@ import javax.sql.DataSource
 
 class DataSourceBuilder(env: Map<String, String>) {
     private val hikariConfig = HikariConfig().apply {
-        jdbcUrl = "jdbc:${env["DB_URL"]}"
+        jdbcUrl = env["DB_JDBC_URL"] ?: String.format(
+            "jdbc:postgresql://%s:%s/%s%s",
+            requireNotNull(env["DB_HOST"]) { "database host must be set if jdbc url is not provided" },
+            requireNotNull(env["DB_PORT"]) { "database port must be set if jdbc url is not provided" },
+            requireNotNull(env["DB_DATABASE"]) { "database name must be set if jdbc url is not provided" },
+            env["DB_USERNAME"]?.let { "?user=$it" } ?: "")
+
+        env["DB_USERNAME"]?.let { this.username = it }
+        env["DB_PASSWORD"]?.let { this.password = it }
+
         maximumPoolSize = 3
         minimumIdle = 1
         idleTimeout = 10001
         connectionTimeout = 1000
         maxLifetime = 30001
+    }
+
+    init {
+        if (!env.containsKey("DB_JDBC_URL")) {
+            checkNotNull(env["DB_USERNAME"]) { "username must be set when vault is disabled" }
+            checkNotNull(env["DB_PASSWORD"]) { "password must be set when vault is disabled" }
+        }
     }
 
     fun getDataSource() = HikariDataSource(hikariConfig)
