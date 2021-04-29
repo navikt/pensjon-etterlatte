fe9f2abd7e3d3c34d13e990f1968d617566f7776
NN <nicolas.nordhagen@nav.no>
2021-04-29 09:25:01 +0200

    API: Oppdatere uthenting av fnr fra context

--- apps/selvbetjening-api/src/main/kotlin/person/PersonRoute.kt
+++ apps/selvbetjening-api/src/main/kotlin/person/PersonRoute.kt
@@ -1,12 +1,13 @@
 package no.nav.etterlatte.person
 
 import io.ktor.application.call
+import io.ktor.auth.principal
 import io.ktor.response.respondText
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.route
-import no.nav.etterlatte.ThreadBoundSecCtx
 import no.nav.etterlatte.common.toJson
+import no.nav.security.token.support.ktor.TokenValidationContextPrincipal
 
 /**
  * Endepunkter for uthenting av person
@@ -14,7 +15,10 @@ import no.nav.etterlatte.common.toJson
 fun Route.personApi(client: PersonClient) {
     route("person") {
         get("innlogget") {
-            val fnr = ThreadBoundSecCtx.get().user()!!
+            val fnr = call.principal<TokenValidationContextPrincipal>()?.context!!
+                .getClaims("tokenx")
+                .getStringClaim("pid")
+
             call.application.environment.log.info("Henter innlogget person med fnr: $fnr")
 
             val person = client.hentPerson(fnr)
