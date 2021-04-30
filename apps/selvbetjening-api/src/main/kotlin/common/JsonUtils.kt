1a0a4ee2a6c9e034d6d653a9e3adb0ffb40245af
NN <nicolas.nordhagen@nav.no>
2021-04-30 11:10:54 +0200

    API: Forsøke ny måte å deserialisere person på

--- apps/selvbetjening-api/src/main/kotlin/common/JsonUtils.kt
+++ apps/selvbetjening-api/src/main/kotlin/common/JsonUtils.kt
@@ -1,5 +1,16 @@
 package no.nav.etterlatte.common
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 fun Any.toJson(): String = jacksonObjectMapper().writeValueAsString(this)
+
+inline fun <reified T : Any> typeRefs(): TypeReference<T> = object : TypeReference<T>() {}
+
+inline fun <reified T : Any> mapJsonToAny(json: String, type: TypeReference<T>, failonunknown: Boolean = false): T {
+    return jacksonObjectMapper()
+        .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, failonunknown)
+        .readValue(json, type)
+}
--- apps/selvbetjening-api/src/main/kotlin/person/PersonService.kt
+++ apps/selvbetjening-api/src/main/kotlin/person/PersonService.kt
@@ -2,16 +2,22 @@ package no.nav.etterlatte.person
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.client.HttpClient
 import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.content.TextContent
 import io.ktor.features.NotFoundException
 import io.ktor.http.ContentType
+import no.nav.etterlatte.common.mapJsonToAny
 import no.nav.etterlatte.common.toJson
+import no.nav.etterlatte.common.typeRefs
 import no.nav.etterlatte.person.model.GraphqlRequest
 import no.nav.etterlatte.person.model.PersonResponse
 import no.nav.etterlatte.person.model.Variables
+import org.codehaus.jackson.JsonNode
 import org.slf4j.LoggerFactory
 
 interface PersonKlient {
@@ -29,15 +35,21 @@ class PersonService(
     }
 
     override suspend fun hentPerson(fnr: String): Person {
-        val query = javaClass.getResource("/pdl/hentPerson.graphql").readText().replace(Regex("[\n\t]"), "")
+        val query = javaClass.getResource("/pdl/hentPerson.graphql")
+            .readText()
+            .replace(Regex("[\n\t]"), "")
 
         val request = GraphqlRequest(query, Variables(ident = fnr)).toJson()
 
-        val response = httpClient.post<PersonResponse>(uri) {
+        val responseNode = httpClient.post<ObjectNode>(uri) {
             header("Tema", TEMA)
             header("Accept", "application/json")
             body = TextContent(request, ContentType.Application.Json)
         }
+
+        logger.info(responseNode.toPrettyString())
+
+        val response = mapJsonToAny(responseNode.toJson(), typeRefs<PersonResponse>())
 
         val hentPerson = response.data?.hentPerson
         if (hentPerson === null) {
