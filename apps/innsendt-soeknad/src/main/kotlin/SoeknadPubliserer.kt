31e549a50357dc28fa38e37454fb31c6b057c78e
Lars Erik Dahl <lars.erik.dahl@nav.no>
2021-05-18 13:53:07 +0200

    Innsendt-soeknad skal ikke legge på journalpost-info

--- apps/innsendt-soeknad/src/main/kotlin/SoeknadPubliserer.kt
+++ apps/innsendt-soeknad/src/main/kotlin/SoeknadPubliserer.kt
@@ -3,33 +3,16 @@ package no.nav.etterlatte
 import com.fasterxml.jackson.databind.node.ObjectNode
 import no.nav.helse.rapids_rivers.MessageContext
 
-class SoeknadPubliserer(val rapid: MessageContext, val db: SoeknadRepository) {
+class SoeknadPubliserer(private val rapid: MessageContext, private val db: SoeknadRepository) {
     fun publiser(soeknad: LagretSoeknad){
         mapper.createObjectNode().apply {
             put("@event_name", "soeknad_innsendt")
             set<ObjectNode>("@skjema_info", mapper.readTree(soeknad.soeknad))
-            set<ObjectNode>("@journalpostInfo", mapper.valueToTree(JournalPostInfo("tittel", AvsenderMottaker(soeknad.fnr, "navn", "FNR"), Bruker(soeknad.fnr, "FNR"))))
             put("@lagret_soeknad_id", soeknad.id)
             put("@template", "soeknad")
+            put("@fnr_soeker", soeknad.fnr)
             rapid.publish(soeknad.id.toString(), toJson())
         }
         db.soeknadSendt(soeknad)
     }
 }
-
-data class JournalPostInfo(
-    val tittel: String,
-    val avsenderMottaker: AvsenderMottaker,
-    val bruker: Bruker
-)
-
-data class AvsenderMottaker(
-    val id: String,
-    val navn:String,
-    val idType: String
-)
-
-data class Bruker(
-    val id: String,
-    val idType: String
-)
