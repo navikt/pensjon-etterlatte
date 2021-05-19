aa7ca8086397accccc501aa7426219b6c51a1689
jorgesti <jorgen.stikbakke@nav.no>
2021-05-19 15:54:15 +0200

    implementerte JsonPath i finnFnrSoeknad

--- apps/finn-fnr-soeknad/src/main/kotlin/FinnFnrSoeknad.kt
+++ apps/finn-fnr-soeknad/src/main/kotlin/FinnFnrSoeknad.kt
@@ -1,6 +1,8 @@
 package no.nav.etterlatte
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.nfeld.jsonpathkt.JsonPath
+import com.nfeld.jsonpathkt.extension.read
 import kotlinx.coroutines.runBlocking
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
@@ -25,14 +27,32 @@ internal class FinnFnrSoeknad(rapidsConnection: RapidsConnection) :
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         //println(packet["@skjema_info"].asText())
 
+
         runBlocking {
 
-            packet["@fnr_liste"] = finnFnrForSkjema(packet["@skjema_info"])
+            packet["@fnr_liste"] = finnFnrForSkjema(packet["@skjema_info"]).toString()
             context.publish(packet.toJson())
             }
     }
-    private fun finnFnrForSkjema(skjemainfo: JsonNode ): JsonNode {
-        return skjemainfo
+    private fun finnFnrForSkjema(skjemainfo: JsonNode ): String? {
+        val jsonpath = JsonPath(skjemainfo.toString())
+        jsonpath.readFromJson<JsonNode>(skjemainfo)
+        return JsonPath.parse(skjemainfo.toString())?.read("$..foedselsnummer")
+        /*val objectMapper = jacksonObjectMapper()
+            .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+
+        val gah:Map<String,Object?>? = objectMapper.treeToValue(skjemainfo)
+        gah?.let {
+            return gah.filter { it.key == "foedselsnummer" }.values.toString()
+
+
+        }
+
+        return "n"
+
+         */
     }
 
 }
