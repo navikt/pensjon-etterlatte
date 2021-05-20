86c28b066dfa0e0c55160eb8825640037c31f7c6
jorgesti <jorgen.stikbakke@nav.no>
2021-05-20 13:15:01 +0200

    implementert sjekk av modulo11

--- apps/finn-fnr-soeknad/src/main/kotlin/FinnFnrSoeknad.kt
+++ apps/finn-fnr-soeknad/src/main/kotlin/FinnFnrSoeknad.kt
@@ -39,14 +39,38 @@ internal class FinnFnrSoeknad(rapidsConnection: RapidsConnection) :
         val regex = """\b(\d{11})\b""".toRegex()
 
         return regex.findAll(skjemainfo.toString())
-            .filter { validerFnr(it.toString()) }
+            .filter { validateControlDigits(it.toString()) }
             .map { it.groupValues[1] }
             .joinToString()
     }
 
-    private fun validerFnr(fnr: String): Boolean {
-        //TODO()
+    //private fun validerFnr(fnr: String): Boolean {
+     //   //TODO()
+     //   return true
+    //}
+    private fun validateControlDigits(value:String): Boolean {
+        val controlDigits1 = intArrayOf(3, 7, 6, 1, 8, 9, 4, 5, 2)
+        val controlDigits2 = intArrayOf(5, 4, 3, 2, 7, 6, 5, 4, 3, 2)
+        val ks1 = Character.getNumericValue(value[9])
+
+        val c1 = mod(controlDigits1, value)
+        if (c1 == 10 || c1 != ks1) {
+            return false
+        }
+
+        val c2 = mod(controlDigits2, value)
+        if (c2 == 10 || c2 != Character.getNumericValue(value[10])) {
+            return false
+        }
+
         return true
+    }
+    private fun mod(arr: IntArray, value:String): Int {
+        val sum = arr.withIndex()
+            .sumBy { (i, m) -> m * Character.getNumericValue(value[i]) }
+
+        val result = 11 - (sum % 11)
+        return if (result == 11) 0 else result
     }
 
 }
