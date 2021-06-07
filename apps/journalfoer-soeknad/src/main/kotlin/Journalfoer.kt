630a55ce6dae16c3f8119a6f3a42e5f7a27721a2
jorgesti <jorgen.stikbakke@nav.no>
2021-06-07 12:23:26 +0200

    endret måten jeg sender inn forsoekFerdigstill

--- apps/journalfoer-soeknad/src/main/kotlin/Journalfoer.kt
+++ apps/journalfoer-soeknad/src/main/kotlin/Journalfoer.kt
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.module.kotlin.treeToValue
 import io.ktor.client.HttpClient
 import io.ktor.client.request.accept
 import io.ktor.client.request.header
+import io.ktor.client.request.parameter
 import io.ktor.client.request.post
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
@@ -71,7 +72,7 @@ class Journalfoer(private val client: HttpClient, private val baseUrl: String) :
             )
         }
         return client.post(baseUrl) {
-            listOf("forsoekFerdigstill" to "true")
+            parameter("forsoekFerdigstill", true)
             accept(ContentType.Application.Json)
             contentType(ContentType.Application.Json)
             header("X-Correlation-ID", MDC.get("X-Correlation-ID") ?: UUID.randomUUID().toString())
