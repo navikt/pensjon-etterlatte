e09d5c54357a1be8cd65d4f0dbacd899910b3f55
NN <nicolas.nordhagen@nav.no>
2021-07-06 14:57:23 +0200

    Fikse UU-problematikk på radioknapper

--- apps/selvbetjening-ui/src/components/felles/RHFRadio.tsx
+++ apps/selvbetjening-ui/src/components/felles/RHFRadio.tsx
@@ -1,30 +1,43 @@
 import React, { ReactNode } from "react";
 import { RadioPanelGruppe, RadioPanelProps } from "nav-frontend-skjema";
 import { IValg } from "../../typer/Spoersmaal";
-import { v4 as uuid } from "uuid";
 import { Controller, FieldError, useFormContext } from "react-hook-form";
 import { FieldPath, FieldValues } from "react-hook-form/dist/types";
 import { get } from "lodash";
-import classnames from "classnames";
 import { useTranslation } from "react-i18next";
 import { RegisterOptions } from "react-hook-form/dist/types/validator";
 
-export const RHFToValgRadio = ({ name, legend, overrideRadios }: {
+/* TODO: Rename to RHFSpoersmaalRadio */
+export const RHFToValgRadio = ({ name, legend, vetIkke }: {
     name: FieldPath<FieldValues>;
     legend?: ReactNode;
-    overrideRadios?: RadioPanelProps[];
+    vetIkke?: boolean;
+}) => {
+    const defaultRadios = [
+        { label: IValg.JA, value: IValg.JA },
+        { label: IValg.NEI, value: IValg.NEI },
+    ];
+
+    if (vetIkke) defaultRadios.push({ label: IValg.VET_IKKE, value: IValg.VET_IKKE })
+
+    return (
+        <RHFInlineRadio
+            name={name}
+            legend={legend}
+            radios={defaultRadios}
+        />
+    )
+}
+
+export const RHFInlineRadio = ({ name, legend, radios }: {
+    name: FieldPath<FieldValues>;
+    legend?: ReactNode;
+    radios: RadioPanelProps[];
 }) => {
     const { t } = useTranslation();
     const { control, formState: { errors } } = useFormContext();
 
     const error: FieldError = get(errors, name)
-
-    let radios: RadioPanelProps[] = overrideRadios || [
-        { label: IValg.JA, value: IValg.JA },
-        { label: IValg.NEI, value: IValg.NEI },
-    ];
-
-    const classNames = classnames(radios.length === 2 && "to-valg-radio")
 
     return (
         <div id={name}>
@@ -32,12 +45,12 @@ export const RHFToValgRadio = ({ name, legend, overrideRadios }: {
                 name={name}
                 control={control}
                 rules={{required: true}}
-                render={({ field: { value, onChange } }) => (
+                render={({ field: { value, onChange, name: TName } }) => (
                     <RadioPanelGruppe
-                        name={uuid()}
+                        name={name}
                         feil={error && t(`feil.${error.ref?.name}.${error.type}`)}
                         legend={legend}
-                        className={classNames}
+                        className={"inline"}
                         radios={radios}
                         checked={value}
                         onChange={(e) => onChange((e.target as HTMLInputElement).value as IValg)}
@@ -65,9 +78,9 @@ export const RHFRadio = ({ name, legend, radios, rules }: {
                 name={name}
                 control={control}
                 rules={{required: true, ...rules}}
-                render={({ field: { value, onChange } }) => (
+                render={({ field: { value, onChange, name } }) => (
                     <RadioPanelGruppe
-                        name={uuid()}
+                        name={name}
                         feil={error && t(`feil.${error.ref?.name}.${error.type}`)}
                         legend={legend}
                         radios={radios}
