04e3a36b849f6d6e160d6599735ae196f6695256
Lars Erik Dahl <lars.erik.dahl@nav.no>
2021-07-08 11:33:10 +0200

    notifikasjoner: System.getenv() er ikke mutable

--- apps/etterlatte-notifikasjoner/src/main/kotlin/Application.kt
+++ apps/etterlatte-notifikasjoner/src/main/kotlin/Application.kt
@@ -3,7 +3,7 @@ package no.nav.etterlatte
 import no.nav.helse.rapids_rivers.RapidApplication
 
 fun main() {
-    val env = System.getenv().apply {
+    val env = System.getenv().toMutableMap().apply {
         put("KAFKA_CONSUMER_GROUP_ID", get("NAIS_APP_NAME")!!.replace("-", ""))
     }
     RapidApplication.create(env).also {
