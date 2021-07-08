cadfc183c0460f7c25f03ac93fee934ce53166d2
jorgesti <jorgen.stikbakke@nav.no>
2021-07-08 14:17:04 +0200

    kafka consumer basert på appens navn + reset policy

--- apps/sjekk-adressebeskyttelse/src/main/kotlin/Application.kt
+++ apps/sjekk-adressebeskyttelse/src/main/kotlin/Application.kt
@@ -3,7 +3,9 @@ package no.nav.etterlatte
 import no.nav.helse.rapids_rivers.RapidApplication
 
 fun main() {
-    val env = System.getenv()
+    val env = System.getenv().toMutableMap().apply {
+        put("KAFKA_CONSUMER_GROUP_ID", get("NAIS_APP_NAME")!!.replace("-", ""))
+    }
     RapidApplication.create(env).also {
         SjekkAdressebeskyttelse(it, AppBuilder(env).pdlService())
     }.start()
