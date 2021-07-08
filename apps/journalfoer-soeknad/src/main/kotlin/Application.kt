13690d4ad3b2d96b047db442825a9fa24b3d9b3b
jorgesti <jorgen.stikbakke@nav.no>
2021-07-08 13:53:06 +0200

    kafka consumer basert på appens navn

--- apps/journalfoer-soeknad/src/main/kotlin/Application.kt
+++ apps/journalfoer-soeknad/src/main/kotlin/Application.kt
@@ -3,7 +3,9 @@ package no.nav.etterlatte
 import no.nav.helse.rapids_rivers.RapidApplication
 
 fun main() {
-    System.getenv().also { env ->
+    System.getenv().toMutableMap().apply {
+        put("KAFKA_CONSUMER_GROUP_ID", get("NAIS_APP_NAME")!!.replace("-", ""))
+    }.also { env ->
         AppBuilder(env).also { appBuilder ->
             RapidApplication.create(env).apply {
                 JournalfoerSoeknad(this, appBuilder.genererPdf(), appBuilder.journalfoerDok())
