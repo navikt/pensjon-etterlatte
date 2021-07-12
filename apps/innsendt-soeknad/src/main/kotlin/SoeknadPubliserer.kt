e21397825dfbc8a5b4810f0ea760ccce2c9472bf
Lars Erik Dahl <lars.erik.dahl@nav.no>
2021-07-12 10:35:47 +0200

    innsendt-soeknad: meldinger publiseres nå som en JsonMessage

--- apps/innsendt-soeknad/src/main/kotlin/SoeknadPubliserer.kt
+++ apps/innsendt-soeknad/src/main/kotlin/SoeknadPubliserer.kt
@@ -1,19 +1,20 @@
 package no.nav.etterlatte
 
-import com.fasterxml.jackson.databind.node.ObjectNode
+import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import java.time.Clock
 import java.time.OffsetDateTime
 
 class SoeknadPubliserer(private val rapid: MessageContext, private val db: SoeknadRepository, private val klokke: Clock = Clock.systemUTC()) {
     fun publiser(soeknad: LagretSoeknad){
-        mapper.createObjectNode().apply {
-            put("@event_name", "soeknad_innsendt")
-            set<ObjectNode>("@skjema_info", mapper.readTree(soeknad.soeknad))
-            put("@lagret_soeknad_id", soeknad.id)
-            put("@template", "soeknad")
-            put("@fnr_soeker", soeknad.fnr)
-            put("@hendelse_gyldig_til", OffsetDateTime.now(klokke).plusMinutes(30L).toString())
+        JsonMessage.newMessage(mapOf(
+            "@event_name" to "soeknad_innsendt",
+            "@skjema_info" to mapper.readTree(soeknad.soeknad),
+            "@lagret_soeknad_id" to soeknad.id,
+            "@template" to "soeknad",
+            "@fnr_soeker" to soeknad.fnr,
+            "@hendelse_gyldig_til" to OffsetDateTime.now(klokke).plusMinutes(30L).toString()
+        )).apply {
             rapid.publish(soeknad.id.toString(), toJson())
         }
         db.soeknadSendt(soeknad)
