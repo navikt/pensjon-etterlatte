1ea1c4636229b252a8f3dab6dc3c887a7527aa4f
Espen Schei <espen.schei@nav.no>
2021-08-11 20:51:32 +0200

    feat: Legg til støtte for retry med eksponensiell backoff.
    Retry er kun lagt til for sendSoeknad-kallet p.t.
    Må ta stilling til om vi kan få problemer med idempotens/timeout.
    
    EY-35

--- apps/selvbetjening-ui/src/api.ts
+++ apps/selvbetjening-ui/src/api.ts
@@ -1,8 +1,17 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import axiosRetry from 'axios-retry';
 
 const api = axios.create({
     withCredentials: true,
     baseURL: "/api",
+});
+
+axiosRetry(api, {
+    retries: 3,
+    retryDelay: (retryCount: number) => retryCount * 2000,
+    // Foreløpig begrenset til kun å gjelde sendSoeknad.
+    // Må ta stilling til om vi kan få problemer med idempotens/timeout.
+    retryCondition: (error: AxiosError) => error.config.url === "/api/soeknad"
 });
 
 export const hentInnloggetPerson = () => {
@@ -15,8 +24,7 @@ export const hentInnloggetPerson = () => {
 };
 
 export const sendSoeknad = (soeknad: object) => {
-    return api
-        .post("/api/soeknad", soeknad)
+    return api.post("/api/soeknad", soeknad)
         .then((response) => {
             if (response.status !== 200) {
                 throw new Error()
