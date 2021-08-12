17b3812d398847c06022021888725bf0e220ab62
NN <nicolas.nordhagen@nav.no>
2021-08-12 15:30:21 +0200

    EY-86 Betinge felt for annen utdanning

--- apps/selvbetjening-ui/src/components/soknad/4-din-situasjon/fragmenter/HoeyesteUtdanning.tsx
+++ apps/selvbetjening-ui/src/components/soknad/4-din-situasjon/fragmenter/HoeyesteUtdanning.tsx
@@ -2,11 +2,16 @@ import { RHFRadio } from "../../../felles/RHFRadio";
 import { useTranslation } from "react-i18next";
 import { RadioProps, SkjemaGruppe } from "nav-frontend-skjema";
 import React from "react";
-import { Utdanning } from "../../../../typer/situasjon";
+import { ISituasjon, Utdanning } from "../../../../typer/situasjon";
 import { RHFInput } from "../../../felles/RHFInput";
+import { useFormContext } from "react-hook-form";
 
 const HoeyesteUtdanning = () => {
     const { t } = useTranslation();
+
+    const { watch } = useFormContext<ISituasjon>()
+
+    const hoyesteFullfoerteUtdanning = watch("utdanning.hoyesteFullfoerteUtdanning")
 
     return (
         <>
@@ -18,13 +23,15 @@ const HoeyesteUtdanning = () => {
                 })}
             />
 
-            <SkjemaGruppe>
-                <RHFInput
-                    name={"utdanning.annenUtdanning"}
-                    label={t("dinSituasjon.utdanning.annenUtdanning")}
-                    placeholder={t("dinSituasjon.utdanning.annenUtdanningPlaceholder")}
-                />
-            </SkjemaGruppe>
+            {hoyesteFullfoerteUtdanning === Utdanning.annen && (
+                <SkjemaGruppe>
+                    <RHFInput
+                        name={"utdanning.annenUtdanning"}
+                        label={t("dinSituasjon.utdanning.annenUtdanning")}
+                        placeholder={t("dinSituasjon.utdanning.annenUtdanningPlaceholder")}
+                    />
+                </SkjemaGruppe>
+            )}
         </>
     )
 }
