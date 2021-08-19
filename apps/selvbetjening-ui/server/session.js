562b18f367addeb1b07acbfea9736b09c2fc0324
NN <nicolas.nordhagen@nav.no>
2021-08-19 10:22:40 +0200

    EY-78 Opprydding i session.js

--- apps/selvbetjening-ui/server/session.js
+++ apps/selvbetjening-ui/server/session.js
@@ -3,25 +3,26 @@ const redis = require("redis");
 const config = require("./config");
 const RedisStore = require("connect-redis");
 
+const options = {
+    cookie: {
+        maxAge: config.session.maxAgeMs,
+        sameSite: "lax",
+        httpOnly: true,
+    },
+    secret: config.session.secret,
+    name: "selvbetjening-ui",
+    resave: false,
+    saveUninitialized: false,
+    unset: "destroy",
+};
+
 const setupSession = () => {
-    const options = {
-        cookie: {
-            maxAge: config.session.maxAgeMs,
-            sameSite: "lax",
-            httpOnly: true,
-        },
-        secret: config.session.secret,
-        name: "selvbetjening-ui",
-        resave: false,
-        saveUninitialized: false,
-        unset: "destroy",
-    };
     if (process.env.NODE_ENV === "production") {
         options.cookie.secure = true;
         options.store = setupRedis();
     }
     return session(options);
-};
+}
 
 const setupRedis = () => {
     const store = RedisStore(session);
@@ -32,11 +33,11 @@ const setupRedis = () => {
     });
     client.unref();
     client.on("debug", console.log);
-
     return new store({
         client: client,
         disableTouch: true,
     });
+
 };
 
 const appSession = setupSession();
