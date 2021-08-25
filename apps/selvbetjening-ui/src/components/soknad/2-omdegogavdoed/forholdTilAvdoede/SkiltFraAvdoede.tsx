e7b63a5ef715c42ada33c7099846398444a8c021
Glenn Wedin <glenn.wedin@nav.no>
2021-08-25 15:45:56 +0200

    tEY-90: forenkler kode, sletter unødvendig test og retter feil.

--- apps/selvbetjening-ui/src/components/soknad/2-omdegogavdoed/forholdTilAvdoede/SkiltFraAvdoede.tsx
+++ apps/selvbetjening-ui/src/components/soknad/2-omdegogavdoed/forholdTilAvdoede/SkiltFraAvdoede.tsx
@@ -8,28 +8,24 @@ import { AlertStripeAdvarsel } from "nav-frontend-alertstriper";
 import { SkjemaGruppe } from "nav-frontend-skjema";
 import { useTranslation } from "react-i18next";
 
-export const giftMindreEnnFemtenOgDodsfallInnenfemAarEtterSkilsmisse = (
-    datoForInngaattPartnerskap: string,
-    datoForDoedsfallet: string,
-    datoForSkilsmisse: string
-): IValg => {
-    const antallAarPartnerskap = antallAarMellom(datoForInngaattPartnerskap, datoForSkilsmisse) || 0;
-    const antallAarMellomSkillsmisseDodsfall = antallAarMellom(datoForSkilsmisse, datoForDoedsfallet) || 0;
-
-    if (antallAarPartnerskap > 15) {
-        return IValg.NEI;
-    }
-    if (antallAarPartnerskap < 15) {
-        if (antallAarMellomSkillsmisseDodsfall < 5) {
-            return IValg.NEI;
-        }
-    }
-    return IValg.JA;
-};
-
 const giftMerEnn25aar = (datoForInngaattPartnerskap: string, datoForSkilsmisse: string): IValg => {
     const antallAarPartnerskap = antallAarMellom(datoForInngaattPartnerskap, datoForSkilsmisse) || 0;
     if (antallAarPartnerskap >= 25) {
+        return IValg.JA;
+    }
+    return IValg.NEI;
+};
+const giftMindreEnn15aar = (datoForInngaattPartnerskap: string, datoForSkilsmisse: string) => {
+    const antallAarPartnerskap = antallAarMellom(datoForInngaattPartnerskap, datoForSkilsmisse) || 0;
+    if (antallAarPartnerskap < 15) {
+        return IValg.JA;
+    }
+    return IValg.NEI;
+};
+
+const mindreEnnFemaarMellomSkillsmisseOgDodsfall = (datoForSkilsmisse: string, datoForDoedsfallet: string) => {
+    const antallAarMellomSkillsmisseDodsfall = antallAarMellom(datoForSkilsmisse, datoForDoedsfallet) || 0;
+    if (antallAarMellomSkillsmisseDodsfall <= 5) {
         return IValg.JA;
     }
     return IValg.NEI;
@@ -48,13 +44,9 @@ const SkiltFraAvdoede = () => {
     const fellesBarn = watch("forholdTilAvdoede.fellesBarn");
     const samboereMedFellesBarn = watch("forholdTilAvdoede.samboereMedFellesBarn");
 
-    /* Dersom gift i mindre enn 15 år og dødsfall mer/opp til 5 år siden skillsmisse */
-    const skalViseSporsmaal = giftMindreEnnFemtenOgDodsfallInnenfemAarEtterSkilsmisse(
-        datoForInngaattPartnerskap, // Må sjekke om 25 år gjelder her også
-        datoForDoedsfallet,
-        datoForSkilsmisse
-    );
+    const mindreEnn15aar = giftMindreEnn15aar(datoForInngaattPartnerskap, datoForSkilsmisse);
     const merEnn25aar = giftMerEnn25aar(datoForInngaattPartnerskap, datoForSkilsmisse);
+    const mindreEnnFemAar = mindreEnnFemaarMellomSkillsmisseOgDodsfall(datoForSkilsmisse, datoForDoedsfallet);
 
     return (
         <>
@@ -80,48 +72,45 @@ const SkiltFraAvdoede = () => {
                 legend={t("omDegOgAvdoed.forholdTilAvdoede.fellesBarn")}
             />
 
-            {/* Dersom felles barn */}
-            {fellesBarn === IValg.JA && (
+            {fellesBarn === IValg.JA && mindreEnn15aar === IValg.JA && (
                 <>
                     <RHFSpoersmaalRadio
                         name={"forholdTilAvdoede.samboereMedFellesBarn"}
                         legend={t("omDegOgAvdoed.forholdTilAvdoede.samboereMedFellesBarn")}
                     />
 
-                    {samboereMedFellesBarn === IValg.NEI &&
-                        merEnn25aar === IValg.NEI && ( // OG ikke samboere/gift i over 25 år?
-                            <SkjemaGruppe>
-                                <AlertStripeAdvarsel>
-                                    {t("omDegOgAvdoed.forholdTilAvdoede.ingenRettighetAdvarsel")}
-                                </AlertStripeAdvarsel>
-                            </SkjemaGruppe>
-                        )}
-
-                    {/* Mottok du ektefellebidrag? */}
-                    {skalViseSporsmaal === IValg.JA && samboereMedFellesBarn === IValg.JA && (
-                        <>
-                            <RHFSpoersmaalRadio
-                                name={"forholdTilAvdoede.mottokEktefelleBidrag"}
-                                legend={t("omDegOgAvdoed.forholdTilAvdoede.mottokEktefelleBidrag")}
-                            />
-                            {mottokEktefelleBidrag === IValg.NEI && (
-                                <SkjemaGruppe>
-                                    <AlertStripeAdvarsel>
-                                        {t("omDegOgAvdoed.forholdTilAvdoede.ingenRettighetAdvarsel")}
-                                    </AlertStripeAdvarsel>
-                                </SkjemaGruppe>
-                            )}
-                        </>
+                    {samboereMedFellesBarn === IValg.NEI && merEnn25aar === IValg.NEI && (
+                        <SkjemaGruppe>
+                            <AlertStripeAdvarsel>
+                                {t("omDegOgAvdoed.forholdTilAvdoede.ingenRettighetAdvarsel")}
+                            </AlertStripeAdvarsel>
+                        </SkjemaGruppe>
                     )}
                 </>
             )}
-            {/* Dersom ikke felles barn */}
-            {fellesBarn === IValg.NEI && skalViseSporsmaal === IValg.JA && merEnn25aar === IValg.NEI && (
+
+            {fellesBarn === IValg.NEI && mindreEnn15aar === IValg.JA && merEnn25aar === IValg.NEI && (
                 <SkjemaGruppe>
                     <AlertStripeAdvarsel>
                         {t("omDegOgAvdoed.forholdTilAvdoede.ingenRettighetAdvarsel")}
                     </AlertStripeAdvarsel>
                 </SkjemaGruppe>
+            )}
+
+            {mindreEnnFemAar === IValg.NEI && (
+                <>
+                    <RHFSpoersmaalRadio
+                        name={"forholdTilAvdoede.mottokEktefelleBidrag"}
+                        legend={t("omDegOgAvdoed.forholdTilAvdoede.mottokEktefelleBidrag")}
+                    />
+                    {mottokEktefelleBidrag === IValg.NEI && (
+                        <SkjemaGruppe>
+                            <AlertStripeAdvarsel>
+                                {t("omDegOgAvdoed.forholdTilAvdoede.ingenRettighetAdvarsel")}
+                            </AlertStripeAdvarsel>
+                        </SkjemaGruppe>
+                    )}
+                </>
             )}
         </>
     );
