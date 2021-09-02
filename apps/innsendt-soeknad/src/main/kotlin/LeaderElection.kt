0360c4da385f7d09b9a87906eeb8d21c3f39c6bf
Lars Erik Dahl <lars.erik.dahl@nav.no>
2021-09-02 11:38:33 +0200

    Innsendt-soeknad: Jsonmelding fra leaderelection kommer som tekst

--- apps/innsendt-soeknad/src/main/kotlin/LeaderElection.kt
+++ apps/innsendt-soeknad/src/main/kotlin/LeaderElection.kt
@@ -1,6 +1,6 @@
 package no.nav.etterlatte
 
-import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
@@ -8,12 +8,13 @@ import org.slf4j.LoggerFactory
 import java.net.InetAddress
 
 object LeaderElection {
-    val logger = LoggerFactory.getLogger("no.pensjon.etterlatte")
-    private  val electorPath: String? = System.getenv("ELECTOR_PATH")
+    private val logger = LoggerFactory.getLogger("no.pensjon.etterlatte")
+    private val electorPath: String? = System.getenv("ELECTOR_PATH")
     private val httpClient = HttpClient(CIO)
+    private val objectMapper = jacksonObjectMapper()
+    private val me:String?  = InetAddress.getLocalHost().hostName
     suspend fun isLeader(): Boolean{
-        val leader = httpClient.get<JsonNode>("http://$electorPath").get("name").asText()
-        val me = InetAddress.getLocalHost().getHostName()
+        val leader = httpClient.get<String>("http://$electorPath").let (objectMapper::readTree).get("name").asText()
         val amLeader = leader == me
         logger.info("Current pod: $me. Leader: $leader. Current pod is leader: $amLeader")
         return amLeader
