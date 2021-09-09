648b35c183c24ce30af18a29609320868eea1a6f
HelgeJanbu <helge.janbu@nav.no>
2021-09-09 11:21:54 +0200

    etterlatte sjekk-adressebeskyttelse: Endret outbound scope til annen url 2.forsøk

--- apps/sjekk-adressebeskyttelse/src/main/kotlin/AppBuilder.kt
+++ apps/sjekk-adressebeskyttelse/src/main/kotlin/AppBuilder.kt
@@ -20,7 +20,7 @@ class AppBuilder(private val props: Map<String, String>) {
         install(Auth) {
             clientCredential {
                 config = props.toMutableMap()
-                    .apply { put("AZURE_APP_OUTBOUND_SCOPE", "api://dev-gcp.etterlatte.etterlatte-pdl-proxy/pdl") }
+                    .apply { put("AZURE_APP_OUTBOUND_SCOPE", "api://dev-gcp.etterlatte.etterlatte-pdl-proxy/.default") }
             }
         }
     }.also { Runtime.getRuntime().addShutdownHook(Thread { it.close() }) }
