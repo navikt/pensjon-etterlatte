9b750f238bb9221a04fdb0988806d9fb1f090256
Glenn Wedin <glenn.wedin@nav.no>
2021-09-23 15:19:31 +0200

    EY-181: flere tester

---
+++ apps/selvbetjening-ui/src/components/felles/HvorforSpoerVi.test.js
@@ -0,0 +1,25 @@
+import { render, fireEvent } from "@testing-library/react";
+import i18n from "../../i18n"; // tar med denne for å slippe warnings fra komponenten vi tester
+import HvorforSpoerVi from "./HvorforSpoerVi";
+
+describe("HvorforSpoerVi", () => {
+    it("skal rendre hvorfor spør vi som lukket boks", () => {
+        const { container } = render(
+            <HvorforSpoerVi>
+                <span data-testid="test-tekst">Diverse informasjon om hvorfor vi spør!</span>
+            </HvorforSpoerVi>
+        );
+        expect(container).toMatchSnapshot();
+    });
+
+    it("Skal åpne boksen og matche med innholdet vi legger inn", () => {
+        const { container, getByTestId } = render(
+            <HvorforSpoerVi>
+                <span data-testid="test-tekst">Diverse informasjon om hvorfor vi spør!</span>
+            </HvorforSpoerVi>
+        );
+        fireEvent.click(getByTestId("hvorforPanel_toggle"));
+        expect(getByTestId("test-tekst").innerHTML).toEqual("Diverse informasjon om hvorfor vi spør!");
+        expect(container).toMatchSnapshot();
+    });
+});
--- apps/selvbetjening-ui/src/components/felles/HvorforSpoerVi.tsx
+++ apps/selvbetjening-ui/src/components/felles/HvorforSpoerVi.tsx
@@ -1,38 +1,33 @@
 import { FC, useState } from "react";
-import { v4 as uuid } from "uuid";
-import { Collapse, Expand } from "@navikt/ds-icons"
-import { BodyLong } from "@navikt/ds-react"
+//import { v4 as uuid } from "uuid";
+import { Collapse, Expand } from "@navikt/ds-icons";
+import { BodyLong } from "@navikt/ds-react";
 import { useTranslation } from "react-i18next";
 
 const HvorforSpoerVi: FC = ({ children }) => {
-    const id = uuid();
+    //const id = uuid();
     const [erApen, setErApen] = useState(false);
     const { t } = useTranslation();
 
     return (
-        <div className={"hvorforPanel"} id={id}>
+        <div className={"hvorforPanel"} /*id={id}*/>
             <button
+                data-testid="hvorforPanel_toggle"
                 type={"button"}
                 className={"hvorforPanel__toggle"}
                 onClick={() => setErApen(!erApen)}
             >
-                <span>
-                    {t("hvorforSpoerVi")}
-                </span>
-                <span>
-                    {erApen ? <Collapse/> : <Expand/>}
-                </span>
+                <span>{t("hvorforSpoerVi")}</span>
+                <span>{erApen ? <Collapse /> : <Expand />}</span>
             </button>
 
             {erApen && (
                 <div className={"hvorforPanel__innhold"}>
-                    <BodyLong>
-                        {children}
-                    </BodyLong>
+                    <BodyLong>{children}</BodyLong>
                 </div>
             )}
         </div>
-    )
-}
+    );
+};
 
 export default HvorforSpoerVi;
