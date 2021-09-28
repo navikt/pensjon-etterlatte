4bda6f48f1fa02e44fc45bb3a6e26ed814a46057
Glenn Wedin <glenn.wedin@nav.no>
2021-09-28 12:52:37 +0200

    EY-185: korrigert løype etter flytskjema og kommentert ut ikke-rett-til tekster på denne saken

--- apps/selvbetjening-ui/src/components/soknad/2-omdegogavdoed/forholdTilAvdoede/SkiltFraAvdoede.tsx
+++ apps/selvbetjening-ui/src/components/soknad/2-omdegogavdoed/forholdTilAvdoede/SkiltFraAvdoede.tsx
@@ -4,7 +4,7 @@ import { ISoekerOgAvdoed } from "../../../../typer/person";
 import { useFormContext } from "react-hook-form";
 import { antallAarMellom } from "../../../../utils/dato";
 import { IValg } from "../../../../typer/Spoersmaal";
-import { Alert } from "@navikt/ds-react";
+//import { Alert } from "@navikt/ds-react";
 import { SkjemaGruppe } from "nav-frontend-skjema";
 import { useTranslation } from "react-i18next";
 
@@ -25,7 +25,7 @@ const giftMindreEnn15aar = (datoForInngaattPartnerskap: string, datoForSkilsmiss
 
 const mindreEnnFemaarMellomSkillsmisseOgDodsfall = (datoForSkilsmisse: string, datoForDoedsfallet: string) => {
     const antallAarMellomSkillsmisseDodsfall = antallAarMellom(datoForSkilsmisse, datoForDoedsfallet) || 0;
-    if (antallAarMellomSkillsmisseDodsfall <= 5) {
+    if (antallAarMellomSkillsmisseDodsfall < 5) {
         return IValg.JA;
     }
     return IValg.NEI;
@@ -40,9 +40,9 @@ const SkiltFraAvdoede = () => {
     const datoForSkilsmisse = watch("forholdTilAvdoede.datoForSkilsmisse");
     const datoForDoedsfallet = watch("avdoed.datoForDoedsfallet");
 
-    const mottokEktefelleBidrag = watch("forholdTilAvdoede.mottokEktefelleBidrag");
+    //const mottokEktefelleBidrag = watch("forholdTilAvdoede.mottokEktefelleBidrag");
     const fellesBarn = watch("forholdTilAvdoede.fellesBarn");
-    const samboereMedFellesBarn = watch("forholdTilAvdoede.samboereMedFellesBarn");
+    //const samboereMedFellesBarn = watch("forholdTilAvdoede.samboereMedFellesBarn");
 
     const mindreEnn15aar = giftMindreEnn15aar(datoForInngaattPartnerskap, datoForSkilsmisse);
     const merEnn25aar = giftMerEnn25aar(datoForInngaattPartnerskap, datoForSkilsmisse);
@@ -79,39 +79,38 @@ const SkiltFraAvdoede = () => {
                         legend={t("omDegOgAvdoed.forholdTilAvdoede.samboereMedFellesBarn")}
                     />
 
-                    {samboereMedFellesBarn === IValg.NEI && merEnn25aar === IValg.NEI && (
+                    {/*samboereMedFellesBarn === IValg.NEI && merEnn25aar === IValg.NEI && (
                         <SkjemaGruppe>
                             <Alert variant={"warning"}>
                                 {t("omDegOgAvdoed.forholdTilAvdoede.ingenRettighetAdvarsel")}
                             </Alert>
                         </SkjemaGruppe>
-                    )}
+                    )*/}
                 </>
             )}
 
-            {fellesBarn === IValg.NEI && mindreEnn15aar === IValg.JA && merEnn25aar === IValg.NEI && (
+            {/*fellesBarn === IValg.NEI && mindreEnn15aar === IValg.JA && merEnn25aar === IValg.NEI && (
                 <SkjemaGruppe>
-                    <Alert variant={"warning"}>
-                        {t("omDegOgAvdoed.forholdTilAvdoede.ingenRettighetAdvarsel")}
-                    </Alert>
+                    <Alert variant={"warning"}>{t("omDegOgAvdoed.forholdTilAvdoede.ingenRettighetAdvarsel")}</Alert>
                 </SkjemaGruppe>
-            )}
+            )*/}
 
-            {mindreEnnFemAar === IValg.NEI && (
+            {(mindreEnnFemAar === IValg.NEI && merEnn25aar === IValg.JA) ||
+            (fellesBarn === IValg.JA && mindreEnn15aar === IValg.NEI && mindreEnnFemAar === IValg.NEI) ? (
                 <>
                     <RHFSpoersmaalRadio
                         name={"forholdTilAvdoede.mottokEktefelleBidrag"}
                         legend={t("omDegOgAvdoed.forholdTilAvdoede.mottokEktefelleBidrag")}
                     />
-                    {mottokEktefelleBidrag === IValg.NEI && (
+                    {/*mottokEktefelleBidrag === IValg.NEI && (
                         <SkjemaGruppe>
                             <Alert variant={"warning"}>
                                 {t("omDegOgAvdoed.forholdTilAvdoede.ingenRettighetAdvarsel")}
                             </Alert>
                         </SkjemaGruppe>
-                    )}
+                    )*/}
                 </>
-            )}
+            ) : null}
         </>
     );
 };
