3bcd6088a6415fc248fe907efb6938ffcc2c74db
Glenn Wedin <glenn.wedin@nav.no>
2021-09-29 13:17:41 +0200

    tester

---
+++ apps/selvbetjening-ui/src/hooks/useLanguage.test.js
@@ -0,0 +1,55 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import i18next from "i18next";
+import { useLanguage } from "./useLanguage";
+
+jest.mock("i18next", () => ({
+    changeLanguage: jest.fn(),
+}));
+
+const navigator = { language: "nb-NO" };
+Object.defineProperty(window, "navigator", {
+    value: navigator,
+    writable: true,
+});
+
+const localStorage = {
+    setItem: jest.fn(),
+    getItem: jest.fn(),
+};
+Object.defineProperty(window, "localStorage", {
+    value: localStorage,
+    writable: true,
+});
+
+describe("useLanguage", () => {
+    it("Skal sette riktig verdi og ha blitt kalt to ganger", () => {
+        const { result } = renderHook(() => useLanguage());
+
+        expect(result.current.currentLanguage).toEqual("nb");
+        expect(i18next.changeLanguage).toHaveBeenCalledTimes(1);
+
+        act(() => {
+            result.current.setLanguage("test");
+        });
+
+        expect(result.current.currentLanguage).toEqual("test");
+        expect(i18next.changeLanguage).toHaveBeenCalledTimes(2);
+    });
+
+    it("Skal sette en verdi i localstorage", () => {
+        const { result } = renderHook(() => useLanguage());
+        act(() => {
+            result.current.setLanguage("test");
+        });
+        expect(localStorage.setItem).toHaveBeenCalledTimes(2);
+    });
+
+    it("Skal returnere state og setState", () => {
+        const { result } = renderHook(() => useLanguage());
+        expect(result.current.setLanguage).toBeDefined();
+        expect(result.current.currentLanguage).toBeDefined();
+    });
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+});
