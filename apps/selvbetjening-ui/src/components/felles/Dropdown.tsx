cc5c9480df97ca5415c48603f3093e9e61cae49a
Glenn Wedin <glenn.wedin@nav.no>
2021-09-30 10:01:02 +0200

    tester alternativ dropdown

--- apps/selvbetjening-ui/src/components/felles/Dropdown.tsx
+++ apps/selvbetjening-ui/src/components/felles/Dropdown.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
 import { Language } from "../../hooks/useLanguage";
-import { BritiskFlagg, NorskFlagg } from "./Flagg";
+//import { BritiskFlagg, NorskFlagg } from "./Flagg";
 import "./Dropdown.scss";
+import { Select, SkjemaGruppe } from "nav-frontend-skjema";
 
 export const Dropdown = (props: { onChange: any; value: string }) => {
-    const [open, setOpen] = useState(false);
+    // const [open, setOpen] = useState(false);
 
+    /*
     const toggle = (e: any) => {
         e.stopPropagation();
         e.preventDefault();
@@ -15,7 +17,12 @@ export const Dropdown = (props: { onChange: any; value: string }) => {
     const select = (e: any) => {
         props.onChange(e.target.dataset.value);
     };
+    */
+    const setLanguage = (e: any) => {
+        props.onChange(e.target.value);
+    };
 
+    /*
     const spraak: any = {
         nb: (
             <>
@@ -35,26 +42,64 @@ export const Dropdown = (props: { onChange: any; value: string }) => {
                 English
             </>
         ),
-    };
+    };*/
 
     return (
-        <div className="dropdown" tabIndex={0} onClick={toggle} onKeyPress={toggle}>
-            <div className="button">{spraak[props.value]}</div>
-            {open && (
-                <ul>
-                    <li onClick={select} onKeyPress={select} tabIndex={0} data-value={Language.NORSK_BOKMAAL}>
-                        <NorskFlagg /> Bokmål
-                    </li>
-                    <li onClick={select} onKeyPress={select} tabIndex={0} data-value={Language.NORSK_NYNORSK}>
-                        <NorskFlagg />
-                        Nynorsk
-                    </li>
-                    <li onClick={select} onKeyPress={select} tabIndex={0} data-value={Language.ENGELSK}>
-                        <BritiskFlagg />
-                        English
-                    </li>
-                </ul>
-            )}
-        </div>
+        <>
+            {/*
+            <div
+                className="dropdown"
+                tabIndex={0}
+                onClick={toggle}
+                onKeyPress={toggle}
+                aria-haspopup="listbox"
+                aria-labelledby="listbox-lang-value"
+            >
+                <div id="listbox-lang-value" className="button">
+                    {spraak[props.value]}
+                </div>
+                {open && (
+                    <ul role="listbox">
+                        <li
+                            role="option"
+                            onClick={select}
+                            onKeyPress={select}
+                            tabIndex={0}
+                            data-value={Language.NORSK_BOKMAAL}
+                        >
+                            <NorskFlagg /> Bokmål
+                        </li>
+                        <li
+                            role="option"
+                            onClick={select}
+                            onKeyPress={select}
+                            tabIndex={0}
+                            data-value={Language.NORSK_NYNORSK}
+                        >
+                            <NorskFlagg />
+                            Nynorsk
+                        </li>
+                        <li
+                            role="option"
+                            onClick={select}
+                            onKeyPress={select}
+                            tabIndex={0}
+                            data-value={Language.ENGELSK}
+                        >
+                            <BritiskFlagg />
+                            English
+                        </li>
+                    </ul>
+                )}
+            </div>
+                */}
+            <SkjemaGruppe className="dropdown_alt2">
+                <Select onChange={setLanguage} value={props.value} label="Velg språk">
+                    <option value={Language.NORSK_BOKMAAL}>Bokmål</option>
+                    <option value={Language.NORSK_NYNORSK}>Nynorsk</option>
+                    <option value={Language.ENGELSK}>English</option>
+                </Select>
+            </SkjemaGruppe>
+        </>
     );
 };
