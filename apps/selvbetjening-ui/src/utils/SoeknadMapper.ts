c10ad97af5b173c6b8091ca8d616e9e0f3282a13
Glenn Wedin <glenn.wedin@nav.no>
2021-10-01 13:57:54 +0200

    EY-207: utdanning ble nullet ut i oppsummeringsmapperen, må struktureres litt bedre

--- apps/selvbetjening-ui/src/utils/SoeknadMapper.ts
+++ apps/selvbetjening-ui/src/utils/SoeknadMapper.ts
@@ -9,7 +9,7 @@ import { IArbeidsforhold, ISelvstendigNaeringsdrivende } from "../typer/arbeidsf
 
 export default class SoeknadMapper {
     private otr: ObjectTreeReader;
-    private readonly t: TFunction
+    private readonly t: TFunction;
 
     constructor(t: TFunction, i18n: i18n) {
         this.t = t;
@@ -22,17 +22,19 @@ export default class SoeknadMapper {
             this.mapOmDegOgAvdoed(soeknad.omDegOgAvdoed),
             this.mapOmDenAvdoede(soeknad.omDenAvdoede),
             this.mapDinSituasjon(soeknad.dinSituasjon),
-            this.mapOpplysningerOmBarn(soeknad.opplysningerOmBarn)
+            this.mapOpplysningerOmBarn(soeknad.opplysningerOmBarn),
         ];
     }
 
     private mapOmDeg(omDeg: ISoeker, bruker: IBruker): Gruppe {
         const personalia = {
             navn: `${bruker.fornavn} ${bruker.etternavn}`,
-            adresse: `${bruker.adresse} ${bruker.husnummer}${bruker.husbokstav || ""}, ${bruker.postnummer} ${bruker.poststed}`,
+            adresse: `${bruker.adresse} ${bruker.husnummer}${bruker.husbokstav || ""}, ${bruker.postnummer} ${
+                bruker.poststed
+            }`,
             foedselsnummer: bruker.foedselsnummer,
             sivilstatus: bruker.sivilstatus,
-            statsborgerskap: bruker.statsborgerskap
+            statsborgerskap: bruker.statsborgerskap,
         };
 
         return {
@@ -41,19 +43,22 @@ export default class SoeknadMapper {
             elementer: [
                 {
                     tittel: "Personalia",
-                    innhold: this.otr.traverse(personalia, "felles")
+                    innhold: this.otr.traverse(personalia, "felles"),
                 },
                 {
                     tittel: "Opplysninger om søkeren",
-                    innhold: this.otr.traverse<ISoeker>({
-                        ...omDeg,
-                        nySivilstatus: {
-                            ...omDeg.nySivilstatus,
-                            sivilstatus: this.t(omDeg.nySivilstatus?.sivilstatus || "")
-                        }
-                    }, "omDeg")
-                }
-            ]
+                    innhold: this.otr.traverse<ISoeker>(
+                        {
+                            ...omDeg,
+                            nySivilstatus: {
+                                ...omDeg.nySivilstatus,
+                                sivilstatus: this.t(omDeg.nySivilstatus?.sivilstatus || ""),
+                            },
+                        },
+                        "omDeg"
+                    ),
+                },
+            ],
         };
     }
 
@@ -63,104 +68,122 @@ export default class SoeknadMapper {
             path: StegPath.OmDegOgAvdoed,
             elementer: [
                 {
-                    innhold: this.otr.traverse<ISoekerOgAvdoed>(omDegOgAvdoed, "omDegOgAvdoed")
-                }
-            ]
+                    innhold: this.otr.traverse<ISoekerOgAvdoed>(omDegOgAvdoed, "omDegOgAvdoed"),
+                },
+            ],
         };
     }
 
     private mapOmDenAvdoede(omDenAvdoede: IAvdoed): Gruppe {
-        const oppholdUtland: Element[] = omDenAvdoede.boddEllerJobbetUtland?.oppholdUtland?.map((oppholdUtland) => {
-            const opphold: IOppholdUtland = {
-                land: oppholdUtland.land,
-                beskrivelse: oppholdUtland.beskrivelse,
-                fraDato: oppholdUtland.fraDato,
-                tilDato: oppholdUtland.tilDato,
-                medlemFolketrygd: oppholdUtland.medlemFolketrygd,
-                mottokPensjon: oppholdUtland.mottokPensjon,
-            };
+        const oppholdUtland: Element[] =
+            omDenAvdoede.boddEllerJobbetUtland?.oppholdUtland?.map((oppholdUtland) => {
+                const opphold: IOppholdUtland = {
+                    land: oppholdUtland.land,
+                    beskrivelse: oppholdUtland.beskrivelse,
+                    fraDato: oppholdUtland.fraDato,
+                    tilDato: oppholdUtland.tilDato,
+                    medlemFolketrygd: oppholdUtland.medlemFolketrygd,
+                    mottokPensjon: oppholdUtland.mottokPensjon,
+                };
 
-            return {
-                tittel: `Opphold i ${opphold.land}`,
-                innhold: this.otr.traverse({
-                    ...opphold,
-                    beskrivelse: opphold.beskrivelse?.map(val => this.t(val)).join(", ")
-                }, "omDenAvdoede.boddEllerJobbetUtland.oppholdUtland")
-            } as Element
-        }) || [];
+                return {
+                    tittel: `Opphold i ${opphold.land}`,
+                    innhold: this.otr.traverse(
+                        {
+                            ...opphold,
+                            beskrivelse: opphold.beskrivelse?.map((val) => this.t(val)).join(", "),
+                        },
+                        "omDenAvdoede.boddEllerJobbetUtland.oppholdUtland"
+                    ),
+                } as Element;
+            }) || [];
 
         return {
             tittel: this.t("omDenAvdoede.tittel"),
             path: StegPath.OmAvdoed,
             elementer: [
                 {
-                    innhold: this.otr.traverse<IAvdoed>({
-                        ...omDenAvdoede,
-                        boddEllerJobbetUtland: undefined
-                    }, "omDenAvdoede")
+                    innhold: this.otr.traverse<IAvdoed>(
+                        {
+                            ...omDenAvdoede,
+                            boddEllerJobbetUtland: undefined,
+                        },
+                        "omDenAvdoede"
+                    ),
                 },
-                ...oppholdUtland
-            ]
+                ...oppholdUtland,
+            ],
         };
     }
 
     private mapDinSituasjon(dinSituasjon: ISituasjon): Gruppe {
-        const arbeidsforhold: Element[] = dinSituasjon.arbeidsforhold?.map(arbeid => {
-            return {
-                tittel: `${arbeid.arbeidsgiver}`,
-                innhold: this.otr.traverse<IArbeidsforhold>({
-                    ...arbeid,
-                    arbeidsgiver: undefined
-                }, "dinSituasjon.arbeidsforhold")
-            } as Element
-        }) || [];
+        const arbeidsforhold: Element[] =
+            dinSituasjon.arbeidsforhold?.map((arbeid) => {
+                return {
+                    tittel: `${arbeid.arbeidsgiver}`,
+                    innhold: this.otr.traverse<IArbeidsforhold>(
+                        {
+                            ...arbeid,
+                            arbeidsgiver: undefined,
+                        },
+                        "dinSituasjon.arbeidsforhold"
+                    ),
+                } as Element;
+            }) || [];
 
-        const selvstendigNaeringsdrivende: Element[] = dinSituasjon.selvstendig?.map(arbeid => {
-            return {
-                tittel: `${arbeid.beskrivelse}`,
-                innhold: this.otr.traverse<ISelvstendigNaeringsdrivende>({
-                    ...arbeid,
-                    beskrivelse: undefined,
-                }, "dinSituasjon.selvstendig")
-            } as Element
-        }) || [];
+        const selvstendigNaeringsdrivende: Element[] =
+            dinSituasjon.selvstendig?.map((arbeid) => {
+                return {
+                    tittel: `${arbeid.beskrivelse}`,
+                    innhold: this.otr.traverse<ISelvstendigNaeringsdrivende>(
+                        {
+                            ...arbeid,
+                            beskrivelse: undefined,
+                        },
+                        "dinSituasjon.selvstendig"
+                    ),
+                } as Element;
+            }) || [];
 
         return {
             tittel: this.t("dinSituasjon.tittel"),
             path: StegPath.DinSituasjon,
             elementer: [
                 {
-                    innhold: this.otr.traverse<ISituasjon>({
-                        ...dinSituasjon,
-                        utdanning: undefined,
-                        arbeidsforhold: undefined,
-                        selvstendig: undefined,
-                    }, "dinSituasjon")
-
+                    innhold: this.otr.traverse<ISituasjon>(
+                        {
+                            ...dinSituasjon,
+                            //utdanning: undefined,
+                            arbeidsforhold: undefined,
+                            selvstendig: undefined,
+                        },
+                        "dinSituasjon"
+                    ),
                 },
                 ...arbeidsforhold,
-                ...selvstendigNaeringsdrivende
-            ]
+                ...selvstendigNaeringsdrivende,
+            ],
         };
     }
 
     private mapOpplysningerOmBarn(opplysningerOmBarn: IOmBarn): Gruppe {
-        const barn: Element[] = opplysningerOmBarn.barn?.map(barn => {
-            return {
-                tittel: `${barn.fornavn} ${barn.etternavn}`,
-                innhold: this.otr.traverse(barn, "omBarn")
-            } as Element
-        }) || [];
+        const barn: Element[] =
+            opplysningerOmBarn.barn?.map((barn) => {
+                return {
+                    tittel: `${barn.fornavn} ${barn.etternavn}`,
+                    innhold: this.otr.traverse(barn, "omBarn"),
+                } as Element;
+            }) || [];
 
         return {
             tittel: this.t("omBarn.tittel"),
             path: StegPath.OmBarn,
             elementer: [
                 {
-                    innhold: this.otr.traverse<IOmBarn>({ ...opplysningerOmBarn, barn: undefined }, "omBarn")
+                    innhold: this.otr.traverse<IOmBarn>({ ...opplysningerOmBarn, barn: undefined }, "omBarn"),
                 },
-                ...barn
-            ]
+                ...barn,
+            ],
         };
     }
 }
