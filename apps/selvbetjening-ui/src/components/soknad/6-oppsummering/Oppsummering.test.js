920489d24807258d92c40ae3359a6fa208dad8c6
Glenn Wedin <glenn.wedin@nav.no>
2021-10-06 14:54:25 +0200

    litt flere ui-tester

--- apps/selvbetjening-ui/src/components/soknad/6-oppsummering/Oppsummering.test.js
+++ apps/selvbetjening-ui/src/components/soknad/6-oppsummering/Oppsummering.test.js
@@ -1,5 +1,6 @@
-import { render } from "@testing-library/react";
+import { render, act, fireEvent } from "@testing-library/react";
 import * as JSutils from "nav-frontend-js-utils";
+import { AccordionItem } from "./AccordionItem";
 import Oppsummering from "./Oppsummering";
 
 jest.mock("react-i18next", () => ({
@@ -24,5 +25,26 @@ describe("Om den avdøde", () => {
     it("Snapshot", () => {
         const { container } = render(<Oppsummering />);
         expect(container).toMatchSnapshot();
+    });
+});
+
+describe("Test accordionItem", () => {
+    it("skal endre aria-expanded", () => {
+        const { container, getByText } = render(
+            <AccordionItem tittel="Testtittel" defaultOpen={false}>
+                Innhold
+            </AccordionItem>
+        );
+        expect(container.querySelectorAll("[aria-expanded]")[0].getAttribute("aria-expanded")).toBe("false");
+        fireEvent.click(getByText("Testtittel"));
+        expect(container.querySelectorAll("[aria-expanded]")[0].getAttribute("aria-expanded")).toBe("true");
+    });
+    it("skal rendre innholdet synlig", () => {
+        const { getByText } = render(
+            <AccordionItem tittel="Testtittel" defaultOpen={true}>
+                Innhold
+            </AccordionItem>
+        );
+        expect(getByText("Innhold")).toBeDefined()
     });
 });
