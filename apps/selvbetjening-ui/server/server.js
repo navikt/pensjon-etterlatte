b3a36d0e0a497af948cccd33c2344a066e66f5b7
NN <nicolas.nordhagen@nav.no>
2021-10-19 14:23:17 +0200

    EY-260 oppdatere ingress prod

--- apps/selvbetjening-ui/server/server.js
+++ apps/selvbetjening-ui/server/server.js
@@ -13,6 +13,11 @@ const app = express();
 app.set("trust proxy", 1);
 app.use(basePath, express.static(buildPath, {index: false}));
 
+// Endpoints to verify is app is ready/alive
+app.get(`${basePath}/isAlive|${basePath}/isReady`, (req, res) => {
+    res.send("OK");
+});
+
 if (config.env.isLabsCluster) {
     api.mock(app);
 } else {
@@ -21,15 +26,6 @@ if (config.env.isLabsCluster) {
 }
 
 decorator.setup(app, `${buildPath}/index.html`);
-
-// Endpoints to verify is app is ready/alive
-app.get(`${basePath}/isAlive|${basePath}/isReady`, (req, res) => {
-    res.send("OK");
-});
-
-app.get(`${basePath}/isAlive|${basePath}/isReady`, (req, res) => {
-    res.send("OK");
-});
 
 const port = config.app.port;
 app.listen(port, () => {
