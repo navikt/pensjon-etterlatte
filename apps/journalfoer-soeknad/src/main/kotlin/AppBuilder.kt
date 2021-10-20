1da49272a1ba678a5f0b2232888c51559017b514
Glenn Wedin <glenn.wedin@nav.no>
2021-10-20 13:11:32 +0200

    EY-194: tester å gå rett på dokarkiv

--- apps/journalfoer-soeknad/src/main/kotlin/AppBuilder.kt
+++ apps/journalfoer-soeknad/src/main/kotlin/AppBuilder.kt
@@ -11,6 +11,7 @@ class AppBuilder(private val props: Map<String, String>) {
     companion object {
         private const val CONFIG_PDF_URL = "PDF_URL"
         private const val CONFIG_DOKARKIV_URL = "DOKARKIV_URL"
+        private const val CONFIG_AZURE_DOKARKIV_SCOPE = "AZURE_DOKARKIV_SCOPE"
     }
 
     fun genererPdf() = PdfGenerator(pdfhttpclient(), props[CONFIG_PDF_URL]!!)
@@ -21,7 +22,7 @@ class AppBuilder(private val props: Map<String, String>) {
         install(Auth) {
             clientCredential {
                 config = System.getenv().toMutableMap().apply {
-                    put("AZURE_APP_OUTBOUND_SCOPE", "api://dev-fss.etterlatte.etterlatte-proxy/.default")
+                    put("AZURE_APP_OUTBOUND_SCOPE", props[CONFIG_AZURE_DOKARKIV_SCOPE])
                 }
             }
         }
