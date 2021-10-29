873ac7938f03674281104467735de2d542896148
Espen Schei <espen.schei@nav.no>
2021-10-29 15:04:13 +0200

    Bør fikse tidssone-problematikk i tester.

--- apps/etterlatte-notifikasjoner/src/test/kotlin/SendNotifikasjonTest.kt
+++ apps/etterlatte-notifikasjoner/src/test/kotlin/SendNotifikasjonTest.kt
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.time.Instant
 import java.time.LocalDateTime
-import java.util.*
+import java.time.ZoneOffset
 
 class SendNotifikasjonTest {
     private val sendNotifikasjon = SendNotifikasjon(
@@ -23,17 +23,17 @@ class SendNotifikasjonTest {
         assertEquals(true, isWithin10Seconds(beskjed.getTidspunkt().toLocalDateTime()))
         assertEquals(
             true,
-            isWithin10Seconds(beskjed.getSynligFremTil().toLocalDateTime(), LocalDateTime.now().plusDays(7))
+            isWithin10Seconds(
+                beskjed.getSynligFremTil().toLocalDateTime(),
+                LocalDateTime.now(ZoneOffset.UTC).plusDays(7)
+            )
         )
         assertEquals(4, beskjed.getSikkerhetsnivaa())
         assertEquals(false, beskjed.getEksternVarsling())
     }
 }
 
-fun isWithin10Seconds(actual: LocalDateTime, expected: LocalDateTime = LocalDateTime.now()): Boolean =
+fun isWithin10Seconds(actual: LocalDateTime, expected: LocalDateTime = LocalDateTime.now(ZoneOffset.UTC)): Boolean =
     actual.isBefore(expected.plusSeconds(10)) && actual.isAfter(expected.minusSeconds(10))
 
-fun Long.toLocalDateTime(): LocalDateTime = LocalDateTime.ofInstant(
-    Instant.ofEpochMilli(this),
-    TimeZone.getDefault().toZoneId()
-)
+fun Long.toLocalDateTime(): LocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(this), ZoneOffset.UTC)
