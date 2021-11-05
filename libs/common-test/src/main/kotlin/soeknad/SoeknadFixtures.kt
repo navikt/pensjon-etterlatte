13d134a5ce271e3d75c29622df577797de76a576
Espen Schei <espen.schei@nav.no>
2021-11-05 11:10:06 +0100

    SoeknadFixtures kan være object

--- libs/common-test/src/main/kotlin/soeknad/SoeknadFixtures.kt
+++ libs/common-test/src/main/kotlin/soeknad/SoeknadFixtures.kt
@@ -4,13 +4,11 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.etterlatte.libs.common.soeknad.Soeknad
 
-class SoeknadFixtures {
-    companion object {
-        private val mapper = jacksonObjectMapper()
-        private fun readFile(path: String) = mapper.readValue<Soeknad>(javaClass.getResource(path)!!.readText())
+object SoeknadFixtures {
+    private val mapper = jacksonObjectMapper()
+    private fun readFile(path: String) = mapper.readValue<Soeknad>(javaClass.getResource(path)!!.readText())
 
-        val soeknadMedBarnepensjon: Soeknad = readFile("/soeknad_med_barnepensjon.json")
-        val soeknadUtenBarn: Soeknad = readFile("/soeknad_uten_barn.json")
-        val soeknadMedBarnUtenBarnepensjon: Soeknad = readFile("/soeknad_med_barn_uten_barnepensjon.json")
-    }
+    val soeknadMedBarnepensjon: Soeknad = readFile("/soeknad_med_barnepensjon.json")
+    val soeknadUtenBarn: Soeknad = readFile("/soeknad_uten_barn.json")
+    val soeknadMedBarnUtenBarnepensjon: Soeknad = readFile("/soeknad_med_barn_uten_barnepensjon.json")
 }
