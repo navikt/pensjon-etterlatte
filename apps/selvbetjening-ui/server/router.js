480151a8e2bae1eed414e8fdebc73f679a6d1f7c
Glenn Wedin <glenn.wedin@nav.no>
2021-11-05 13:42:10 +0100

    config

--- apps/selvbetjening-ui/server/router.js
+++ apps/selvbetjening-ui/server/router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const parser = require("body-parser");
+const config = require("./config");
 const { generateSummary } = require("./generateSummary");
 
 const router = express.Router();
