1569d8cd55ec4c81189b0f90406a801513105f47
Emil Schrøder <emilschroder@Emils-MacBook-Pro.local>
2022-01-19 13:08:39 +0100

    Bruk web worker for nedtelling

--- apps/selvbetjening-ui/client/src/components/felles/UtloeptSession.tsx
+++ apps/selvbetjening-ui/client/src/components/felles/UtloeptSession.tsx
@@ -1,60 +1,52 @@
 import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import Nedteller from "./Nedteller";
 import LukkbarAlert from "./LukkbarAlert";
-import { hentUtløpstidForInnlogging } from "../../api/api";
-import { konverterSekunderTilTimer } from "../../utils/konverterSekunderTilTimer";
+import { hentUtloepstidForInnlogging } from "../../api/api";
+import { konverterSekunderTilTid } from "../../utils/konverterSekunderTilTid";
+import WebWorker from "../../utils/workers/WebWorker";
 
 const UtloeptSession = () => {
     const [open, setIsOpen] = useState<boolean>(false);
-    const [tidIgjen, setTidIgjen] = useState<number>(0);
-    const [nedtellerProps, setNedtellerProps] = useState({});
-    const [pause, setPause] = useState<number>();
+    const [harBlittLukket, setHarBlittLukket] = useState<boolean>(false);
+    const [tidIgjen, setTidIgjen] = useState<number>();
+    const [tid, setTid] = useState(konverterSekunderTilTid(0));
     const { t } = useTranslation();
 
     useEffect(() => {
-        hentUtløpstidForInnlogging().then((utgaarOm: string) => {
-            setTidIgjen(parseInt(utgaarOm));
+        hentUtloepstidForInnlogging().then((utgaarOm: string) => {
+            const sluttTid = new Date();
+            sluttTid.setSeconds(sluttTid.getSeconds() + parseInt(utgaarOm));
+            WebWorker.registrerNedtellingsLytter({ sluttTid: sluttTid.valueOf(), callbackFn: setTidIgjen });
         });
+
+        return () => {
+            if (WebWorker) WebWorker.fjernNedtellingsLytter();
+        };
     }, []);
 
     useEffect(() => {
-        clearTimeout(pause);
+        if (typeof tidIgjen !== "number") return;
+
         const femMinutter = 5 * 60;
-        const tidTilAlert = tidIgjen - femMinutter;
+        setTid(konverterSekunderTilTid(tidIgjen));
 
-        if (tidIgjen !== 0) {
-            if (tidIgjen < femMinutter) {
-                const props = konverterSekunderTilTimer(tidIgjen);
-                Object.assign(props, { visTimer: false });
-                setNedtellerProps(props);
-                setIsOpen(true);
-            } else {
-                const pauseId = () =>
-                    window.setTimeout(() => {
-                        setNedtellerProps({ minutter: 5, visTimer: false });
-                        setIsOpen(true);
-                    }, tidTilAlert * 1000);
-
-                setPause(pauseId);
-            }
+        if (tidIgjen <= 0) {
+            loggUt();
+        } else if (tidIgjen <= femMinutter && !harBlittLukket) {
+            setIsOpen(true);
         }
-
-        return () => {
-            if (pause) {
-                clearTimeout(pause);
-            }
-        };
     }, [tidIgjen]);
 
     const loggUt = async () => {
         try {
-            const response: string = await hentUtløpstidForInnlogging();
+            const response: string = await hentUtloepstidForInnlogging();
 
             if (parseInt(response) === 0) {
                 window.location.reload();
             } else {
-                setTidIgjen(parseInt(response));
+                const sluttTid = new Date();
+                sluttTid.setSeconds(sluttTid.getSeconds() + parseInt(response));
+                WebWorker.oppdaterSluttTid(sluttTid.valueOf());
             }
         } catch (error) {
             window.location.reload();
@@ -65,10 +57,15 @@ const UtloeptSession = () => {
         <>
             {open && (
                 <div className="utlogging-alert-wrap">
-                    <LukkbarAlert onClose={() => setIsOpen(false)}>
+                    <LukkbarAlert
+                        onClose={() => {
+                            setIsOpen(false);
+                            setHarBlittLukket(true);
+                        }}
+                    >
                         {`${t("brukerLoggesUt.info1")} `}
                         <strong>
-                            <Nedteller {...nedtellerProps} naarFerdig={loggUt} />
+                            {`${tid.minutter.toString().padStart(2, "0")}:${tid.sekunder.toString().padStart(2, "0")}`}
                         </strong>
                         {` ${t("brukerLoggesUt.tid")}.
                             ${t("brukerLoggesUt.info2")}`}
