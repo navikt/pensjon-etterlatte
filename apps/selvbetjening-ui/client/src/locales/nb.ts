e9b348d09d25284ae08c1c433e4a7cb91e4df499
Espen Schei <espen.schei@nav.no>
2022-01-24 13:32:30 +0100

    EY-382: E-post må være optional.

--- apps/selvbetjening-ui/client/src/locales/nb.ts
+++ apps/selvbetjening-ui/client/src/locales/nb.ts
@@ -113,7 +113,7 @@ export default {
   "omDeg.flyktningHvorfor": "Hvis du er registrert som flyktning hos UDI kan det avgjøre om du får gjenlevendepensjon og hvor mye du kan få.",
   "omDeg.kontaktinfo.telefonnummer": "Telefonnummer",
   "omDeg.kontaktinfo.telefonnummerPlaceholder": "8 siffer",
-  "omDeg.kontaktinfo.epost": "E-post",
+  "omDeg.kontaktinfo.epost": "E-post (valgfri)",
   "omDeg.utbetalingsInformasjon.tittel": "Oppgi bankopplysninger",
   "omDeg.utbetalingsInformasjon.bankkontoType": "Ønsker du å motta utbetalingen på norsk eller utenlandsk bankkonto?",
   "omDeg.utbetalingsInformasjon.kontonummer": "Oppgi norsk kontonummer for utbetaling",
