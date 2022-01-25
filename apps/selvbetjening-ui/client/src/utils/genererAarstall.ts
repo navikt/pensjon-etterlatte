454c802d89bbbc6cd25cb09f0c9a2bf0ddb2ebfd
Emil Schrøder <emil.schroder@nav.no>
2022-01-25 14:39:36 +0100

    Fjern ubrukt funksjon

--- apps/selvbetjening-ui/client/src/utils/genererAarstall.ts
+++
@@ -1,9 +0,0 @@
-export const genererAarstall = (startAar = 1900, sluttAar?: number) => {
-    const sluttDato = sluttAar || new Date().getFullYear();
-    const aar = [];
-    for (let i = startAar; i <= sluttDato; i++) {
-        aar.push(startAar);
-        startAar++;
-    }
-    return aar;
-};
