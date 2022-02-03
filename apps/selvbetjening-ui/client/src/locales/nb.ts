7188a4e68b851babc002e627ef045b6ec726a581
Espen Schei <espen.schei@nav.no>
2022-02-03 09:52:58 +0100

    Endring i tekst for brukere med uføretrygd eller alderspensjon. (#83)

--- apps/selvbetjening-ui/client/src/locales/nb.ts
+++ apps/selvbetjening-ui/client/src/locales/nb.ts
@@ -26,7 +26,7 @@ export default {
   "forside.hei": "Hei, {{navn}}",
   "forside.startSoeknad": "Start søknad",
   "forside.omYtelsene.innhold": "Hvis ektefellen, partneren eller samboeren din er død, kan du ha rett til gjenlevendepensjon eller overgangsstønad til gjenlevende. Du kan også ha rett til støtte hvis du tidligere har vært gift, partner eller samboer med avdøde. Vi vil vurdere om du har rett på gjenlevendepensjon eller overgangsstønad basert på din situasjon. Inntekten din avgjør hvor mye penger du kan få.",
-  "forside.omYtelsene.papirsoeknad.innhold": "Hvis du allerede får alderspensjon eller uføretrygd kan du søke om gjenlevendetillegg. Da må du søke på",
+  "forside.omYtelsene.papirsoeknad.innhold": "Får du alderspensjon eller uføretrygd fra før? Da kan du søke om gjenlevendetillegg i uføretrygd eller gjenlevenderett i alderspensjon. Du må søke på",
   "forside.omYtelsene.papirsoeknad.href": "https://www.nav.no/soknader/nb/person/stonader-ved-dodsfall/gjenlevende-ektefelle-partner-eller-samboer#NAV170105",
   "forside.omYtelsene.papirsoeknad.tekst": "papir.",
   "forside.omYtelsene.lenkeGjenlevende.href": "https://www.nav.no/gjenlevendepensjon",
