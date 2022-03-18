ba4508d4ef83da10238c57c2157fc3e610e4b283
jorgesti <jorgen.stikbakke@nav.no>
2022-03-18 14:07:14 +0100

    skudd i blinde

--- apps/etterlatte-notifikasjoner/build.gradle.kts
+++ apps/etterlatte-notifikasjoner/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation(Kafka.Clients)
+    //implementation(Kafka.Clients)
 
     implementation(Ktor.OkHttp)
     implementation(Ktor.ClientCore)
@@ -15,10 +15,15 @@ dependencies {
     implementation("com.github.navikt:brukernotifikasjon-schemas:1.2022.02.23-10.36-ad966c7d1c14")
     implementation(project(":libs:ktorclient-auth-clientcredentials"))
     implementation ("com.nfeld.jsonpathkt:jsonpathkt:2.0.0")
-    implementation("io.confluent:kafka-avro-serializer:6.2.2") {
+    implementation("io.confluent:kafka-avro-serializer:6.2.1") {
         exclude(group = "org.slf4j", module = "slf4j-log4j12")
     }
-
+    implementation("org.apache.kafka:kafka-clients"){
+        version {
+            strictly("2.8.1")
+        }
+    }
+    implementation("no.nav:kafka-embedded-env:2.8.1")
     testImplementation(Ktor.ClientMock)
     testImplementation(Kafka.EmbeddedEnv)
 }
