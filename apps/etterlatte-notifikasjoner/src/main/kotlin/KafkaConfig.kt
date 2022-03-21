1e90178bfde9b0d56482650477099e8d95804420
jorgesti <jorgen.stikbakke@nav.no>
2022-03-21 11:27:50 +0100

    test med bruk av spesific avro reader

--- apps/etterlatte-notifikasjoner/src/main/kotlin/KafkaConfig.kt
+++ apps/etterlatte-notifikasjoner/src/main/kotlin/KafkaConfig.kt
@@ -32,46 +32,28 @@ class KafkaConfig(
     private val log: Logger = LoggerFactory.getLogger(KafkaConfig::class.java)
 
     internal fun producerConfig() = Properties().apply {
-        put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
-        put(ProducerConfig.ACKS_CONFIG, acksConfig)
-        put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1")
-        put(ProducerConfig.LINGER_MS_CONFIG, "0")
-        //put(ProducerConfig.RETRIES_CONFIG, "0")
-        //put(ProducerConfig.BATCH_SIZE_CONFIG, "1")
-        put(ProducerConfig.CLIENT_ID_CONFIG, clientId) //TODO Må man ha 'producer' her?!
-        put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer::class.java)
-        put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer::class.java)
-        //put("schema.registry.url", schemaRegistryUrl)
-        put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl)
-        //put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT")
-        //put(SaslConfigs.SASL_MECHANISM, "PLAIN")
-        //put(SaslConfigs.DEFAULT_SASL_MECHANISM, "PLAIN")
-        /*put(
-            SaslConfigs.SASL_JAAS_CONFIG,
-            "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"$username\" password=\"$password\";"
-        )
-
-         */
-
+        this[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+        this[ProducerConfig.ACKS_CONFIG] = acksConfig
+        this[ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION] = "1"
+        this[ProducerConfig.LINGER_MS_CONFIG] = "0"
+        this[ProducerConfig.CLIENT_ID_CONFIG] = clientId //TODO Må man ha 'producer' her?!
+        this[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = KafkaAvroSerializer::class.java
+        this[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = KafkaAvroSerializer::class.java
+        this[AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG] = schemaRegistryUrl
+        this["specific.avro.reader"] = true
+        this[CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+        this[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = "SSL"
+        this[SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG] = ""
+        this[SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG] = trustStoreType
+        this[SslConfigs.SSL_KEYSTORE_TYPE_CONFIG] = keyStoreType
         if (!truststore.isNullOrBlank()) {
-            try {
-                this[CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
-                this[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = "SSL"
-                //this[SaslConfigs.SASL_MECHANISM] = "PLAIN"
-                this[SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG] = ""
-                this[SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG] = trustStoreType
-                this[SslConfigs.SSL_KEYSTORE_TYPE_CONFIG] = keyStoreType
-                this[SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG] = truststore
-                this[SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG] = truststorePassword
-                this[SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG] = keyStore
-                this[SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG] = keyStorePassword
-                this[SchemaRegistryClientConfig.USER_INFO_CONFIG ] = username + ":" + password
-                this[SchemaRegistryClientConfig.BASIC_AUTH_CREDENTIALS_SOURCE] = "USER_INFO"
-                //this[SslConfigs.SSL_KEY_PASSWORD_CONFIG] = keyStorePassword
-                log.info("Configured '${SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG}' location ")
-            } catch (ex: Exception) {
-                log.error("Failed to set '${SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG}' location", ex)
-            }
+            this[SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG] = truststore
+            this[SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG] = truststorePassword
+            this[SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG] = keyStore
+            this[SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG] = keyStorePassword
+            this[SchemaRegistryClientConfig.USER_INFO_CONFIG] = username + ":" + password
+            this[SchemaRegistryClientConfig.BASIC_AUTH_CREDENTIALS_SOURCE] = "USER_INFO"
+            log.info("Configured '${SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG}' location ")
         }
     }
 }
