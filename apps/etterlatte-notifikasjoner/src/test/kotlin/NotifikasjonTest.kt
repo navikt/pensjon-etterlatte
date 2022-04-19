6a32685f7451fe0c9639a8d2971679107b466da4
Espen Schei <espen.schei@nav.no>
2022-04-19 13:55:40 +0200

    EY-534: Send notifikasjon til innsender og oppdater tekst for barnepensjon

--- apps/etterlatte-notifikasjoner/src/test/kotlin/NotifikasjonTest.kt
+++ apps/etterlatte-notifikasjoner/src/test/kotlin/NotifikasjonTest.kt
@@ -5,15 +5,17 @@ import no.nav.common.JAASCredential
 import no.nav.common.KafkaEnvironment
 import no.nav.etterlatte.Notifikasjon
 import no.nav.etterlatte.SendNotifikasjon
+import no.nav.etterlatte.mapper
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import org.apache.kafka.clients.producer.MockProducer
-import org.apache.kafka.clients.producer.Producer
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import soeknad.InnsendtSoeknadFixtures
 import java.util.*
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -52,9 +54,13 @@ internal class NotifikasjonTest {
         embeddedKafkaEnvironment.withSecurity
     }
 
+    @BeforeEach
+    fun before() {
+        mockKafkaProducer.clear()
+    }
+
     @Test
     fun `Skal legge bekreftelsesmelding på køen når notifikasjon er sendt`() {
-
         val inspector = TestRapid()
             .apply {
                 Notifikasjon(
@@ -69,6 +75,7 @@ internal class NotifikasjonTest {
                             "@event_name" to "soeknad_innsendt",
                             "@dokarkivRetur" to "123456",
                             "@fnr_soeker" to "07106123912",
+                            "@skjema_info" to mapper.writeValueAsString(InnsendtSoeknadFixtures.gjenlevendepensjon()),
                             "@lagret_soeknad_id" to "4",
                             "@dokarkivRetur" to (mapOf("journalpostId" to "3"))
                         )
@@ -82,6 +89,44 @@ internal class NotifikasjonTest {
         assertEquals("3", inspector.message(0).get("@journalpostId").asText())
         assertEquals("4", inspector.message(0).get("@lagret_soeknad_id").asText())
         assertEquals("SendNotifikasjon 3", inspector.key(0))
+        assertEquals(mockKafkaProducer.history().size, 1)
+        assertEquals(mockKafkaProducer.history()[0].value().getTekst(), "Vi har mottatt søknaden din om gjenlevendepensjon")
+    }
+
+
+    @Test
+    fun `Skal opprette to notifikasjoner dersom innsender ikke er samme som søker`() {
+        val inspector = TestRapid()
+            .apply {
+                Notifikasjon(
+                    sendMelding,
+                    this
+                )
+            }
+            .apply {
+                sendTestMessage(
+                    JsonMessage.newMessage(
+                        mapOf(
+                            "@event_name" to "soeknad_innsendt",
+                            "@dokarkivRetur" to "123456",
+                            "@fnr_soeker" to "07106123912",
+                            "@skjema_info" to mapper.writeValueAsString(InnsendtSoeknadFixtures.barnepensjon()),
+                            "@lagret_soeknad_id" to "4",
+                            "@dokarkivRetur" to (mapOf("journalpostId" to "5"))
+                        )
+                    )
+                        .toJson()
+                )
+            }.inspektør
+
+        assertEquals("notifikasjon_sendt", inspector.message(0).get("@event_name").asText())
+        assertEquals("Notifikasjon sendt", inspector.message(0).get("@notifikasjon").asText())
+        assertEquals("5", inspector.message(0).get("@journalpostId").asText())
+        assertEquals("4", inspector.message(0).get("@lagret_soeknad_id").asText())
+        assertEquals("SendNotifikasjon 5", inspector.key(0))
+        assertEquals(mockKafkaProducer.history().size, 2)
+        assertEquals(mockKafkaProducer.history()[0].value().getTekst(), "Vi har mottatt søknaden din om barnepensjon")
+        assertEquals(mockKafkaProducer.history()[1].value().getTekst(), "Vi har mottatt søknaden din om barnepensjon")
     }
 
     @AfterAll
--- apps/etterlatte-notifikasjoner/src/test/kotlin/SendNotifikasjonTest.kt
+++ apps/etterlatte-notifikasjoner/src/test/kotlin/SendNotifikasjonTest.kt
@@ -8,21 +8,37 @@ import io.mockk.mockk
 import no.nav.brukernotifikasjon.schemas.input.BeskjedInput
 import no.nav.brukernotifikasjon.schemas.input.NokkelInput
 import org.apache.kafka.clients.producer.Producer
+import soeknad.InnsendtSoeknadFixtures
 
 class SendNotifikasjonTest {
-
     private val mockKafkaProducer = mockk<Producer<NokkelInput, BeskjedInput>>()
-
     private val sendNotifikasjon = SendNotifikasjon(
         mapOf(
         "BRUKERNOTIFIKASJON_BESKJED_TOPIC" to "test_topic",
     ), mockKafkaProducer)
 
     @Test
-    fun opprettMelding() {
-        val beskjed = sendNotifikasjon.opprettBeskjed()
+    fun `skal opprette melding for gjenlevendepensjon`() {
+        val beskjed = sendNotifikasjon.opprettBeskjed(InnsendtSoeknadFixtures.gjenlevendepensjon())
         assertEquals(false, beskjed.getEksternVarsling())
         assertEquals("Vi har mottatt søknaden din om gjenlevendepensjon", beskjed.getTekst())
+        assertEquals(true, isWithin10Seconds(beskjed.getTidspunkt().toLocalDateTime()))
+        assertEquals(
+            true,
+            isWithin10Seconds(
+                beskjed.getSynligFremTil().toLocalDateTime(),
+                LocalDateTime.now(ZoneOffset.UTC).plusDays(7)
+            )
+        )
+        assertEquals(4, beskjed.getSikkerhetsnivaa())
+        assertEquals(false, beskjed.getEksternVarsling())
+    }
+
+    @Test
+    fun `skal opprette melding for barnepensjon`() {
+        val beskjed = sendNotifikasjon.opprettBeskjed(InnsendtSoeknadFixtures.barnepensjon())
+        assertEquals(false, beskjed.getEksternVarsling())
+        assertEquals("Vi har mottatt søknaden din om barnepensjon", beskjed.getTekst())
         assertEquals(true, isWithin10Seconds(beskjed.getTidspunkt().toLocalDateTime()))
         assertEquals(
             true,
