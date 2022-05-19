f96e866d722cea6de5ec3b7858d9037b17cddb80
Emil Schrøder <emil.schroder@nav.no>
2022-05-19 13:40:44 +0200

    Legg til correlation id i node server

--- apps/etterlatte-node-server/src/monitoring/logger.ts
+++ apps/etterlatte-node-server/src/monitoring/logger.ts
@@ -1,5 +1,6 @@
 import { createLogger, format, transports } from 'winston'
 import { PrometheusTransport } from './transport'
+import { v4 as uuid } from 'uuid'
 
 const { Console } = transports
 const { colorize, combine, timestamp, simple, json } = format
@@ -10,7 +11,10 @@ const production = combine(timestamp(), json())
 /* Bruker simple() for lettlest logg (lokal stdout/stderr) */
 const dev = combine(colorize(), simple())
 
+const correlationId = uuid()
+
 const WinstonLogger = createLogger({
+    defaultMeta: { x_correlation_id: correlationId },
     level: 'info',
     format: !!process.env.NAIS_CLUSTER_NAME ? production : dev,
     transports: [new Console(), new PrometheusTransport()],
---
+++ libs/common/src/main/kotlin/logging/LogUtils.kt
@@ -0,0 +1,4 @@
+package logging
+
+class LogUtils {
+}
