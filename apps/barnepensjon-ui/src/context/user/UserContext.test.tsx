892fd0fe0bc295cbe1bb8e4265d35ab024b61150
Emil Schrøder <emil.schroder@nav.no>
2023-02-24 11:13:54 +0100

    Oppdater tester til å bruke react 18

--- apps/barnepensjon-ui/src/context/user/UserContext.test.tsx
+++ apps/barnepensjon-ui/src/context/user/UserContext.test.tsx
@@ -1,4 +1,5 @@
-import { act, renderHook } from '@testing-library/react-hooks'
+import { act } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react'
 import { ActionTypes, User } from './user'
 import { UserProvider, useUserContext } from './UserContext'
 import React from 'react'
