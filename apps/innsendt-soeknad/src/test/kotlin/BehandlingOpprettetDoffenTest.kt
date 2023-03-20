e8b239ee36a9ab12139b2d211461e34a91ec02cb
Øyvind Haarberg <oyvind.haarberg@gmail.com>
2023-03-20 14:18:05 +0100

    Test for BehandlingOpprettetDoffen-river

---
+++ apps/innsendt-soeknad/src/test/kotlin/BehandlingOpprettetDoffenTest.kt
@@ -0,0 +1,34 @@
+import io.kotest.assertions.any
+import io.mockk.spyk
+import io.mockk.verify
+import no.nav.etterlatte.toJson
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class BehandlingOpprettetDoffenTest {
+
+    @Test
+    fun `leser melding om opprettet behandling og registrerer dette i databasen`() {
+        val message = mapOf(
+            "@lagret_soeknad_id" to 1337,
+            "sakId" to 123,
+            "behandlingId" to UUID.randomUUID(),
+            "@event_name" to "trenger_behandling"
+        ).toJson()
+
+        val db = spyk<TestRepo>()
+        val testRapid = TestRapid().apply {
+            BehandlingOpprettetDoffen(this, db)
+        }
+
+        testRapid.sendTestMessage(message)
+        assertEquals(1, db.arkiveringOk.size)
+        assertEquals(1, db.harBehandlingDoffen.size)
+
+        verify(exactly = 1) { db.soeknadHarBehandling(any(), any(), any()) }
+        verify(exactly = 0) { db.soeknadArkivert(any()) }
+    }
+
+}
--- apps/innsendt-soeknad/src/test/kotlin/JournalpostSkrevetTest.kt
+++ apps/innsendt-soeknad/src/test/kotlin/JournalpostSkrevetTest.kt
@@ -2,17 +2,9 @@
 import io.mockk.spyk
 import io.mockk.verify
 import no.nav.etterlatte.JournalpostSkrevet
-import no.nav.etterlatte.mapper
-import no.nav.helse.rapids_rivers.JsonMessage
-import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import soeknad.LagretSoeknad
-import soeknad.SoeknadID
-import soeknad.SoeknadRepository
-import soeknad.UlagretSoeknad
-import java.util.*
 
 class JournalpostSkrevetTest {
 
@@ -81,84 +73,4 @@ class JournalpostSkrevetTest {
     }
 }
 
-fun jsonTestMessage(journalpost: Long, soeknad: Long, dokumentInfoId: Long? = null) =
-    JsonMessage("{}", MessageProblems("{}")).apply {
-        this["@dokarkivRetur"] = mapper.createObjectNode().also {
-            it.put("journalpostferdigstilt", false)
-            it.put("journalpostId", journalpost)
-            dokumentInfoId?.also { dii ->
-                it.putArray("dokumenter").addObject().put("dokumentInfoId", dii)
 
-            }
-        }
-        this["@lagret_soeknad_id"] = soeknad
-    }
-
-fun testMessage(journalpost: Long, soeknad: Long, dokumentInfoId: Long? = null) =
-    jsonTestMessage(journalpost, soeknad, dokumentInfoId).toJson()
-
-
-class TestRepo: SoeknadRepository {
-    val arkiveringOk = mutableListOf<SoeknadID>()
-    val arkiveringFeilet = mutableListOf<SoeknadID>()
-    val venterPaaBehandlingDoffen = mutableListOf<SoeknadID>()
-
-    override fun ferdigstillSoeknad(soeknad: UlagretSoeknad): SoeknadID {
-        TODO("Not yet implemented")
-    }
-
-    override fun lagreKladd(soeknad: UlagretSoeknad): LagretSoeknad {
-        TODO("Not yet implemented")
-    }
-
-    override fun soeknadSendt(id: SoeknadID) {
-        TODO("Not yet implemented")
-    }
-
-    override fun soeknadArkivert(id: SoeknadID, payload: String?) {
-        arkiveringOk += id
-    }
-
-    override fun soeknadTilDoffenArkivert(id: SoeknadID, payload: String?) {
-        arkiveringOk += id
-        venterPaaBehandlingDoffen += id
-    }
-
-    override fun soeknadHarBehandling(id: SoeknadID, sakId: Long, behandlingId: UUID) {
-        TODO("Not yet implemented")
-    }
-
-    override fun soeknadFeiletArkivering(id: SoeknadID, jsonFeil: String) {
-        arkiveringFeilet += id
-
-    }
-
-    override fun usendteSoeknader(): List<LagretSoeknad> {
-        TODO("Not yet implemented")
-    }
-
-    override fun slettArkiverteSoeknader(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun finnKladd(fnr: String, kilde: String): LagretSoeknad? {
-        TODO("Not yet implemented")
-    }
-
-    override fun slettKladd(fnr: String, kilde: String): SoeknadID? {
-        TODO("Not yet implemented")
-    }
-
-    override fun slettOgKonverterKladd(fnr: String, kilde: String): SoeknadID? {
-        TODO("Not yet implemented")
-    }
-
-    override fun slettUtgaatteKladder(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun arkiverteUtenBehandlingIDoffen(): List<LagretSoeknad> {
-        TODO("Not yet implemented")
-    }
-
-}
