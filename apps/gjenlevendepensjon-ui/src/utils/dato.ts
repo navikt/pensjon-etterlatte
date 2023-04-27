f9248f0396f8ad7127b5d02ac588dfa0c137eed5
Nicolas N <nicolas.nordhagen@nav.no>
2023-04-27 10:38:38 +0200

    Hotfix for å unngå error ved bruk av syntetiske fnr

--- apps/gjenlevendepensjon-ui/src/utils/dato.ts
+++ apps/gjenlevendepensjon-ui/src/utils/dato.ts
@@ -13,9 +13,14 @@ export const hentAlder = (foedselsdato: Date | string): number => {
 
 // Støtter også D-nummer.
 export const hentAlderFraFoedselsnummer = (foedselsnummer: string): number => {
-    const foedselsDato = navFaker.personIdentifikator.getFødselsdato(foedselsnummer)
+    try {
+        const foedselsDato = navFaker.personIdentifikator.getFødselsdato(foedselsnummer)
 
-    return hentAlder(foedselsDato)
+        return hentAlder(foedselsDato)
+    } catch (e) {
+        // navFaker støtter ikke syntetiske fnr og kaster en feil. Returnerer 0 får å omgå problemet...
+        return 0
+    }
 }
 
 export const antallAarMellom = (
