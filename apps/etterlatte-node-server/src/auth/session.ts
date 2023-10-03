9903dd4631826327383110d5e4a889b7ce1e4dcc
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-10-03 10:33:06 +0200

    Fix bad error message for pods

--- apps/etterlatte-node-server/src/auth/session.ts
+++ apps/etterlatte-node-server/src/auth/session.ts
@@ -1,22 +1,28 @@
 import { Request, RequestHandler, Response } from "express";
 import jwt from "jsonwebtoken";
+import logger from "../monitoring/logger";
 
 export default function session(): RequestHandler {
     return async (req: Request, res: Response) => {
         const { authorization } = req.headers
-        const token = authorization!!.split(' ')[1]
+        if(authorization !== undefined) {
+            const token = authorization.split(' ')[1]
 
-        if (token) {
-            const decoded = jwt.decode(token)
+            if (token) {
+                const decoded = jwt.decode(token)
 
-            if (!decoded || typeof decoded === 'string') {
-                res.sendStatus(500)
+                if (!decoded || typeof decoded === 'string') {
+                    res.sendStatus(500)
+                } else {
+                    const exp = decoded['exp'] as number
+                    res.send(`${exp * 1000}`);
+                }
             } else {
-                const exp = decoded['exp'] as number
-                res.send(`${exp * 1000}`);
+                res.sendStatus(401);
             }
         } else {
-            res.sendStatus(401);
+            logger.info('Session expired')
+            res.sendStatus(403)
         }
     }
 }
