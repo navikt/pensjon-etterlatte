93a823203bb77e1fbfc7e08118625d0306fc9e2f
Mads Opheim <mads.opheim@nav.no>
2023-10-04 17:41:34 +0200

    EY-2829: Meir oppsett for å finne toml

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,7 @@ import ca.cutterslade.gradle.analyze.AnalyzeDependenciesTask
 
 plugins {
     `kotlin-dsl`
-    id("ca.cutterslade.analyze") version "1.9.1" apply true
+    alias(libs.plugins.cutterslade.analyze) apply true
 }
 
 group = "no.nav.etterlatte"
@@ -19,6 +19,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("gradle-plugin"))
+    implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
 
 tasks {
