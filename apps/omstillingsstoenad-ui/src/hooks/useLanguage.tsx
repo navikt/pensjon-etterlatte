9b58d8176ca5903dfcf3cb5ccef9889b8ae0258a
Emil Schrøder <emil.schroder@nav.no>
2024-01-16 13:01:26 +0100

    Sett språk på dekoratøren

--- apps/omstillingsstoenad-ui/src/hooks/useLanguage.tsx
+++ apps/omstillingsstoenad-ui/src/hooks/useLanguage.tsx
@@ -1,14 +1,24 @@
 import { useEffect } from 'react'
 import i18next from '../i18n'
 import { useSoknadContext } from '../context/soknad/SoknadContext'
+import { useCookies } from 'react-cookie';
+import { setParams } from '@navikt/nav-dekoratoren-moduler';
+
+const dekoratorLanguageCookieName = 'decorator-language';
 
 export const useLanguage = () => {
     const { state: soeknadState } = useSoknadContext()
+    const [cookies, setCookie] = useCookies([dekoratorLanguageCookieName]);
+    const { [dekoratorLanguageCookieName]: dekoratørSpraak } = cookies;
+
+    !dekoratørSpraak && setCookie(dekoratorLanguageCookieName, soeknadState.spraak);
 
     useEffect(() => {
         i18next.changeLanguage(soeknadState.spraak, (err, t) => {
             if (err) return console.log('something went wrong loading', err)
             t('key')
         })
+        setParams({ language: soeknadState.spraak }).then();
+        setCookie(dekoratorLanguageCookieName, soeknadState.spraak)
     }, [soeknadState.spraak])
 }
