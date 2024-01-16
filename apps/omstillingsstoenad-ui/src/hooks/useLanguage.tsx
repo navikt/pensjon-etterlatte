1596f12b9d6f4a4609e1e9676681a503d89a4336
Emil Schrøder <emil.schroder@nav.no>
2024-01-16 14:43:02 +0100

    Bruk if og formater filen

--- apps/omstillingsstoenad-ui/src/hooks/useLanguage.tsx
+++ apps/omstillingsstoenad-ui/src/hooks/useLanguage.tsx
@@ -1,27 +1,26 @@
 import { useEffect } from 'react'
 import i18next from '../i18n'
 import { useSoknadContext } from '../context/soknad/SoknadContext'
-import { useCookies } from 'react-cookie';
-import { setParams } from '@navikt/nav-dekoratoren-moduler';
+import { useCookies } from 'react-cookie'
+import { setParams } from '@navikt/nav-dekoratoren-moduler'
 
-const dekoratorLanguageCookieName = 'decorator-language';
+const dekoratorLanguageCookieName = 'decorator-language'
 
 export const useLanguage = () => {
     const { state: soeknadState } = useSoknadContext()
-    const [cookies, setCookie] = useCookies([dekoratorLanguageCookieName]);
-    const { [dekoratorLanguageCookieName]: dekoratørSpraak } = cookies;
-
+    const [cookies, setCookie] = useCookies([dekoratorLanguageCookieName])
+    const { [dekoratorLanguageCookieName]: dekoratørSpraak } = cookies
 
     useEffect(() => {
         i18next.changeLanguage(soeknadState.spraak, (err, t) => {
             if (err) return console.log('something went wrong loading', err)
             t('key')
         })
-        setParams({ language: soeknadState.spraak }).then();
+        setParams({ language: soeknadState.spraak }).then()
         setCookie(dekoratorLanguageCookieName, soeknadState.spraak)
     }, [soeknadState.spraak])
 
     useEffect(() => {
-        !dekoratørSpraak && setCookie(dekoratorLanguageCookieName, soeknadState.spraak);
+        if (!dekoratørSpraak) setCookie(dekoratorLanguageCookieName, soeknadState.spraak)
     }, [])
 }
