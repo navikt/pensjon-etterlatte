bf8992df802f4375933fc8d907704aa0850084c2
Emil Schrøder <emil.schroder@nav.no>
2024-01-29 13:44:32 +0100

    Revert endring med hvorforspoervi

--- apps/omstillingsstoenad-ui/src/components/felles/HvorforSpoerVi.tsx
+++ apps/omstillingsstoenad-ui/src/components/felles/HvorforSpoerVi.tsx
@@ -33,7 +33,7 @@ const HvorforSpoerVi: FC<{ title: string; children: any }> = ({ title, children 
         <div>
             <ToggleButton data-testid="hvorforPanel_toggle" type={'button'} onClick={click} aria-expanded={erApen}>
                 <span>{erApen ? <Collapse /> : <Expand />}</span>
-                <span>{title ? title : t('hvorforSpoerVi')}</span>
+                <span>{t('hvorforSpoerVi')}</span>
             </ToggleButton>
 
             {erApen && <BodyLong>{children}</BodyLong>}
