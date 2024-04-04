8b4338de1846c19b1b62458ddab5263146b2010d
Nicolas N <nicolas.nordhagen@nav.no>
2024-04-04 19:34:47 +0200

    EY-3778 Slette gjenlevendepensjon-ui, workflow, readme, m.m.

--- apps/gjenlevendepensjon-ui/src/components/felles/Banner.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Banner from "./Banner";
-
-describe("Banner", () => {
-    it("should render given text", () => {
-        const { getByText } = render(<Banner tekst="Dette er test-tekst" />);
-        expect(getByText("Dette er test-tekst")).toBeInTheDocument();
-    });
-});
