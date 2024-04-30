b83ae0c3fd9829df5926ac73e833003e1de0bb86
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-04-30 10:48:54 +0200

    EY-3784 Bigip blokkerer internal

--- apps/etterlatte-node-server/src/server.ts
+++ apps/etterlatte-node-server/src/server.ts
@@ -26,7 +26,7 @@ app.set('trust proxy', 1)
 app.use(basePath, express.static(buildPath, { index: false }))
 app.use(parser.json())
 
-app.use(`${basePath}/internal/selftest`, selftestRouter)
+app.use(`${basePath}/selftest`, selftestRouter)
 
 // Endpoints to verify is app is ready/alive
 app.get(`${basePath}/isAlive|${basePath}/isReady`, (req: Request, res: Response) => {
