2840b81dad1013a588c44c6d11fe82a28d0ee29e
Mads Opheim <mads.opheim@nav.no>
2024-05-02 09:22:58 +0200

    Sett opp lisenssjekk for backend her tilsvarande den vi har i Gjenny

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+import com.github.jk1.license.render.InventoryReportRenderer
+import com.github.jk1.license.render.JsonReportRenderer
+
+plugins {
+    alias(libs.plugins.license) apply true
+}
+
+licenseReport {
+    renderers = arrayOf(JsonReportRenderer(), InventoryReportRenderer())
+}
