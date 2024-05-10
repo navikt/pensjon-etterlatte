096bf25356e28f3e640064f56b0373f93e03f6a7
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-05-10 17:47:25 +0200

    Update logging

--- apps/etterlatte-node-server/src/server.ts
+++ apps/etterlatte-node-server/src/server.ts
@@ -25,7 +25,7 @@ app.use(
 logger.info(`check new image updated basePath: ${basePath} buildPath ${buildPath} apiurl ${config.app.apiUrl}`)
 logger.info(`buildfolder exists ${fs.existsSync(buildPath)}`)
 app.use((req, res, next) => {
-    logger.info(`request: ${req}`);
+    logger.info(`request, hostname ${req.hostname} path. ${req.path}`);
     next();
 });
 app.set('trust proxy', 1)
