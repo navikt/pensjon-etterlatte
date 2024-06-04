b6faa3e091541f8e4c64b0f129d44908bb6c8c8c
Mads Opheim <mads.opheim@nav.no>
2024-06-04 12:33:59 +0200

    web-vitals 4 gjer ting på ein litt annan måte, tydelegvis

--- apps/barnepensjon-ui/src/reportWebVitals.ts
+++ apps/barnepensjon-ui/src/reportWebVitals.ts
@@ -1,13 +1,13 @@
-import { ReportCallback } from 'web-vitals'
+import { onCLS, onFCP, onLCP, onTTFB, onINP } from 'web-vitals'
 
-const reportWebVitals = (onPerfEntry?: ReportCallback) => {
+const reportWebVitals = (onPerfEntry?: any) => {
     if (onPerfEntry && onPerfEntry instanceof Function) {
-        import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-            getCLS(onPerfEntry)
-            getFID(onPerfEntry)
-            getFCP(onPerfEntry)
-            getLCP(onPerfEntry)
-            getTTFB(onPerfEntry)
+        import('web-vitals').then(() => {
+            onCLS(onPerfEntry)
+            onINP(onPerfEntry)
+            onFCP(onPerfEntry)
+            onLCP(onPerfEntry)
+            onTTFB(onPerfEntry)
         })
     }
 }
