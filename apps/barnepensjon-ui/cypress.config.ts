9b40962c6fb138b5bc823882caaae51ee8f9742e
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-06-05 12:54:29 +0200

    fix cypress tester

--- apps/barnepensjon-ui/cypress.config.ts
+++ apps/barnepensjon-ui/cypress.config.ts
@@ -4,11 +4,7 @@ export default defineConfig({
   chromeWebSecurity: false,
   video: false,
   screenshotOnRunFailure: false,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+    e2e: {
+        testIsolation: false,
     },
-  },
 })
