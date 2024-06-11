cdb0f70743a887159a77376054c5766d839686a4
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2024-06-11 14:37:16 +0200

    Tar bort import som ikkje blir brukt (#1308)

--- apps/omstillingsstoenad-ui/src/utils/translation.test.js
+++ apps/omstillingsstoenad-ui/src/utils/translation.test.js
@@ -1,5 +1,5 @@
 import { getTransKey } from './translation'
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 describe('Test translation-utils', () => {
     it('getTranskey skal fjerne firkantparentes fra errornavn', () => {
