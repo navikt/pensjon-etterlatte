b08ac50b9ff399875f4bdb1a56af9b1bfc9f1afa
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-06-27 08:31:39 +0200

    Oppjustere i personvernerklæring i BP

--- apps/barnepensjon-ui/src/components/common/ProcessingData.tsx
+++ apps/barnepensjon-ui/src/components/common/ProcessingData.tsx
@@ -17,16 +17,16 @@ export function ProcessingDataParentAndGuardian({ t, isParent }: { t: TFunction;
                 </ExpansionCard.Header>
                 <ExpansionCard.Content>
                     <FormElement>
-                        <Heading size={'xsmall'}>{t('howWeProcessDataTitle')}</Heading>
+                        <Heading size={'small'}>{t('howWeProcessDataTitle')}</Heading>
                         <BodyLong>
                             {t(isParent ? 'howWeProcessDataContentParent' : 'howWeProcessDataContentGuardian')}
                         </BodyLong>
                     </FormElement>
 
                     <FormElement>
-                        <Heading size={'xsmall'}>{t('collectAndProcessTitle')}</Heading>
+                        <Heading size={'small'}>{t('collectAndProcessTitle')}</Heading>
 
-                        <List as={'ul'} size={'small'}>
+                        <List as={'ul'}>
                             <ListItemWithIndent>
                                 {t(isParent ? 'collectAndProcess_li1_parent' : 'collectAndProcess_li1_guardian')}
                             </ListItemWithIndent>
@@ -36,10 +36,10 @@ export function ProcessingDataParentAndGuardian({ t, isParent }: { t: TFunction;
                     </FormElement>
 
                     <FormElement>
-                        <Heading size={'xsmall'}>{t('weWillRetrieveInfo')}</Heading>
+                        <Heading size={'small'}>{t('weWillRetrieveInfo')}</Heading>
                         <BodyLong>{t(isParent ? 'infoWeRetrieve_parent' : 'infoWeRetrieve_guardian')}</BodyLong>
 
-                        <List as={'ul'} size={'small'}>
+                        <List as={'ul'}>
                             <ListItemWithIndent>{t('infoWeRetrieve_li1')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('infoWeRetrieve_li2')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('infoWeRetrieve_li3')}</ListItemWithIndent>
@@ -49,7 +49,7 @@ export function ProcessingDataParentAndGuardian({ t, isParent }: { t: TFunction;
                         <BodyLong>
                             {t(isParent ? 'survivingParentInfo_parent' : 'survivingParentInfo_guardian')}
                         </BodyLong>
-                        <List as={'ul'} size={'small'}>
+                        <List as={'ul'}>
                             <ListItemWithIndent>{t('survivingParentInfo_li1')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('survivingParentInfo_li2')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('survivingParentInfo_li3')}</ListItemWithIndent>
@@ -59,17 +59,17 @@ export function ProcessingDataParentAndGuardian({ t, isParent }: { t: TFunction;
                     </FormElement>
 
                     <FormElement>
-                        <Heading size={'xsmall'}>{t('disclosureOfInformationTitle')}</Heading>
+                        <Heading size={'small'}>{t('disclosureOfInformationTitle')}</Heading>
                         <BodyLong>{t('disclosureOfInformationContent')}</BodyLong>
                     </FormElement>
 
                     <FormElement>
-                        <Heading size={'xsmall'}>{t('durationDataIsStoredTitle')}</Heading>
+                        <Heading size={'small'}>{t('durationDataIsStoredTitle')}</Heading>
                         <BodyLong>{t('durationDataIsStoredContent')}</BodyLong>
                     </FormElement>
 
                     <FormElement>
-                        <Heading size={'xsmall'}>{t('automaticProcessingTitle')}</Heading>
+                        <Heading size={'small'}>{t('automaticProcessingTitle')}</Heading>
                         <BodyLong>{t('automaticProcessingContent1')}</BodyLong>
                         <FormElement>
                             <BodyLong>
@@ -81,7 +81,7 @@ export function ProcessingDataParentAndGuardian({ t, isParent }: { t: TFunction;
                         </FormElement>
                         <BodyLong>{t('automaticProcessingContent4')}</BodyLong>
 
-                        <List as={'ul'} size={'small'}>
+                        <List as={'ul'}>
                             <ListItemWithIndent>{t('automaticProcessingContent_li1')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('automaticProcessingContent_li2')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('automaticProcessingContent_li3')}</ListItemWithIndent>
@@ -90,7 +90,7 @@ export function ProcessingDataParentAndGuardian({ t, isParent }: { t: TFunction;
                         <BodyLong>{t('automaticProcessingContent5')}</BodyLong>
                     </FormElement>
 
-                    <Heading size={'xsmall'}>{t('aboutPrivacyTitle')}</Heading>
+                    <Heading size={'small'}>{t('aboutPrivacyTitle')}</Heading>
                     <BodyLong>
                         <Trans value={t('aboutPrivacy')} />
                     </BodyLong>
@@ -111,14 +111,14 @@ export function ProcessingDataChild({ t }: { t: TFunction }) {
                 </ExpansionCard.Header>
                 <ExpansionCard.Content>
                     <FormElement>
-                        <Heading size={'xsmall'}>{t('howWeProcessDataTitle')}</Heading>
+                        <Heading size={'small'}>{t('howWeProcessDataTitle')}</Heading>
                         <BodyLong>{t('howWeProcessDataContentChild')}</BodyLong>
                     </FormElement>
 
                     <FormElement>
-                        <Heading size={'xsmall'}>{t('collectAndProcessTitle')}</Heading>
+                        <Heading size={'small'}>{t('collectAndProcessTitle')}</Heading>
 
-                        <List as={'ul'} size={'small'}>
+                        <List as={'ul'}>
                             <ListItemWithIndent>{t('collectAndProcess_li1_child')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('collectAndProcess_li2_child')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('collectAndProcess_li3')}</ListItemWithIndent>
@@ -126,10 +126,10 @@ export function ProcessingDataChild({ t }: { t: TFunction }) {
                     </FormElement>
 
                     <FormElement>
-                        <Heading size={'xsmall'}>{t('weWillRetrieveInfo')}</Heading>
+                        <Heading size={'small'}>{t('weWillRetrieveInfo')}</Heading>
                         <BodyLong>{t('infoWeRetrieve_child')}</BodyLong>
 
-                        <List as={'ul'} size={'small'}>
+                        <List as={'ul'}>
                             <ListItemWithIndent>{t('infoWeRetrieve_li1')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('infoWeRetrieve_li2')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('infoWeRetrieve_li3')}</ListItemWithIndent>
@@ -137,7 +137,7 @@ export function ProcessingDataChild({ t }: { t: TFunction }) {
                         </List>
 
                         <BodyLong>{t('survivingParentInfo_child')}</BodyLong>
-                        <List as={'ul'} size={'small'}>
+                        <List as={'ul'}>
                             <ListItemWithIndent>{t('survivingParentInfo_li1')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('survivingParentInfo_li2')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('survivingParentInfo_li3')}</ListItemWithIndent>
@@ -147,17 +147,17 @@ export function ProcessingDataChild({ t }: { t: TFunction }) {
                     </FormElement>
 
                     <FormElement>
-                        <Heading size={'xsmall'}>{t('disclosureOfInformationTitle_child')}</Heading>
+                        <Heading size={'small'}>{t('disclosureOfInformationTitle_child')}</Heading>
                         <BodyLong>{t('disclosureOfInformationContent')}</BodyLong>
                     </FormElement>
 
                     <FormElement>
-                        <Heading size={'xsmall'}>{t('durationDataIsStoredTitle')}</Heading>
+                        <Heading size={'small'}>{t('durationDataIsStoredTitle')}</Heading>
                         <BodyLong>{t('durationDataIsStoredContent')}</BodyLong>
                     </FormElement>
 
                     <FormElement>
-                        <Heading size={'xsmall'}>{t('automaticProcessingTitle')}</Heading>
+                        <Heading size={'small'}>{t('automaticProcessingTitle')}</Heading>
                         <BodyLong>{t('automaticProcessingContent1_child')}</BodyLong>
                         <FormElement>
                             <BodyLong>
@@ -169,7 +169,7 @@ export function ProcessingDataChild({ t }: { t: TFunction }) {
                         </FormElement>
                         <BodyLong>{t('automaticProcessingContent4')}</BodyLong>
 
-                        <List as={'ul'} size={'small'}>
+                        <List as={'ul'}>
                             <ListItemWithIndent>{t('automaticProcessingContent_li1')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('automaticProcessingContent_li2')}</ListItemWithIndent>
                             <ListItemWithIndent>{t('automaticProcessingContent_li3')}</ListItemWithIndent>
@@ -178,7 +178,7 @@ export function ProcessingDataChild({ t }: { t: TFunction }) {
                         <BodyLong>{t('automaticProcessingContent5')}</BodyLong>
                     </FormElement>
 
-                    <Heading size={'xsmall'}>{t('aboutPrivacyTitle')}</Heading>
+                    <Heading size={'small'}>{t('aboutPrivacyTitle')}</Heading>
                     <BodyLong>
                         <Trans value={t('aboutPrivacy_child')} />
                     </BodyLong>
