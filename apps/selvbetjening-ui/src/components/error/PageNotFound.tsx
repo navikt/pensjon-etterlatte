4a0e9dc5384e4d80b298f8d3d04a32e83ec6ce18
Bjorn Golberg <bjorn.golberg@gmail.com>
2024-06-27 08:07:37 +0200

    Fjerner ubrukte komponenter som ikke er aksel og ubrukte dependencies

--- apps/selvbetjening-ui/src/components/error/PageNotFound.tsx
+++ apps/selvbetjening-ui/src/components/error/PageNotFound.tsx
@@ -1,9 +1,7 @@
-import FormGroup from '../common/FormGroup'
 import { BodyShort, Button, Heading, Link } from '@navikt/ds-react'
 import useTranslation from '../../hooks/useTranslation'
 import { useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
-import FormElement from '../common/FormElement'
 import { BugIcon } from '@navikt/aksel-icons'
 
 export default function PageNotFound() {
@@ -21,35 +19,7 @@ export default function PageNotFound() {
 
     return (
         <div>
-            <FormGroup>
-                <FormElement>
-                    <Heading size={'medium'}>{t('title')}</Heading>
-                </FormElement>
-                <FormElement>
-                    <BodyShort>{t('description')}</BodyShort>
-                </FormElement>
-                <FormGroup>
-                    <Button as={'a'} href={t('backToFrontpage.href')}>
-                        {t('backToFrontpage')}
-                    </Button>
-                </FormGroup>
-                <Link href={t('errorInLink.href')}>
-                    <BugIcon fontSize={20} aria-hidden={true} />
-                    {t('errorInLink')}
-                </Link>
-            </FormGroup>
-            <FormElement>
-                <Heading size={'medium'}>{t('title.english')}</Heading>
-            </FormElement>
-            <FormElement>
-                <BodyShort>
-                    {t('description.english.part1')}
-                    <Link href={t('backToFrontpage.href')} inlineText>
-                        {t('backToFrontpage.english')}
-                    </Link>
-                    {t('description.english.part2')}
-                </BodyShort>
-            </FormElement>
+            Not found!
         </div>
     )
 }
