d5cb3a3987b28fa6151709441ba4fe06729835c7
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2024-08-08 15:43:56 +0200

    Ny sourcemappath

--- apps/etterlatte-node-server/src/routers/loggerRouter.ts
+++ apps/etterlatte-node-server/src/routers/loggerRouter.ts
@@ -18,10 +18,13 @@ function isStackInfoValid(numbers: IStackLineNoColumnNo): boolean {
     return numbers.lineno > 0 && numbers.columnno >= 0
 }
 
-const sourcemapLocation = '/app/build/assets'
+const sourcemapLocation = '/app/etterlatte-node-server/build/assets'
 
 async function sourceMapMapper(numbers: IStackLineNoColumnNo): Promise<NullableMappedPosition> {
     const sourcemapFile = fs.readdirSync(sourcemapLocation).find((file) => file.includes('.map')) ?? ''
+    frontendLogger.error({
+        message: `Fant fil: ${sourcemapFile}`
+    })
     const rawSourceMap = fs.readFileSync(`${sourcemapLocation}/${sourcemapFile}`).toString()
     const smc = await new sourceMap.SourceMapConsumer(rawSourceMap)
     return Promise.resolve(smc.originalPositionFor({ line: numbers.lineno, column: numbers.columnno }))
@@ -31,7 +34,6 @@ loggerRouter.post('/', express.json(), (req, res) => {
     const logEvent = req.body as LogEvent
     const errorData = logEvent.data
 
-    frontendLogger.info('Frontendlogging: test')
     if (logEvent.type && logEvent.type === 'info') {
         frontendLogger.info('Frontendlogging: ', JSON.stringify(logEvent))
     } else {
