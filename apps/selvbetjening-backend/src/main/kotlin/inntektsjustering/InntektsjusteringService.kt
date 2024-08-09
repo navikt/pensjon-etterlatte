32514fc0b27f645841d3183746c944f1e7945d15
Bjorn Golberg <bjorn.golberg@gmail.com>
2024-08-09 14:51:45 +0200

    tester sende melding

--- apps/selvbetjening-backend/src/main/kotlin/inntektsjustering/InntektsjusteringService.kt
+++ apps/selvbetjening-backend/src/main/kotlin/inntektsjustering/InntektsjusteringService.kt
@@ -2,6 +2,8 @@ package no.nav.etterlatte.inntektsjustering
 
 import no.nav.etterlatte.kafka.KafkaProdusent
 import no.nav.etterlatte.libs.common.person.Foedselsnummer
+import no.nav.helse.rapids_rivers.JsonMessage
+import java.util.UUID
 
 class InntektsjusteringService(
 	val inntektsjusteringRepository: InntektsjusteringRepository,
@@ -14,6 +16,15 @@ class InntektsjusteringService(
 
 	fun lagreInntektsjustering(fnr: Foedselsnummer, request: InntektsjusteringLagre) {
 		inntektsjusteringRepository.lagreInntektsjustering(fnr, request)
+		val message =
+			JsonMessage.newMessage(
+				mapOf(
+					"@event_name" to "inntektsjustering",
+					"aar" to "2025"
+				)
+			)
+
+		produsent.publiser(UUID.randomUUID().toString(), message.toJson())
 	}
 
 
