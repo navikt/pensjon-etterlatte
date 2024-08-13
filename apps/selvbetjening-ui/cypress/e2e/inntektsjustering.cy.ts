6e0f5c2f0e19ca35e20f4d156214bea3b2e77bce
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-08-13 12:29:51 +0200

    Skeleton for test

---
+++ apps/selvbetjening-ui/cypress/e2e/inntektsjustering.cy.ts
@@ -0,0 +1,3 @@
+describe('tester for inntektsjustering', () => {
+    it('skal ikke vise inntektsjustering når ingen er opprettet', () => {})
+})
--- apps/selvbetjening-ui/cypress/e2e/landing.cy.ts
+++
@@ -1,3 +0,0 @@
-describe('test landing', () => {
-    it('should countain something', () => {})
-})
