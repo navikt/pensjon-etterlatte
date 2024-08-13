088d526cae467e4ba10167d6aa176daa12c88b5b
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-08-13 08:34:48 +0200

    Legg inn typer for cypress i tsconfig

--- apps/selvbetjening-ui/cypress.config.ts
+++ apps/selvbetjening-ui/cypress.config.ts
@@ -6,8 +6,5 @@ export default defineConfig({
     screenshotOnRunFailure: false,
     e2e: {
         testIsolation: false,
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
     },
 })
