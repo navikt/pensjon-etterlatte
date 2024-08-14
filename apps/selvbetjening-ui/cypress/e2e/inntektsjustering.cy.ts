643f8450c69183037c68b6309dff32b18295875d
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-08-14 15:11:24 +0200

    Legg til cypress test for inntekstjustering

--- apps/selvbetjening-ui/cypress/e2e/inntektsjustering.cy.ts
+++ apps/selvbetjening-ui/cypress/e2e/inntektsjustering.cy.ts
@@ -1,6 +1,12 @@
-describe('tester for inntektsjustering', () => {
-    it('skal ikke vise inntektsjustering når ingen er opprettet', () => {
+describe('landingsside for inntetsjustering', () => {
+    it('viser informasjonen til innloged bruker', () => {
         cy.visit('http://localhost:5173/selvbetjening/inntektsjustering')
-        cy.findByText('Fant ingen inntektsjustering')
+        cy.findByText('STOR SNERK')
+        cy.findByText('GIFT')
+        cy.findByText('11057523044')
+        cy.findByText('Norge')
+        cy.findByText('Humlegården 1')
+        cy.findByText('0000 Humla')
+        cy.findByText('+47 11111111')
     })
 })
