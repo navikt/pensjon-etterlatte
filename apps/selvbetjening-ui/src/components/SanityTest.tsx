8e3b1442def041f8a8c7dec89aafcb0782bee9e1
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-08-19 13:47:02 +0200

    Opprydning i setup for rik tekst fra sanity

--- apps/selvbetjening-ui/src/components/SanityTest.tsx
+++ apps/selvbetjening-ui/src/components/SanityTest.tsx
@@ -1,85 +1,13 @@
 import { useEffect, useState } from 'react'
-import { BodyShort, Heading, Link, List, Select } from '@navikt/ds-react'
-import { PortableText, PortableTextComponents } from '@portabletext/react'
+import { Select } from '@navikt/ds-react'
 import styled from 'styled-components'
 import { fetchSanity } from '../locales/Sanity.ts'
+import { SanityRikTekst } from '../common/sanity/SanityRikTekst.tsx'
 
 export enum Language {
     BOKMAAL = 'NB',
     NYNORSK = 'NN',
     ENGELSK = 'EN',
-}
-
-const components: PortableTextComponents = {
-    marks: {
-        p: ({ children }) => <BodyShort>{children}</BodyShort>,
-        b: ({ children }) => (
-            <BodyShort>
-                <b>{children}</b>
-            </BodyShort>
-        ),
-        em: ({ children }) => (
-            <BodyShort>
-                <em>{children}</em>
-            </BodyShort>
-        ),
-        i: ({ children }) => (
-            <BodyShort>
-                <i>{children}</i>
-            </BodyShort>
-        ),
-        u: ({ children }) => (
-            <BodyShort>
-                <u>{children}</u>
-            </BodyShort>
-        ),
-        del: ({ children }) => (
-            <BodyShort>
-                <del>{children}</del>
-            </BodyShort>
-        ),
-        a: ({ children, value }) => (
-            <Link href={value.href} target="_blank" rel="noopener noreferrer">
-                {children}
-            </Link>
-        ),
-    },
-    block: {
-        h1: ({ children }) => (
-            <Heading size="xlarge" level="1">
-                {children}
-            </Heading>
-        ),
-        h2: ({ children }) => (
-            <Heading size="large" level="2">
-                {children}
-            </Heading>
-        ),
-        h3: ({ children }) => (
-            <Heading size="medium" level="3">
-                {children}
-            </Heading>
-        ),
-        h4: ({ children }) => (
-            <Heading size="small" level="4">
-                {children}
-            </Heading>
-        ),
-        h5: ({ children }) => (
-            <Heading size="xsmall" level="5">
-                {children}
-            </Heading>
-        ),
-        h6: ({ children }) => <h6>{children}</h6>,
-    },
-    list: {
-        bullet: ({ children }) => <List as="ul">{children}</List>,
-        number: ({ children }) => <List as="ol">{children}</List>,
-    },
-    listItem: {
-        bullet: ({ children }) => <List.Item>{children}</List.Item>,
-        number: ({ children }) => <List.Item>{children}</List.Item>,
-    },
 }
 
 export default function SanityTest() {
@@ -104,7 +32,7 @@ export default function SanityTest() {
                     </Select>
                 </SelectWrapper>
             </div>
-            <div>{text && <PortableText value={text} components={components} />}</div>
+            <div>{text && <SanityRikTekst text={text} />}</div>
         </div>
     )
 }
