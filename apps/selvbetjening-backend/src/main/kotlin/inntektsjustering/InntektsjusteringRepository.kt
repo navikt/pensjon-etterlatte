be5577469bc5166feada0001d6b2e51a340d555a
Bjørn Golberg <bjorn.golberg.tools@gmail.com>
2024-08-23 16:21:17 +0200

    Inntektsjustering bruk felles dto (#1418)
    Co-authored-by: Bjorn Golberg <bjorn.golberg@gmail.com>

--- apps/selvbetjening-backend/src/main/kotlin/inntektsjustering/InntektsjusteringRepository.kt
+++ apps/selvbetjening-backend/src/main/kotlin/inntektsjustering/InntektsjusteringRepository.kt
@@ -2,68 +2,61 @@ package no.nav.etterlatte.inntektsjustering
 
 import no.nav.etterlatte.inntektsjustering.Queries.HENT
 import no.nav.etterlatte.inntektsjustering.Queries.LAGRE
+import no.nav.etterlatte.libs.common.inntektsjustering.Inntektsjustering
 import no.nav.etterlatte.libs.common.person.Foedselsnummer
 import no.nav.etterlatte.libs.utils.database.firstOrNull
-import no.nav.etterlatte.libs.utils.database.singleOrNull
-import java.sql.Timestamp
-import java.time.LocalDateTime
 import java.time.ZoneId
-import javax.sql.DataSource
 import java.util.UUID
+import javax.sql.DataSource
 
 class InntektsjusteringRepository(
-	private val ds: DataSource
+    private val ds: DataSource
 ) {
 
-	private val connection get() = ds.connection
+    private val connection get() = ds.connection
 
-	private val postgresTimeZone = ZoneId.of("UTC")
+    private val postgresTimeZone = ZoneId.of("UTC")
 
-	fun hentInntektsjustering(fnr: Foedselsnummer) = connection.use {
-		it
-			.prepareStatement(HENT)
-			.apply {
-				setString(1, fnr.value)
-			}.executeQuery()
-			.firstOrNull{
-				Inntektsjustering(
-					id = UUID.fromString(getString("id")),
-					arbeidsinntekt = getInt("arbeidsinntekt"),
-					naeringsinntekt = getInt("naeringsinntekt"),
-					arbeidsinntektUtland = getInt("arbeidsinntekt_utland"),
-					naeringsinntektUtland = getInt("naeringsinntekt_utland"),
-					tidspunkt = getTimestamp("innsendt").asLocalDateTime()
-				)
-			}
-	}
+    fun hentInntektsjustering(fnr: Foedselsnummer) = connection.use {
+        it
+            .prepareStatement(HENT)
+            .apply {
+                setString(1, fnr.value)
+            }.executeQuery()
+            .firstOrNull {
+                Inntektsjustering(
+                    id = UUID.fromString(getString("id")),
+                    arbeidsinntekt = getInt("arbeidsinntekt"),
+                    naeringsinntekt = getInt("naeringsinntekt"),
+                    arbeidsinntektUtland = getInt("arbeidsinntekt_utland"),
+                    naeringsinntektUtland = getInt("naeringsinntekt_utland"),
+                    tidspunkt = getTimestamp("innsendt").toInstant()
+                )
+            }
+    }
 
-	fun lagreInntektsjustering(fnr: Foedselsnummer, inntektsjustering: InntektsjusteringLagre) = connection.use {
-		it.prepareStatement(LAGRE)
-			.apply {
-				setObject(1, inntektsjustering.id)
-				setString(2, fnr.value)
-				setInt(3, inntektsjustering.arbeidsinntekt)
-				setInt(4, inntektsjustering.naeringsinntekt)
-				setInt(5, inntektsjustering.arbeidsinntektUtland)
-				setInt(6, inntektsjustering.naeringsinntektUtland)
-			}.execute()
-	}
+    fun lagreInntektsjustering(fnr: Foedselsnummer, inntektsjustering: InntektsjusteringLagre) = connection.use {
+        it.prepareStatement(LAGRE)
+            .apply {
+                setObject(1, inntektsjustering.id)
+                setString(2, fnr.value)
+                setInt(3, inntektsjustering.arbeidsinntekt)
+                setInt(4, inntektsjustering.naeringsinntekt)
+                setInt(5, inntektsjustering.arbeidsinntektUtland)
+                setInt(6, inntektsjustering.naeringsinntektUtland)
+            }.execute()
+    }
 
-	private fun Timestamp.asLocalDateTime(): LocalDateTime =
-		toLocalDateTime()
-			.atZone(postgresTimeZone)
-			.withZoneSameInstant(ZoneId.systemDefault())
-			.toLocalDateTime()
 }
 
 private object Queries {
-	val HENT = """
+    val HENT = """
 		SELECT * FROM inntektsjustering
 		WHERE fnr = ?
 		ORDER BY innsendt DESC
 		""".trimIndent()
 
-	val LAGRE = """
+    val LAGRE = """
 		INSERT INTO inntektsjustering (id, fnr, arbeidsinntekt, naeringsinntekt, arbeidsinntekt_utland, naeringsinntekt_utland)
 		VALUES (?, ?, ?, ?, ?, ?)
 	""".trimIndent()
