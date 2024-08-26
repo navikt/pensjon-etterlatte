c7b410c2f989fe2b2a379ae8a7e6b76c256c5c97
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2024-08-26 11:36:28 +0200

    Kommenter ut personopplysninger (#1419)
    * Kommenter ut personopplysninger
    
    * Kommenter ut tester

--- apps/selvbetjening-ui/src/inntektsjustering/Inntektsjustering.tsx
+++ apps/selvbetjening-ui/src/inntektsjustering/Inntektsjustering.tsx
@@ -1,12 +1,11 @@
 import { ReactNode } from 'react'
 import { Heading, HStack, LinkPanel, VStack } from '@navikt/ds-react'
-import { InformasjonOmInnloggetBruker } from './components/InformasjonOmInnloggetBruker.tsx'
 
 export const Inntektsjustering = (): ReactNode => {
     return (
         <HStack justify="center">
             <VStack gap="8" align="start" paddingBlock="8" paddingInline="4">
-                <InformasjonOmInnloggetBruker />
+                {/*<InformasjonOmInnloggetBruker />*/}
                 <LinkPanel href={`${import.meta.env.BASE_URL}/inntektsjustering/opprett`} border>
                     <Heading size="small">Legg inn inntekt til neste år</Heading>
                 </LinkPanel>
