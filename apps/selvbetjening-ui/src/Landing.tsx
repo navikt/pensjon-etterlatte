a22909b934571bbdaf801319a241135d897fe4fc
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-09-11 13:27:08 +0200

    I forgor

--- apps/selvbetjening-ui/src/Landing.tsx
+++ apps/selvbetjening-ui/src/Landing.tsx
@@ -1,6 +1,5 @@
 import { Button, Heading, VStack } from '@navikt/ds-react'
 import { useNavigate } from 'react-router-dom'
-import SanityTest from './components/SanityTest.tsx'
 
 export const Landing = () => {
     const navigate = useNavigate()
@@ -8,7 +7,6 @@ export const Landing = () => {
     return (
         <VStack gap="4" align="center">
             <Heading size="xlarge">Velkommen til selvbetjening!</Heading>
-            <SanityTest />
             <div>
                 <Button onClick={() => navigate('/inntektsjustering')}>Til inntektsjustering</Button>
             </div>
