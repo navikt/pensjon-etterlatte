040b77fb2556ecbf9c09c3528b3ac63681207f5b
Mads Opheim <mads.opheim@nav.no>
2024-09-11 13:34:03 +0200

    Samlar oppsettet for testkøyring, omtrent tilsvarande som i Gjenny

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import com.github.jk1.license.render.InventoryReportRenderer
 import com.github.jk1.license.render.JsonReportRenderer
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     alias(libs.plugins.license) apply true
@@ -8,4 +9,26 @@ plugins {
 licenseReport {
     renderers = arrayOf(JsonReportRenderer(), InventoryReportRenderer())
     allowedLicensesFile = File("$projectDir/buildSrc/akseptable-lisenser.json")
+}
+
+subprojects.forEach { sub ->
+    sub.repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/navikt/pensjon-etterlatte-libs")
+            credentials {
+                username = "token"
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    sub.tasks {
+        withType<Test> {
+            useJUnitPlatform()
+            testLogging {
+                events(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+                exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+            }
+        }
+    }
 }
