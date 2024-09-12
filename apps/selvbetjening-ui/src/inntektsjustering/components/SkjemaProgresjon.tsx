4e4937387304da443d7508a9eac2cebe96c1b8b4
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-09-12 14:27:28 +0200

    Bruke språk context for skjema progresjon

--- apps/selvbetjening-ui/src/inntektsjustering/components/SkjemaProgresjon.tsx
+++ apps/selvbetjening-ui/src/inntektsjustering/components/SkjemaProgresjon.tsx
@@ -2,9 +2,11 @@ import { FormProgress } from '@navikt/ds-react'
 import useSWR, { SWRResponse } from 'swr'
 import { apiURL } from '../../utils/api.ts'
 import { Navigate } from 'react-router-dom'
-import { Spraak } from '../../common/spraak/spraak.ts'
+import { useSpraak } from '../../common/spraak/SpraakContext.tsx'
 
-export const SkjemaProgresjon = ({ aktivtSteg, valgtSpraak }: { aktivtSteg: number; valgtSpraak: Spraak }) => {
+export const SkjemaProgresjon = ({ aktivtSteg }: { aktivtSteg: number }) => {
+    const spraak = useSpraak()
+
     const { data, error }: SWRResponse<never[], boolean, boolean> = useSWR(
         `${apiURL}/sanity?` + new URLSearchParams('sanityQuery=*[_type == "fellesKomponenter"].skjemaProgresjon')
     )
@@ -19,15 +21,15 @@ export const SkjemaProgresjon = ({ aktivtSteg, valgtSpraak }: { aktivtSteg: numb
                 totalSteps={4}
                 activeStep={aktivtSteg}
                 translations={{
-                    step: `${data[0]['stegXAvX']['steg'][valgtSpraak]} ${aktivtSteg} ${data[0]['stegXAvX']['av'][valgtSpraak]} ${4}`,
-                    showAllSteps: data[0]['visAlleSteg'][valgtSpraak],
-                    hideAllSteps: data[0]['skjulAlleSteg'][valgtSpraak],
+                    step: `${data[0]['stegXAvX']['steg'][spraak]} ${aktivtSteg} ${data[0]['stegXAvX']['av'][spraak]} ${4}`,
+                    showAllSteps: data[0]['visAlleSteg'][spraak],
+                    hideAllSteps: data[0]['skjulAlleSteg'][spraak],
                 }}
             >
-                <FormProgress.Step interactive={false}>{data[0]['stegLabels']['steg1'][valgtSpraak]}</FormProgress.Step>
-                <FormProgress.Step interactive={false}>{data[0]['stegLabels']['steg2'][valgtSpraak]}</FormProgress.Step>
-                <FormProgress.Step interactive={false}>{data[0]['stegLabels']['steg3'][valgtSpraak]}</FormProgress.Step>
-                <FormProgress.Step interactive={false}>{data[0]['stegLabels']['steg4'][valgtSpraak]}</FormProgress.Step>
+                <FormProgress.Step interactive={false}>{data[0]['stegLabels']['steg1'][spraak]}</FormProgress.Step>
+                <FormProgress.Step interactive={false}>{data[0]['stegLabels']['steg2'][spraak]}</FormProgress.Step>
+                <FormProgress.Step interactive={false}>{data[0]['stegLabels']['steg3'][spraak]}</FormProgress.Step>
+                <FormProgress.Step interactive={false}>{data[0]['stegLabels']['steg4'][spraak]}</FormProgress.Step>
             </FormProgress>
         )
     )
