fb6a8fe6b1ee29eb69aca8405ebd7508eef54915
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-09-13 10:32:01 +0200

    Oppdatere koden for å bruke de nye typene

--- apps/selvbetjening-ui/src/common/spraak/SpraakVelger.tsx
+++ apps/selvbetjening-ui/src/common/spraak/SpraakVelger.tsx
@@ -3,13 +3,14 @@ import { Select } from '@navikt/ds-react'
 import { Navigate } from 'react-router-dom'
 import { useSpraak, useSpraakDispatch } from './SpraakContext.tsx'
 import { useSanityInnhold } from '../sanity/useSanityInnhold.ts'
+import { FellesKomponenter } from '../../sanity.types.ts'
 
 export const SpraakVelger = () => {
     const spraak = useSpraak()
 
     const spraakDispatch = useSpraakDispatch()
 
-    const { innhold, error, isLoading } = useSanityInnhold<never>('*[_type == "fellesKomponenter"].spraakVelger')
+    const { innhold, error, isLoading } = useSanityInnhold<FellesKomponenter>('*[_type == "fellesKomponenter"]')
 
     if (error && !isLoading) {
         return <Navigate to="/system-utilgjengelig" />
@@ -18,7 +19,7 @@ export const SpraakVelger = () => {
     return (
         !!innhold && (
             <Select
-                label={innhold['label'][spraak]}
+                label={innhold.spraakVelger?.label?.[spraak]}
                 value={spraak}
                 onChange={(e) => spraakDispatch.setSpraak(e.target.value as Spraak)}
             >
