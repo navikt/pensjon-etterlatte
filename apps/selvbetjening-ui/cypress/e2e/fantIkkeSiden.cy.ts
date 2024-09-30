ce7eecbe64ba24838900569f09aabd373945e144
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-09-30 15:24:16 +0200

    Endring på rekkefølge i side ikke funnet tester

--- apps/selvbetjening-ui/cypress/e2e/fantIkkeSiden.cy.ts
+++ apps/selvbetjening-ui/cypress/e2e/fantIkkeSiden.cy.ts
@@ -1,12 +1,16 @@
 import { testAccesibility } from '../support/accessibility.ts'
 
 describe('"Side ikke funnet" skal fungere som forventet', () => {
-    it('Skal vise "side ikke funnet" ved route som ikke eksisterer', () => {
+    beforeEach(() => {
         cy.visit('http://localhost:5173/selvbetjening/inntektjustering/hei-dette-er-meg-route')
+    })
 
-        cy.findByText('Beklager, vi fant ikke siden').should('exist')
-
+    it('Skal ikke ha noen UU feil', () => {
         testAccesibility()
+    })
+
+    it('Skal vise "side ikke funnet" ved route som ikke eksisterer', () => {
+        cy.findByText('Beklager, vi fant ikke siden').should('exist')
     })
 
     it('Skal ikke navigere til "side ikke funnet" hvis routen faktisk finnes', () => {
@@ -16,8 +20,6 @@ describe('"Side ikke funnet" skal fungere som forventet', () => {
     })
 
     it('Skal kunne navigere seg til nav.no hvis siden ikke finnes', () => {
-        cy.visit('http://localhost:5173/selvbetjening/inntektjustering/hei-dette-er-meg-route')
-
         cy.findByRole('link', { name: 'Gå til forsiden' }).click()
 
         cy.url().should('eq', 'https://www.nav.no/')
