11e766684bfc45e151de82d9275ee97a1351335b
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-10-02 11:46:18 +0200

    Fiks UU feil for inntekt neste år

--- apps/selvbetjening-ui/cypress/support/accessibility.ts
+++ apps/selvbetjening-ui/cypress/support/accessibility.ts
@@ -5,8 +5,7 @@ type CypressAxeContext = string | Node | ContextObject | undefined
 
 export const testAccesibility = (context?: CypressAxeContext, options?: Options) => {
     // Her må vi vente litt slik at dataen fra sanity klarer å loade og mountes i DOM'en før UU testene kjøres
-    setTimeout(() => {
-        cy.injectAxe()
-        cy.checkA11y(context, options)
-    }, 1000)
+
+    cy.injectAxe()
+    cy.checkA11y(context, options)
 }
