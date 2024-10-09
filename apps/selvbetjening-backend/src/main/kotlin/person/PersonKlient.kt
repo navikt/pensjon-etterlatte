294bdca4adceaf987bdb38faa2f8a305ca5b5db6
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-10-09 12:03:04 +0200

    Start arbeidet for pdlKlient

--- apps/selvbetjening-backend/src/main/kotlin/person/PersonKlient.kt
+++ apps/selvbetjening-backend/src/main/kotlin/person/PersonKlient.kt
@@ -1,2 +1,18 @@
 package no.nav.etterlatte.person
 
+import io.ktor.client.HttpClient
+import no.nav.etterlatte.libs.common.person.Foedselsnummer
+import org.slf4j.LoggerFactory
+
+class PersonKlient(
+    private val httpClient: HttpClient,
+) {
+    private val logger = LoggerFactory.getLogger(PersonKlient::class.java)
+
+    companion object {
+        private const val TEMA = "PEN"
+        private const val BEHANDLINGSNUMMER = "behandlingsnummer"
+    }
+
+    suspend fun hentPerson(fnr: Foedselsnummer)
+}
