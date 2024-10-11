55321ded6b03a2b1e764a34c36caa5836cfa8d3e
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-10-11 10:38:45 +0200

    Annen route

--- apps/selvbetjening-backend/src/main/kotlin/person/PersonRoute.kt
+++ apps/selvbetjening-backend/src/main/kotlin/person/PersonRoute.kt
@@ -8,7 +8,7 @@ import io.ktor.server.routing.route
 import no.nav.etterlatte.fnrFromToken
 
 fun Route.person(service: PersonService) {
-    route("/api/person/innlogget") {
+    route("/api/person/innlogget/forenklet") {
         get {
             val fnr = fnrFromToken()
 
