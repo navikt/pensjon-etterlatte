61b6e190a21e0f0dea30fea5f0757815b28c1838
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-10-14 15:57:20 +0200

    Legge til radio gruppe for om man skal gå av med alderspensjon

--- apps/selvbetjening-ui/src/types/inntektsjustering.ts
+++ apps/selvbetjening-ui/src/types/inntektsjustering.ts
@@ -1,11 +1,20 @@
 export interface Inntekt {
+    skalGaaAvMedAlderspensjon?: SkalGaaAvMedAlderspensjon
     arbeidsinntekt: number
     naeringsinntekt: number
     inntektFraUtland: number
     AFPInntekt: number
     AFPTjenesteordning?: string
 }
+
+export enum SkalGaaAvMedAlderspensjon {
+    JA,
+    NEI,
+    VET_IKKE,
+}
+
 export const inntektDefaultValues: Inntekt = {
+    skalGaaAvMedAlderspensjon: undefined,
     arbeidsinntekt: 0,
     naeringsinntekt: 0,
     inntektFraUtland: 0,
