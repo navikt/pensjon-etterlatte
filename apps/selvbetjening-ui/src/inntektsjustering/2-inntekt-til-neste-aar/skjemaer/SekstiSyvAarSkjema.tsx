5e6f5835bec83dc524996194e43c5878bfd87b8e
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2024-10-18 13:11:37 +0200

    Update apps/selvbetjening-ui/src/inntektsjustering/2-inntekt-til-neste-aar/skjemaer/SekstiSyvAarSkjema.tsx
    Co-authored-by: Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>

--- apps/selvbetjening-ui/src/inntektsjustering/2-inntekt-til-neste-aar/skjemaer/SekstiSyvAarSkjema.tsx
+++ apps/selvbetjening-ui/src/inntektsjustering/2-inntekt-til-neste-aar/skjemaer/SekstiSyvAarSkjema.tsx
@@ -66,8 +66,9 @@ export const SekstiSyvAarSkjema = () => {
         sumAvInntekt,
     } = innhold.inntektSkjemaer.sekstiSyvAarSkjema
 
-    const foersteDagNesteAar = new Date(new Date().setFullYear(new Date().getFullYear() + 1, 0, 1))
-    const sisteDagNesteAar = new Date(new Date().setFullYear(new Date().getFullYear() + 1, 11, 31))
+const nesteAar = new Date().getFullYear() + 1
+const foersteDagNesteAar = new Date(nesteAar, 0, 1)
+const sisteDagNesteAar = new Date(nesteAar, 11, 31)
 
     return (
         !!innhold && (
