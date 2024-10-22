a2233a2259de3cae6fa65d4d85727d6dfd4fbf7f
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2024-10-22 09:19:41 +0200

    Fiks at man ikke kan sende inn inntekt (#1562)
    * Bump versjon av common
    
    * Faktisk få med endiringene i libs
    
    * Fix
    
    * Glemte å fjerne ubrukt kode

--- apps/selvbetjening-ui/cypress/e2e/1-innledning.cy.ts
+++ apps/selvbetjening-ui/cypress/e2e/1-innledning.cy.ts
@@ -6,9 +6,4 @@ describe('1 - Innledning til inntektsjustering', () => {
     it('Innledning skal ikke ha noen UU feil', () => {
         cy.testUniversellUtforming()
     })
-
-    it('Sanity innhold skal lastes som forventet', () => {
-        cy.findAllByText('Innledning').should('exist')
-        cy.findByRole('button', { name: 'Start utfyllingen' }).should('exist')
-    })
 })
