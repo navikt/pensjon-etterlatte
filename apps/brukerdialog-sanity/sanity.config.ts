40d3dbe377b8c563a91231b352b14e548b7ccf01
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-11-11 19:56:37 +0100

    Legg inn live edit workspace

--- apps/brukerdialog-sanity/sanity.config.ts
+++ apps/brukerdialog-sanity/sanity.config.ts
@@ -22,6 +22,19 @@ export default defineConfig([
     {
         projectId: 'u0dlg8d8',
         dataset: 'selvbetjening-ui',
+        name: 'selvbetjening-ui-live-edit',
+        title: 'Selvbetjening live edit',
+        basePath: '/selvbetjening-ui',
+        plugins: [structureTool(), visionTool()],
+        auth: auth,
+        liveEdit: true,
+        schema: {
+            types: [...inntektsjusteringSchemaTypes, fantIkkeSidenSchemaType, systemUtilgjengeligSchemaType],
+        },
+    },
+    {
+        projectId: 'u0dlg8d8',
+        dataset: 'selvbetjening-ui',
         name: 'selvbetjening-ui',
         title: 'Selvbetjening',
         basePath: '/selvbetjening-ui',
