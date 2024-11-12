3243eaa9fe5c41e994c461a37bb61d38fe8d255e
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-11-12 18:55:20 +0100

    Fiks broken kommando for eslint v9

--- apps/omstillingsstoenad-ui/eslint.config.mjs
+++ apps/omstillingsstoenad-ui/eslint.config.mjs
@@ -18,6 +18,7 @@ const compat = new FlatCompat({
 });
 
 export default [{
+    files: ["**/*.ts", "**/*.tsx"],
     ignores: ["node_modules/*", "nais/*", "build/*"],
 }, ...compat.extends("plugin:react/recommended", "plugin:@typescript-eslint/recommended"), {
     plugins: {
