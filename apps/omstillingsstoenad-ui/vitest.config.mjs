03b320e96ac709bceaae044af7ffcfe566127b67
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-12-02 15:37:46 +0100

    Fix v2

--- apps/omstillingsstoenad-ui/vitest.config.mjs
+++ apps/omstillingsstoenad-ui/vitest.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config.mjs'
 
 // https://vitejs.dev/config/
-export default mergeConfig(
-    viteConfig,
-    defineConfig({
-        test: {
-            globals: true, // https://github.com/testing-library/vue-testing-library/issues/296
-            environment: 'jsdom',
-        },
-    })
-)
+export default defineConfig(configEnv => mergeConfig(
+        viteConfig(configEnv),
+        defineConfig({
+            test: {
+                globals: true, // https://github.com/testing-library/vue-testing-library/issues/296
+                environment: 'jsdom',
+            },
+        })
+))
