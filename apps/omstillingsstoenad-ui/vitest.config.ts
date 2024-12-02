9ff756370073771fa1c323191eaa339ae5f40280
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-12-02 15:23:41 +0100

    Dis work

--- apps/omstillingsstoenad-ui/vitest.config.ts
+++ apps/omstillingsstoenad-ui/vitest.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: react(),
     test: {
         globals: true, // https://github.com/testing-library/vue-testing-library/issues/296
         environment: 'jsdom',
