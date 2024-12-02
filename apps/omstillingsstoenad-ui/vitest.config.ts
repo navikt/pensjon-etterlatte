b43b70e44962ff2efb59169d3ca22aad4c28116c
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-12-02 15:27:51 +0100

    Merge vitest config med vite config

--- apps/omstillingsstoenad-ui/vitest.config.ts
+++ apps/omstillingsstoenad-ui/vitest.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, mergeConfig } from 'vitest/config'
+import viteConfig from './vite.config.mjs'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: react(),
-    test: {
-        globals: true, // https://github.com/testing-library/vue-testing-library/issues/296
-        environment: 'jsdom',
-    },
-})
+export default mergeConfig(
+    viteConfig,
+    defineConfig({
+        test: {
+            globals: true, // https://github.com/testing-library/vue-testing-library/issues/296
+            environment: 'jsdom',
+        },
+    })
+)
