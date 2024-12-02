06d4f641021bcb022cc9dcdeff24da1a7cad7063
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-12-02 15:20:36 +0100

    Legg inn react som plugin

--- apps/omstillingsstoenad-ui/vitest.config.ts
+++ apps/omstillingsstoenad-ui/vitest.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [react()],
     test: {
         globals: true, // https://github.com/testing-library/vue-testing-library/issues/296
         environment: 'jsdom',
