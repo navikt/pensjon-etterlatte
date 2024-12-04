65f05e9f42ca789a3dfae9c10147e88b249fb36f
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-12-04 12:45:25 +0100

    Endre base path for selvbetjening

--- apps/selvbetjening-ui/src/main.tsx
+++ apps/selvbetjening-ui/src/main.tsx
@@ -3,16 +3,13 @@ import { createRoot } from 'react-dom/client'
 import '@navikt/ds-css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { SystemUtilgjengelig } from './common/SystemUtilgjengelig.tsx'
-import { InntektsjusteringKvittering } from './inntektsjustering/4-kvittering/InntektsjusteringKvittering.tsx'
 import { fetcher } from './utils/api.ts'
 import { SWRConfig } from 'swr'
-import { InntektsjusteringInnledning } from './inntektsjustering/1-innledning/InntektsjusteringInnledning.tsx'
 import { ProvideSpraakContext } from './common/spraak/SpraakContext.tsx'
-import { InntektsjusteringOppsummering } from './inntektsjustering/3-oppsummering/InntektsjusteringOppsummering.tsx'
-import { InntektsjusteringInntektTilNesteAar } from './inntektsjustering/2-inntekt-til-neste-aar/InntektsjusteringInntektTilNesteAar.tsx'
 import { FantIkkeSiden } from './common/FantIkkeSiden.tsx'
 import { ProvideInntektContext } from './common/inntekt/InntektContext.tsx'
 import { ProvideInnloggetInnbyggerContext } from './common/innloggetInnbygger/InnloggetInnbyggerContext.tsx'
+import { inntektjusteringRoutes } from './inntektsjustering/inntektjusteringRoutes.tsx'
 
 const router = createBrowserRouter(
     [
@@ -25,26 +22,11 @@ const router = createBrowserRouter(
                     element: <FantIkkeSiden />,
                 },
                 { path: '/system-utilgjengelig', element: <SystemUtilgjengelig /> },
-                {
-                    path: '/innledning',
-                    element: <InntektsjusteringInnledning />,
-                },
-                {
-                    path: '/inntekt-til-neste-aar',
-                    element: <InntektsjusteringInntektTilNesteAar />,
-                },
-                {
-                    path: '/oppsummering',
-                    element: <InntektsjusteringOppsummering />,
-                },
-                {
-                    path: '/kvittering',
-                    element: <InntektsjusteringKvittering />,
-                },
+                ...inntektjusteringRoutes,
             ],
         },
     ],
-    { basename: '/omstillingsstonad/inntekt' }
+    { basename: '/omstillingsstonad' }
 )
 
 createRoot(document.getElementById('root')!).render(
