d8d71128102d77063e22e26a71b1bbbd13fb0474
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2024-12-05 12:05:33 +0100

    Enkelt skjema for å melde inn inntekt

--- apps/selvbetjening-ui/src/meldInnEndring/meldInnEndringSkjema/MeldInnEndringSkjema.tsx
+++ apps/selvbetjening-ui/src/meldInnEndring/meldInnEndringSkjema/MeldInnEndringSkjema.tsx
@@ -1,3 +1,41 @@
+import { BodyShort, Box, Heading, HStack, Select, Textarea, VStack } from '@navikt/ds-react'
+import { SpraakVelger } from '../../common/spraak/SpraakVelger.tsx'
+
+enum EndringerAaMeldeInn {
+    INNTEKT = 'Inntekt',
+    AKTIVITET = 'Aktivitet',
+}
+
 export const MeldInnEndringSkjema = () => {
-    return <></>
+    return (
+        <main>
+            <HStack justify="center" padding="8" minHeight="100vh">
+                <VStack gap="6" maxWidth="42.5rem">
+                    <HStack justify="end">
+                        <SpraakVelger />
+                    </HStack>
+                    <Heading size="xlarge" level="1">
+                        Meld inn endring
+                    </Heading>
+                    <BodyShort>
+                        Her kan du melde inn endringer relatert til livet ditt, vennligst hold det kort og ungå
+                        personopplysninger. Hvis du vil ettersende dokumenter kan du gjøre det her LEGG INN LENKE.
+                    </BodyShort>
+                    <Box maxWidth="20rem">
+                        <Select label="Hva det gjelder">
+                            <option value="">Velg årsak</option>
+                            {Object.entries(EndringerAaMeldeInn).map(([key, value]) => (
+                                <option key={key} value={key}>
+                                    {value}
+                                </option>
+                            ))}
+                        </Select>
+                    </Box>
+                    <Box maxWidth="30rem">
+                        <Textarea label="Beskrivelse" maxLength={500} />
+                    </Box>
+                </VStack>
+            </HStack>
+        </main>
+    )
 }
