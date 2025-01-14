0cc9fb619cd55d6c93bfb8b6726225599e47a6d3
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-14 13:11:58 +0100

    Sanity schema for meld inn endring

--- apps/brukerdialog-sanity/schemaTypes/meldInnEndring/meldInnEndringMeldFraSchemaType.ts
+++ apps/brukerdialog-sanity/schemaTypes/meldInnEndring/meldInnEndringMeldFraSchemaType.ts
@@ -1,3 +1,246 @@
-import { defineType } from 'sanity'
+import { defineField, defineType } from 'sanity'
+import { spraakBlockFields, spraakStringFields } from '../spraak'
+import { errorVedTomInputSchemaField, textFieldSchemaFields } from '../inntektsjustering/skjemaer/fellesSchemaFields'
 
-export const meldInnEndringMeldFraSchemaType = defineType({})
+export const meldInnEndringMeldFraSchemaType = defineType({
+    name: 'meldInnEndringMeldFra',
+    title: '2 - Meld fra om endring',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'dokumentTittel',
+            title: 'Dokument tittel',
+            type: 'string',
+        }),
+        defineField({
+            name: 'tittel',
+            title: 'Tittel',
+            type: 'object',
+            fields: spraakStringFields,
+        }),
+        defineField({
+            name: 'endring',
+            title: 'Hvilken type endring',
+            type: 'object',
+            fields: [
+                defineField({
+                    name: 'legend',
+                    title: 'Legend',
+                    type: 'object',
+                    fields: spraakStringFields,
+                }),
+                defineField({
+                    name: 'description',
+                    title: 'Description (valgfri)',
+                    type: 'object',
+                    fields: spraakStringFields,
+                }),
+                defineField({
+                    name: 'radios',
+                    title: 'Radios',
+                    type: 'object',
+                    fields: [
+                        defineField({
+                            name: 'aktivitetOgInntekt',
+                            title: 'Aktivitet og inntekt',
+                            type: 'object',
+                            fields: [
+                                defineField({
+                                    name: 'label',
+                                    title: 'Label',
+                                    type: 'object',
+                                    fields: spraakStringFields,
+                                }),
+                                defineField({
+                                    name: 'description',
+                                    title: 'Description (valgfri)',
+                                    type: 'object',
+                                    fields: spraakStringFields,
+                                }),
+                            ],
+                        }),
+                        defineField({
+                            name: 'inntekt',
+                            title: 'Inntekt',
+                            type: 'object',
+                            fields: spraakStringFields,
+                        }),
+                        defineField({
+                            name: 'annet',
+                            title: 'Annet',
+                            type: 'object',
+                            fields: spraakStringFields,
+                        }),
+                    ],
+                }),
+                errorVedTomInputSchemaField,
+            ],
+        }),
+        defineField({
+            name: 'informasjonOmEndring',
+            title: 'Informasjon om endring',
+            type: 'object',
+            fields: [
+                defineField({
+                    name: 'aktivitetOgInntekt',
+                    title: 'Aktivitet og inntekt',
+                    type: 'object',
+                    fields: [
+                        defineField({
+                            name: 'endringAccordion',
+                            title: 'Accordion om endringer',
+                            type: 'object',
+                            fields: [
+                                defineField({
+                                    name: 'jobbItem',
+                                    title: 'Jobb item',
+                                    type: 'object',
+                                    fields: [
+                                        defineField({
+                                            name: 'tittel',
+                                            title: 'Tittel',
+                                            type: 'object',
+                                            fields: spraakStringFields,
+                                        }),
+                                        defineField({
+                                            name: 'innhold',
+                                            title: 'Innhold',
+                                            type: 'object',
+                                            fields: spraakBlockFields,
+                                        }),
+                                    ],
+                                }),
+                                defineField({
+                                    name: 'studierItem',
+                                    title: 'Studier item',
+                                    type: 'object',
+                                    fields: [
+                                        defineField({
+                                            name: 'tittel',
+                                            title: 'Tittel',
+                                            type: 'object',
+                                            fields: spraakStringFields,
+                                        }),
+                                        defineField({
+                                            name: 'innhold',
+                                            title: 'Innhold',
+                                            type: 'object',
+                                            fields: spraakBlockFields,
+                                        }),
+                                    ],
+                                }),
+                                defineField({
+                                    name: 'annenAktivitetItem',
+                                    title: 'Annen aktivitet item',
+                                    type: 'object',
+                                    fields: [
+                                        defineField({
+                                            name: 'tittel',
+                                            title: 'Tittel',
+                                            type: 'object',
+                                            fields: spraakStringFields,
+                                        }),
+                                        defineField({
+                                            name: 'innhold',
+                                            title: 'Innhold',
+                                            type: 'object',
+                                            fields: spraakBlockFields,
+                                        }),
+                                    ],
+                                }),
+                            ],
+                        }),
+                        defineField({
+                            name: 'hovedinnhold',
+                            title: 'Hovedinnhold',
+                            type: 'object',
+                            fields: spraakBlockFields,
+                        }),
+                    ],
+                }),
+                defineField({
+                    name: 'inntekt',
+                    title: 'Inntekt',
+                    type: 'object',
+                    fields: [
+                        defineField({
+                            name: 'hovedinnhold',
+                            title: 'Hovedinnhold',
+                            type: 'object',
+                            fields: spraakBlockFields,
+                        }),
+                        defineField({
+                            name: 'hvorforViSpoerOmInntektReadMore',
+                            title: 'Hvorfor vi spør om inntekt read more',
+                            type: 'object',
+                            fields: [
+                                defineField({
+                                    name: 'tittel',
+                                    title: 'Tittel',
+                                    type: 'object',
+                                    fields: spraakStringFields,
+                                }),
+                                defineField({
+                                    name: 'innhold',
+                                    title: 'Innhold',
+                                    type: 'object',
+                                    fields: spraakBlockFields,
+                                }),
+                            ],
+                        }),
+                        defineField({
+                            name: 'svarPaaSpoersmaal',
+                            title: 'Svar på spørsmål',
+                            type: 'object',
+                            fields: spraakBlockFields,
+                        }),
+                    ],
+                }),
+                defineField({
+                    name: 'annet',
+                    title: 'Annet',
+                    type: 'object',
+                    fields: [
+                        defineField({
+                            name: 'hovedinnhold',
+                            title: 'Hovedinnhold',
+                            type: 'object',
+                            fields: spraakBlockFields,
+                        }),
+                        defineField({
+                            name: 'endreKontaktEllerKontonummerAlert',
+                            title: 'Alert for å endre kontakt eller kontonummer',
+                            type: 'object',
+                            fields: [
+                                defineField({
+                                    name: 'tittel',
+                                    title: 'Tittel',
+                                    type: 'object',
+                                    fields: spraakStringFields,
+                                }),
+                                defineField({
+                                    name: 'innhold',
+                                    title: 'Innhold',
+                                    type: 'object',
+                                    fields: spraakBlockFields,
+                                }),
+                            ],
+                        }),
+                        defineField({
+                            name: 'detViTrengerForAaBehandleEndring',
+                            title: 'Det vi trenger for å behandle endring',
+                            type: 'object',
+                            fields: spraakBlockFields,
+                        }),
+                    ],
+                }),
+            ],
+        }),
+        defineField({
+            name: 'beskrivelseAvEndring',
+            title: 'Beskrivelse av endring',
+            type: 'object',
+            fields: textFieldSchemaFields,
+        }),
+    ],
+})
