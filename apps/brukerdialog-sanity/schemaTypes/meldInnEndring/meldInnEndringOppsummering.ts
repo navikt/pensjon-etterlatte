53ca4cc299075f0594b467d7beb1aa30e066d7d3
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-16 19:57:25 +0100

    Sanity schema for oppsummering

--- apps/brukerdialog-sanity/schemaTypes/meldInnEndring/meldInnEndringOppsummering.ts
+++ apps/brukerdialog-sanity/schemaTypes/meldInnEndring/meldInnEndringOppsummering.ts
@@ -1,5 +1,5 @@
 import { defineField, defineType } from 'sanity'
-import { spraakBlockFields } from '../spraak'
+import { spraakBlockFields, spraakStringFields } from '../spraak'
 
 export const meldInnEndringOppsummering = defineType({
     name: 'meldInnEndringOppsummering',
@@ -16,6 +16,79 @@ export const meldInnEndringOppsummering = defineType({
             title: 'Veiledning',
             type: 'object',
             fields: spraakBlockFields,
+        }),
+        defineField({
+            name: 'skjemaSammendrag',
+            title: 'Skjema sammendrag',
+            type: 'object',
+            fields: [
+                defineField({
+                    name: 'tittel',
+                    title: 'Tittel',
+                    type: 'object',
+                    fields: spraakStringFields,
+                }),
+                defineField({
+                    name: 'endreSvarLenke',
+                    title: 'Endre svar lenke',
+                    type: 'object',
+                    fields: [
+                        defineField({
+                            name: 'tekst',
+                            title: 'Tekst',
+                            type: 'object',
+                            fields: spraakStringFields,
+                        }),
+                    ],
+                }),
+                defineField({
+                    name: 'endring',
+                    title: 'Endring',
+                    type: 'object',
+                    fields: [
+                        defineField({
+                            name: 'label',
+                            title: 'Label',
+                            type: 'object',
+                            fields: spraakStringFields,
+                        }),
+                        defineField({
+                            name: 'value',
+                            title: 'Value',
+                            type: 'object',
+                            fields: [
+                                defineField({
+                                    name: 'aktivitetOgInntekt',
+                                    title: 'Aktivitet og inntekt',
+                                    type: 'object',
+                                    fields: spraakStringFields,
+                                }),
+                                defineField({
+                                    name: 'inntekt',
+                                    title: 'Inntekt',
+                                    type: 'object',
+                                    fields: spraakStringFields,
+                                }),
+                                defineField({
+                                    name: 'annet',
+                                    title: 'Annet',
+                                    type: 'object',
+                                    fields: spraakStringFields,
+                                }),
+                            ],
+                        }),
+                    ],
+                }),
+            ],
+        }),
+        defineField({
+            name: 'feilIOppretelseAvEndring',
+            title: 'Feil i opprettelse av endring',
+            type: 'object',
+            fields: [
+                defineField({ name: 'tittel', title: 'Tittel', type: 'object', fields: spraakStringFields }),
+                defineField({ name: 'innhold', title: 'Innhold', type: 'object', fields: spraakBlockFields }),
+            ],
         }),
     ],
 })
