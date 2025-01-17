d4d79f21669659eb01772c8ec7fe71d6cc34928d
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-17 13:34:26 +0100

    Oppsett for API kall i opprettele av meld inn endring

---
+++ apps/selvbetjening-ui/src/meldInnEndring/3-oppsummering/FeilIOppretelseAvEndring.tsx
@@ -0,0 +1,34 @@
+import { useSpraak } from '../../common/spraak/SpraakContext.tsx'
+import { useSanityInnhold } from '../../common/sanity/useSanityInnhold.ts'
+import { MeldInnEndringOppsummering } from '../sanity.types.ts'
+import { KomponentLaster } from '../../common/KomponentLaster.tsx'
+import { Alert, Heading } from '@navikt/ds-react'
+import { SanityRikTekst } from '../../common/sanity/SanityRikTekst.tsx'
+
+export const FeilIInnsendingAvEndring = () => {
+    const spraak = useSpraak()
+
+    const {
+        innhold,
+        error: innholdError,
+        isLoading: innholdIsLoading,
+    } = useSanityInnhold<MeldInnEndringOppsummering>('*[_type == "meldInnEndringOppsummering"]')
+
+    if (innholdIsLoading) {
+        return <KomponentLaster />
+    }
+    if (innholdError) {
+        throw innholdError
+    }
+
+    return (
+        !!innhold && (
+            <Alert variant="error">
+                <Heading spacing size="small" level="3">
+                    {innhold.feilIOppretelseAvEndring?.tittel?.[spraak]}
+                </Heading>
+                <SanityRikTekst text={innhold.feilIOppretelseAvEndring?.innhold?.[spraak]} />
+            </Alert>
+        )
+    )
+}
