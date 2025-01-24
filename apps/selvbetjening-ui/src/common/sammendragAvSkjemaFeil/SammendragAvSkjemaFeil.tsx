75feedebc44912d06bd7f8cbc0d851260fba66b8
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-24 13:26:23 +0100

    flytt beskrivelse slik at den ikke vises før man må

--- apps/selvbetjening-ui/src/common/sammendragAvSkjemaFeil/SammendragAvSkjemaFeil.tsx
+++ apps/selvbetjening-ui/src/common/sammendragAvSkjemaFeil/SammendragAvSkjemaFeil.tsx
@@ -2,7 +2,6 @@ import { FieldErrors, FieldValues } from 'react-hook-form'
 import { useSpraak } from '../spraak/SpraakContext.tsx'
 import { useSanityInnhold } from '../sanity/useSanityInnhold.ts'
 import { SammendragAvSkjemaFeil as SammendragAvSkjemaFeilInnhold } from '../sanity.types.ts'
-import { KomponentLaster } from '../KomponentLaster.tsx'
 import { ErrorSummary } from '@navikt/ds-react'
 import { formaterFieldErrors } from './skjemaError.ts'
 
@@ -17,10 +16,7 @@ export const SammendragAvSkjemaFeil = <T extends FieldValues>({ errors }: Props<
         '*[_type == "sammendragAvSkjemaFeil"]'
     )
 
-    if (isLoading) {
-        return <KomponentLaster />
-    }
-    if (error) {
+    if (error && !isLoading) {
         throw error
     }
 
