4e1f39ee1d1a20df705665f8eb602fad073129ee
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-27 15:09:16 +0100

    Oppdater html tittel utifra hvilket OMS skjema man er på

--- apps/selvbetjening-ui/src/inntektsjustering/InntektsjusteringOutlet.tsx
+++ apps/selvbetjening-ui/src/inntektsjustering/InntektsjusteringOutlet.tsx
@@ -6,9 +6,15 @@ import {
 } from '../common/featureToggles/FeatureTogglesContext.tsx'
 import { InntektSkjemaLukket } from './components/inntektSkjemaLukket/InntektSkjemaLukket.tsx'
 import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export const InntektsjusteringOutlet = () => {
     const omsInntetksjusteringSkjemaFeatureToggle = useFeatureToggle(FeatureToggleNavn.OMS_INNTEKTSJUSTERING_SKJEMA)
+
+    useEffect(() => {
+        document.title = 'Inntekt neste år'
+    }, [])
+
     return (
         <ProvideInntektContext>
             {omsInntetksjusteringSkjemaFeatureToggle.status === FeatureToggleStatus.PAA ? (
