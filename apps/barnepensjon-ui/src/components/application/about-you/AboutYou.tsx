95748e5ee9500ba5cde9995cb193d3579e15dbd4
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-29 13:54:32 +0100

    Migrer over til biome

--- apps/barnepensjon-ui/src/components/application/about-you/AboutYou.tsx
+++ apps/barnepensjon-ui/src/components/application/about-you/AboutYou.tsx
@@ -1,65 +1,65 @@
-import { FormProvider, useForm } from 'react-hook-form'
-import { JaNeiVetIkke } from '~api/dto/FellesOpplysninger'
-import { ActionTypes } from '~context/application/application'
-import { useApplicationContext } from '~context/application/ApplicationContext'
-import { useUserContext } from '~context/user/UserContext'
-import useTranslation from '../../../hooks/useTranslation'
-import ErrorSummaryWrapper from '../../common/ErrorSummaryWrapper'
-import Navigation from '../../common/Navigation'
-import { RHFTelefonInput } from '../../common/rhf/RHFInput'
-import { RHFGeneralQuestionRadio } from '../../common/rhf/RHFRadio'
-import StepHeading from '../../common/StepHeading'
-import { StepProps } from '../Dialogue'
-import LoggedInUserInfo from './LoggedInUserInfo'
-import FormElement from '../../common/FormElement'
-import { IAboutYou } from '~types/person'
-import PaymentDetails from '../../common/PaymentDetails'
-import useCountries, { Options } from '../../../hooks/useCountries'
-import { Bredde } from '~utils/bredde'
-import Datepicker from '../../common/Datepicker'
-import { Box, Heading, HGrid, ReadMore } from '@navikt/ds-react'
-import { GridColumns, GridGap } from '~utils/grid'
-import FormGroup from '../../common/FormGroup'
-import { ApplicantRole } from '~types/applicant'
-import { RHFCombobox } from '~components/common/rhf/RHFCombobox'
-import { LogEvents, useAmplitude } from '~hooks/useAmplitude'
-import { FieldErrors } from 'react-hook-form/dist/types/errors'
+import { FormProvider, useForm } from "react-hook-form";
+import { JaNeiVetIkke } from "~api/dto/FellesOpplysninger";
+import { ActionTypes } from "~context/application/application";
+import { useApplicationContext } from "~context/application/ApplicationContext";
+import { useUserContext } from "~context/user/UserContext";
+import useTranslation from "../../../hooks/useTranslation";
+import ErrorSummaryWrapper from "../../common/ErrorSummaryWrapper";
+import Navigation from "../../common/Navigation";
+import { RHFTelefonInput } from "../../common/rhf/RHFInput";
+import { RHFGeneralQuestionRadio } from "../../common/rhf/RHFRadio";
+import StepHeading from "../../common/StepHeading";
+import { StepProps } from "../Dialogue";
+import LoggedInUserInfo from "./LoggedInUserInfo";
+import FormElement from "../../common/FormElement";
+import { IAboutYou } from "~types/person";
+import PaymentDetails from "../../common/PaymentDetails";
+import useCountries, { Options } from "../../../hooks/useCountries";
+import { Bredde } from "~utils/bredde";
+import Datepicker from "../../common/Datepicker";
+import { Box, Heading, HGrid, ReadMore } from "@navikt/ds-react";
+import { GridColumns, GridGap } from "~utils/grid";
+import FormGroup from "../../common/FormGroup";
+import { ApplicantRole } from "~types/applicant";
+import { RHFCombobox } from "~components/common/rhf/RHFCombobox";
+import { LogEvents, useAmplitude } from "~hooks/useAmplitude";
+import { FieldErrors } from "react-hook-form/dist/types/errors";
 
 export default function AboutYou({ next }: StepProps) {
-    const { t } = useTranslation('aboutYou')
-    const { state, dispatch } = useApplicationContext()
-    const { state: user } = useUserContext()
-    const { countries }: { countries: Options[] } = useCountries()
-    const { logEvent } = useAmplitude()
+    const { t } = useTranslation("aboutYou");
+    const { state, dispatch } = useApplicationContext();
+    const { state: user } = useUserContext();
+    const { countries }: { countries: Options[] } = useCountries();
+    const { logEvent } = useAmplitude();
 
     const save = (data: any) => {
-        dispatch({ type: ActionTypes.UPDATE_ABOUT_YOU, payload: { ...data } })
-        next!()
-    }
+        dispatch({ type: ActionTypes.UPDATE_ABOUT_YOU, payload: { ...data } });
+        next!();
+    };
 
-    const logErrors = (data: FieldErrors<any>) => {
-        Object.keys(data).map((error) => logEvent(LogEvents.VALIDATION_ERROR, { skjemanavn: 'AboutYou', id: error }))
-    }
+    const logErrors = (data: FieldErrors) => {
+        Object.keys(data).map((error) => logEvent(LogEvents.VALIDATION_ERROR, { skjemanavn: "AboutYou", id: error }));
+    };
 
     const methods = useForm<IAboutYou>({
         defaultValues: state.aboutYou || {},
         shouldUnregister: true,
-    })
+    });
 
     const {
         handleSubmit,
         watch,
         formState: { errors },
-    } = methods
+    } = methods;
 
-    const residesInNorway = watch('residesInNorway')
-    const stayedAbroad = watch('stayedAbroad')
-    const isChild = state.applicant?.applicantRole === ApplicantRole.CHILD
-    const isGuardian = state.applicant?.applicantRole === ApplicantRole.GUARDIAN
+    const residesInNorway = watch("residesInNorway");
+    const stayedAbroad = watch("stayedAbroad");
+    const isChild = state.applicant?.applicantRole === ApplicantRole.CHILD;
+    const isGuardian = state.applicant?.applicantRole === ApplicantRole.GUARDIAN;
 
     return (
         <>
-            <StepHeading>{isGuardian ? t('titleGuardian') : t('title')}</StepHeading>
+            <StepHeading>{isGuardian ? t("titleGuardian") : t("title")}</StepHeading>
 
             <LoggedInUserInfo user={user} />
 
@@ -69,14 +69,14 @@ export default function AboutYou({ next }: StepProps) {
                         {!user.adressebeskyttelse && isChild && (
                             <FormGroup>
                                 <FormElement>
-                                    <Heading size={'small'}>{t('staysAbroadTitle')}</Heading>
+                                    <Heading size={"small"}>{t("staysAbroadTitle")}</Heading>
                                 </FormElement>
 
-                                <RHFGeneralQuestionRadio name={'residesInNorway'} legend={t('residesInNorway')} />
+                                <RHFGeneralQuestionRadio name={"residesInNorway"} legend={t("residesInNorway")} />
 
                                 {residesInNorway === JaNeiVetIkke.JA && (
                                     <FormElement>
-                                        <RHFGeneralQuestionRadio name={'stayedAbroad'} legend={t('stayedAbroad')} />
+                                        <RHFGeneralQuestionRadio name={"stayedAbroad"} legend={t("stayedAbroad")} />
                                     </FormElement>
                                 )}
 
@@ -85,23 +85,23 @@ export default function AboutYou({ next }: StepProps) {
                                         <Box maxWidth="14rem">
                                             <FormElement>
                                                 <RHFCombobox
-                                                    id={'stayedAbroadCountry'}
-                                                    name={'stayedAbroadCountry'}
-                                                    label={t('stayedAbroadCountry')}
+                                                    id={"stayedAbroadCountry"}
+                                                    name={"stayedAbroadCountry"}
+                                                    label={t("stayedAbroadCountry")}
                                                     options={countries.map((country) => country.label)}
                                                 />
                                             </FormElement>
                                         </Box>
-                                        <HGrid gap={GridGap} columns={GridColumns} align={'start'}>
+                                        <HGrid gap={GridGap} columns={GridColumns} align={"start"}>
                                             <Datepicker
-                                                name={'stayedAbroadFromDate'}
-                                                label={t('stayedAbroadFromDate')}
+                                                name={"stayedAbroadFromDate"}
+                                                label={t("stayedAbroadFromDate")}
                                                 maxDate={new Date()}
                                                 valgfri={true}
                                             />
                                             <Datepicker
-                                                name={'stayedAbroadToDate'}
-                                                label={t('stayedAbroadToDate')}
+                                                name={"stayedAbroadToDate"}
+                                                label={t("stayedAbroadToDate")}
                                                 maxDate={new Date()}
                                                 valgfri={true}
                                             />
@@ -113,9 +113,9 @@ export default function AboutYou({ next }: StepProps) {
                                     <Box maxWidth="14rem">
                                         <FormElement>
                                             <RHFCombobox
-                                                id={'countryOfResidence'}
-                                                name={'countryOfResidence'}
-                                                label={t('countryOfResidence')}
+                                                id={"countryOfResidence"}
+                                                name={"countryOfResidence"}
+                                                label={t("countryOfResidence")}
                                                 options={countries.map((country) => country.label)}
                                             />
                                         </FormElement>
@@ -127,8 +127,8 @@ export default function AboutYou({ next }: StepProps) {
                         {!!user.foedselsnummer && !user.telefonnummer && !isGuardian && (
                             <FormElement>
                                 <RHFTelefonInput
-                                    name={'phoneNumber'}
-                                    label={t('phoneNumberOptional', { ns: 'common' })}
+                                    name={"phoneNumber"}
+                                    label={t("phoneNumberOptional", { ns: "common" })}
                                     valgfri={true}
                                     htmlSize={Bredde.S}
                                     autoComplete="tel"
@@ -139,25 +139,25 @@ export default function AboutYou({ next }: StepProps) {
 
                     {!user.adressebeskyttelse && isChild && (
                         <>
-                            <Heading size={'small'}>{t('paymentsFromNav')}</Heading>
+                            <Heading size={"small"}>{t("paymentsFromNav")}</Heading>
                             <FormGroup>
                                 <FormElement>
                                     <RHFGeneralQuestionRadio
-                                        name={'disabilityBenefits'}
-                                        legend={t('disabilityBenefits')}
+                                        name={"disabilityBenefits"}
+                                        legend={t("disabilityBenefits")}
                                     />
-                                    <ReadMore header={t('whyWeAsk', { ns: 'common' })}>
-                                        {t('disabilityBenefitsInfo')}
+                                    <ReadMore header={t("whyWeAsk", { ns: "common" })}>
+                                        {t("disabilityBenefitsInfo")}
                                     </ReadMore>
                                 </FormElement>
 
                                 <FormElement>
                                     <RHFGeneralQuestionRadio
-                                        name={'workAssessmentAllowance'}
-                                        legend={t('workAssessmentAllowance')}
+                                        name={"workAssessmentAllowance"}
+                                        legend={t("workAssessmentAllowance")}
                                     />
-                                    <ReadMore header={t('whyWeAsk', { ns: 'common' })}>
-                                        {t('workAssessmentAllowanceInfo')}
+                                    <ReadMore header={t("whyWeAsk", { ns: "common" })}>
+                                        {t("workAssessmentAllowanceInfo")}
                                     </ReadMore>
                                 </FormElement>
                             </FormGroup>
@@ -166,7 +166,7 @@ export default function AboutYou({ next }: StepProps) {
 
                     {!user.adressebeskyttelse && isChild && (
                         <>
-                            <Heading size={'small'}>{t('bankAccountNumberAndPayment')}</Heading>
+                            <Heading size={"small"}>{t("bankAccountNumberAndPayment")}</Heading>
                             <PaymentDetails />
                         </>
                     )}
@@ -177,5 +177,5 @@ export default function AboutYou({ next }: StepProps) {
                 </form>
             </FormProvider>
         </>
-    )
+    );
 }
