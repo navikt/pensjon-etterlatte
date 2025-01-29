e12b44fbf470ab49d122d28e73777b838c355bb6
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-29 14:02:53 +0100

    Migrer over til biome

--- apps/barnepensjon-ui/src/context/language/LanguageContext.tsx
+++ apps/barnepensjon-ui/src/context/language/LanguageContext.tsx
@@ -1,64 +1,64 @@
-import React, { createContext, FC, useContext, useState } from 'react'
-import { Language } from './language'
-import { Namespace, TKey } from './translations'
-import nb from '../../locales/nb'
-import nn from '../../locales/nn'
-import en from '../../locales/en'
-import { FCProps } from '../../types/FCProps'
-import { useCookies } from 'react-cookie'
-import { setParams } from '@navikt/nav-dekoratoren-moduler'
+import React, { createContext, FC, useContext, useState } from "react";
+import { Language } from "./language";
+import { Namespace, TKey } from "./translations";
+import nb from "../../locales/nb";
+import nn from "../../locales/nn";
+import en from "../../locales/en";
+import { FCProps } from "../../types/FCProps";
+import { useCookies } from "react-cookie";
+import { setParams } from "@navikt/nav-dekoratoren-moduler";
 
-const initialLanguage = (localStorage.getItem('language') as Language) || Language.BOKMAAL
-const initialTranslations = nb as Record<Namespace, Record<TKey, any>>
+const initialLanguage = (localStorage.getItem("language") as Language) || Language.BOKMAAL;
+const initialTranslations = nb as Record<Namespace, Record<string, string>>;
 
-document.documentElement.lang = initialLanguage
+document.documentElement.lang = initialLanguage;
 
 const LanguageContext = createContext({
     language: initialLanguage,
     translations: initialTranslations,
     updateLanguage: (_: Language) => {}, // eslint-disable-line  @typescript-eslint/no-unused-vars
-})
+});
 
-const useLanguageContext = () => useContext(LanguageContext)
+const useLanguageContext = () => useContext(LanguageContext);
 
 const languages = {
     nb,
     nn,
     en,
-}
+};
 
-const dekoratorLanguageCookieName = 'decorator-language'
+const dekoratorLanguageCookieName = "decorator-language";
 
 const LanguageProvider: FC<FCProps> = ({ children }) => {
-    const [language, setLanguage] = useState<Language>(initialLanguage)
-    const [translations, setTranslations] = useState<Record<Namespace, Record<TKey, any>>>(languages[language])
-    const [cookies, setCookie] = useCookies([dekoratorLanguageCookieName])
-    const { [dekoratorLanguageCookieName]: dekoratørSpraak } = cookies
+    const [language, setLanguage] = useState<Language>(initialLanguage);
+    const [translations, setTranslations] = useState<Record<Namespace, Record<string, string>>>(languages[language]);
+    const [cookies, setCookie] = useCookies([dekoratorLanguageCookieName]);
+    const { [dekoratorLanguageCookieName]: dekoratørSpraak } = cookies;
 
     if (!dekoratørSpraak) {
-        setCookie(dekoratorLanguageCookieName, language)
+        setCookie(dekoratorLanguageCookieName, language);
     }
 
     const updateLanguage = (lang: Language) => {
-        console.log('setting language and translations')
+        console.log("setting language and translations");
 
-        localStorage.setItem('language', lang)
-        setLanguage(lang)
-        document.documentElement.lang = lang
+        localStorage.setItem("language", lang);
+        setLanguage(lang);
+        document.documentElement.lang = lang;
 
-        if (lang === Language.NYNORSK) setTranslations(nn)
-        else if (lang === Language.ENGELSK) setTranslations(en)
-        else setTranslations(nb)
+        if (lang === Language.NYNORSK) setTranslations(nn);
+        else if (lang === Language.ENGELSK) setTranslations(en);
+        else setTranslations(nb);
 
-        setParams({ language: lang }).then()
-        setCookie(dekoratorLanguageCookieName, lang)
-    }
+        setParams({ language: lang }).then();
+        setCookie(dekoratorLanguageCookieName, lang);
+    };
 
     return (
         <LanguageContext.Provider value={{ language, translations, updateLanguage }}>
             {children}
         </LanguageContext.Provider>
-    )
-}
+    );
+};
 
-export { useLanguageContext, LanguageProvider }
+export { useLanguageContext, LanguageProvider };
