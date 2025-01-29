062aa078d875e11ee2f5dc9afd74a4562649eb90
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-29 15:10:44 +0100

    wtf?

--- apps/barnepensjon-ui/src/components/application/summary/fragments/SummaryYourSituation.tsx
+++ apps/barnepensjon-ui/src/components/application/summary/fragments/SummaryYourSituation.tsx
@@ -1,38 +1,38 @@
-import { isEmpty } from "lodash";
-import { memo } from "react";
-import useTranslation from "../../../../hooks/useTranslation";
-import { ISituationChild } from "../../../../types/situation";
-import { StepLabelKey, StepPath } from "../../../../utils/steps";
-import { AccordionItem } from "../AccordionItem";
-import { TextGroup, TextGroupJaNeiVetIkke } from "../TextGroup";
+import { isEmpty } from 'lodash'
+import { memo } from 'react'
+import useTranslation from '../../../../hooks/useTranslation'
+import { ISituationChild } from '../../../../types/situation'
+import { StepLabelKey, StepPath } from '../../../../utils/steps'
+import { AccordionItem } from '../AccordionItem'
+import { TextGroup, TextGroupJaNeiVetIkke } from '../TextGroup'
 
 interface Props {
-    pathPrefix: string;
-    yourSituation?: ISituationChild;
+    pathPrefix: string
+    yourSituation?: ISituationChild
 }
 
 export const SummaryYourSituation = memo(({ yourSituation, pathPrefix }: Props) => {
-    const { t } = useTranslation("yourSituation");
+    const { t } = useTranslation('yourSituation')
 
-    if (!yourSituation || isEmpty(yourSituation)) return null;
+    if (!yourSituation || isEmpty(yourSituation)) return null
 
     return (
         <AccordionItem
-            title={t("title")}
+            title={t('title')}
             path={`/skjema/${pathPrefix}/${StepPath.YourSituation}`}
-            pathText={t(StepLabelKey.YourSituation, { ns: "summary" })}
+            pathText={t(StepLabelKey.YourSituation, { ns: 'summary' })}
         >
             <>
                 <TextGroup
-                    title={t("whyDoYouApply")}
+                    title={t('whyDoYouApply')}
                     content={yourSituation.whyDoYouApply?.map((item) => ` ${t(item)}`).toString()}
                 />
                 <TextGroup
-                    title={t("timeUsedForEducation")}
+                    title={t('timeUsedForEducation')}
                     content={t(yourSituation.timeUsedForEducation as string)}
                 />
-                <TextGroupJaNeiVetIkke title={t("doYouHaveIncome")} content={yourSituation.doYouHaveIncome} />
+                <TextGroupJaNeiVetIkke title={t('doYouHaveIncome')} content={yourSituation.doYouHaveIncome} />
             </>
         </AccordionItem>
-    );
-});
+    )
+})
