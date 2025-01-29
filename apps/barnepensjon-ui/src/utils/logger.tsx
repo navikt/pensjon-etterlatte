4fd856131bf48dedbe14056c2a6c030508b0e880
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-29 13:53:31 +0100

    Migrer over til biome

--- apps/barnepensjon-ui/src/utils/logger.tsx
+++ apps/barnepensjon-ui/src/utils/logger.tsx
@@ -1,42 +1,44 @@
-import Bowser from 'bowser'
-import {axiosInstance as api, isDev} from "~api/axios";
+import Bowser from "bowser";
+import { axiosInstance as api, isDev } from "~api/axios";
 
-const browser: any = Bowser.getParser(window.navigator.userAgent)
+const browser = Bowser.getParser(window.navigator.userAgent);
 
-const GYLDIG_FNR = (input: string | undefined) => /^\d{11}$/.test(input ?? '')
+const GYLDIG_FNR = (input: string | undefined) => /^\d{11}$/.test(input ?? "");
 
 function sanitizeUrlPossibleFnr(url?: string): string {
     if (url) {
-        const splittedUrl = url.split('/')
+        const splittedUrl = url.split("/");
         return splittedUrl
-                .map((urlpart) => {
-                    if (GYLDIG_FNR(urlpart)) {
-                        return urlpart.substring(0, 5).concat('******')
-                    }
-                    return urlpart
-                })
-                .join('/')
+            .map((urlpart) => {
+                if (GYLDIG_FNR(urlpart)) {
+                    return urlpart.substring(0, 5).concat("******");
+                }
+                return urlpart;
+            })
+            .join("/");
     }
-    return ''
+    return "";
 }
 
 const defaultContext = {
     url: sanitizeUrlPossibleFnr(window.location.href),
     userAgent: window.navigator.userAgent,
-    userDeviceInfo: browser.parsedResult,
-}
+    userDeviceInfo: { browser: browser.parse().getBrowser(), os: browser.parse().getOS() },
+};
 
 export interface IStackLineNoColumnNo {
-    readonly lineno: number
-    readonly columnno: number
-    readonly message: any
-    readonly error: any
+    readonly lineno: number;
+    readonly columnno: number;
+    readonly message: any;
+    readonly error: any;
 }
 
 export const loggFunc = async (data: any) => {
     if (isDev) {
-        console.log(`Logging til pod er deaktivert for lokal kjøring, returnerer uten å logge dit. Meldinga var: ${JSON.stringify(data)}`)
-        return
+        console.log(
+            `Logging til pod er deaktivert for lokal kjøring, returnerer uten å logge dit. Meldinga var: ${JSON.stringify(data)}`,
+        );
+        return;
     }
 
     try {
@@ -49,59 +51,54 @@ export const loggFunc = async (data: any) => {
 
 export const logger = {
     info: (stackLineNoColumnNo: IStackLineNoColumnNo) => {
-        const data = { type: 'info', stackInfo: stackLineNoColumnNo, jsonContent: { ...defaultContext } }
-        loggFunc(data)
-                .catch((err) => {
-                    console.error('Unable to log info message: ', data, ' err: ', err)
-                })
+        const data = { type: "info", stackInfo: stackLineNoColumnNo, jsonContent: { ...defaultContext } };
+        loggFunc(data).catch((err) => {
+            console.error("Unable to log info message: ", data, " err: ", err);
+        });
     },
     error: (stackLineNoColumnNo: IStackLineNoColumnNo) => {
-        const data = { type: 'error', stackInfo: stackLineNoColumnNo, jsonContent: { ...defaultContext } }
-        loggFunc(data)
-                .catch((err) => {
-                    console.error('Unable to log error message: ', data, ' err: ', err)
-                })
+        const data = { type: "error", stackInfo: stackLineNoColumnNo, jsonContent: { ...defaultContext } };
+        loggFunc(data).catch((err) => {
+            console.error("Unable to log error message: ", data, " err: ", err);
+        });
     },
     generalInfo: (info: object) => {
-        const data = { type: 'info', data: info, jsonContent: { ...defaultContext } }
-        loggFunc(data)
-                .catch((err) => {
-                    console.error('Unable to log info message: ', data, ' err: ', err)
-                })
+        const data = { type: "info", data: info, jsonContent: { ...defaultContext } };
+        loggFunc(data).catch((err) => {
+            console.error("Unable to log info message: ", data, " err: ", err);
+        });
     },
     generalError: (info: object) => {
-        const data = { type: 'error', data: info, jsonContent: { ...defaultContext } }
-        loggFunc(data)
-                .catch((err) => {
-                    console.error('Unable to log error message: ', data, ' err: ', err)
-                })
+        const data = { type: "error", data: info, jsonContent: { ...defaultContext } };
+        loggFunc(data).catch((err) => {
+            console.error("Unable to log error message: ", data, " err: ", err);
+        });
     },
-}
-
+};
 
 export const setupWindowOnError = () => {
-    addEventListener('error', (event) => {
-        const { error: kanskjeError, lineno, colno, message } = event
+    addEventListener("error", (event) => {
+        const { error: kanskjeError, lineno, colno, message } = event;
 
         //Ignorerer js som kræsjer fra andre domener
-        if(message.toLowerCase().includes('script error')) {
-            return true
+        if (message.toLowerCase().includes("script error")) {
+            return true;
         }
 
-        const error = kanskjeError || {}
-        if (import.meta.env.MODE === 'development') {
-            console.error(error.message, error.stack)
+        const error = kanskjeError || {};
+        if (import.meta.env.MODE === "development") {
+            console.error(error.message, error.stack);
         } else {
-            if (message !== 'ResizeObserver loop completed with undelivered notifications.') {
-                logger.error({ lineno, columnno: colno, message, error: JSON.stringify(error) })
+            if (message !== "ResizeObserver loop completed with undelivered notifications.") {
+                logger.error({ lineno, columnno: colno, message, error: JSON.stringify(error) });
             }
 
-            if (error.stack && error.stack?.indexOf('invokeGuardedCallbackDev') >= 0 && !error.alreadySeen) {
-                error.alreadySeen = true
-                event.preventDefault()
-                return true
+            if (error.stack && error.stack?.indexOf("invokeGuardedCallbackDev") >= 0 && !error.alreadySeen) {
+                error.alreadySeen = true;
+                event.preventDefault();
+                return true;
             }
         }
-        return true
-    })
-}
+        return true;
+    });
+};
