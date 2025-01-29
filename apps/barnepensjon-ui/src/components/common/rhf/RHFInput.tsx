3b6503c0d059dc1ea4e92f492ba277e423139f7d
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-29 14:17:06 +0100

    Migrer over til biome

--- apps/barnepensjon-ui/src/components/common/rhf/RHFInput.tsx
+++ apps/barnepensjon-ui/src/components/common/rhf/RHFInput.tsx
@@ -1,42 +1,30 @@
-import { ChangeEvent } from 'react'
-import { Controller, FieldError, useFormContext } from 'react-hook-form'
-import { FieldPath, FieldValues } from 'react-hook-form/dist/types'
-import { get } from 'lodash'
-import { RegisterOptions } from 'react-hook-form/dist/types/validator'
-import { fnr } from '@navikt/fnrvalidator'
-import {
-    kontonrMatcher,
-    partialKontonrMatcher,
-    partialProsentMatcher,
-    prosentMatcher,
-    telefonnrMatcher,
-} from '../../../utils/matchers'
-import useTranslation from '../../../hooks/useTranslation'
-import { getErrorKey } from '../../../utils/errors'
-import { isValidBIC, isValidIBAN } from 'ibantools'
-import { Textarea, TextareaProps, TextField, TextFieldProps } from '@navikt/ds-react'
+import { ChangeEvent } from "react";
+import { Controller, FieldError, useFormContext } from "react-hook-form";
+import { FieldPath, FieldValues } from "react-hook-form/dist/types";
+import { get } from "lodash";
+import { RegisterOptions } from "react-hook-form/dist/types/validator";
+import { fnr } from "@navikt/fnrvalidator";
+import { kontonrMatcher, partialKontonrMatcher, telefonnrMatcher } from "../../../utils/matchers";
+import useTranslation from "../../../hooks/useTranslation";
+import { getErrorKey } from "../../../utils/errors";
+import { isValidBIC, isValidIBAN } from "ibantools";
+import { TextField, TextFieldProps } from "@navikt/ds-react";
 
-interface RHFProps extends Omit<TextFieldProps, 'name'> {
-    name: FieldPath<FieldValues>
-    rules?: Omit<RegisterOptions<FieldValues, FieldPath<FieldValues>>, 'required'>
-    valgfri?: boolean
-}
-
-interface RHFInputAreaProps extends Omit<TextareaProps, 'name'> {
-    name: FieldPath<FieldValues>
-    rules?: Omit<RegisterOptions<FieldValues, FieldPath<FieldValues>>, 'required'>
-    valgfri?: boolean
+interface RHFProps extends Omit<TextFieldProps, "name"> {
+    name: FieldPath<FieldValues>;
+    rules?: Omit<RegisterOptions<FieldValues, FieldPath<FieldValues>>, "required">;
+    valgfri?: boolean;
 }
 
 export const RHFInput = ({ name, rules, valgfri, ...rest }: RHFProps) => {
-    const { t } = useTranslation('error')
+    const { t } = useTranslation("error");
     const {
         control,
         formState: { errors },
-    } = useFormContext()
+    } = useFormContext();
 
-    const error: FieldError = get(errors, name) as FieldError
-    const feilmelding = !!error ? t(getErrorKey(error)) : undefined
+    const error: FieldError = get(errors, name) as FieldError;
+    const feilmelding = !!error ? t(getErrorKey(error)) : undefined;
 
     return (
         <Controller
@@ -44,84 +32,43 @@ export const RHFInput = ({ name, rules, valgfri, ...rest }: RHFProps) => {
             control={control}
             rules={{ required: !valgfri, ...rules }}
             render={({ field: { value, onChange } }) => (
-                <TextField id={name} value={value || ''} onChange={onChange} error={feilmelding} {...rest} />
+                <TextField id={name} value={value || ""} onChange={onChange} error={feilmelding} {...rest} />
             )}
         />
-    )
-}
+    );
+};
 
-export const RHFInputArea = ({ name, rules, className, valgfri, description, ...rest }: RHFInputAreaProps) => {
-    const { t } = useTranslation()
-    const {
-        control,
-        formState: { errors },
-    } = useFormContext()
-
-    const error: FieldError = get(errors, name) as FieldError
-    const feilmelding = !!error ? t(getErrorKey(error)) : undefined
-
-    const i18nLocale = {
-        counterLeft: t('counterLeft', { ns: 'common' }),
-        counterTooMuch: t('counterTooMuch', { ns: 'common' }),
-    }
-
-    return (
-        <Controller
-            name={name}
-            control={control}
-            rules={{ required: !valgfri, ...rules }}
-            render={({ field: { value, onChange } }) => (
-                <div className={className}>
-                    <Textarea
-                        id={name}
-                        value={value || ''}
-                        onChange={onChange}
-                        error={feilmelding}
-                        {...rest}
-                        i18n={i18nLocale}
-                        description={
-                            description
-                                ? `${description}\n\n${t('noSensitiveData', { ns: 'common' })}`
-                                : t('noSensitiveData', { ns: 'common' })
-                        }
-                    />
-                </div>
-            )}
-        />
-    )
-}
-
-const match = (value: any, matcher: RegExp, separator: string) => {
-    const match = value.match(matcher)
+const match = (value: string, matcher: RegExp, separator: string) => {
+    const match = value.match(matcher);
 
     if (!!match) {
-        const del1 = match[1]
-        const del2 = match[2] ? `${separator}${match[2]}` : ''
-        const del3 = match[3] ? `${separator}${match[3]}` : ''
+        const del1 = match[1];
+        const del2 = match[2] ? `${separator}${match[2]}` : "";
+        const del3 = match[3] ? `${separator}${match[3]}` : "";
 
-        return `${del1}${del2}${del3}`
+        return `${del1}${del2}${del3}`;
     }
 
-    return undefined
-}
+    return undefined;
+};
 
-const format = (e: ChangeEvent<HTMLInputElement>, matcher: RegExp, separator = ' '): string => {
-    const value = e.target.value
+const format = (e: ChangeEvent<HTMLInputElement>, matcher: RegExp, separator = " "): string => {
+    const value = e.target.value;
 
-    const result = match(value, matcher, separator)
+    const result = match(value, matcher, separator);
 
-    return result || value.substring(0, value.length - 1)
-}
+    return result || value.substring(0, value.length - 1);
+};
 
 export const RHFKontonummerInput = ({ name, rules, ...rest }: RHFProps) => {
-    const { t } = useTranslation('error')
+    const { t } = useTranslation("error");
     const {
         control,
         formState: { errors },
-    } = useFormContext()
+    } = useFormContext();
 
-    const error: FieldError = get(errors, name) as FieldError
-    const feilmelding = !!error ? t(getErrorKey(error)) : undefined
+    const error: FieldError = get(errors, name) as FieldError;
+    const feilmelding = !!error ? t(getErrorKey(error)) : undefined;
 
     return (
         <Controller
@@ -131,87 +78,26 @@ export const RHFKontonummerInput = ({ name, rules, ...rest }: RHFProps) => {
             render={({ field: { value, onChange } }) => (
                 <TextField
                     id={name}
-                    value={value || ''}
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(format(e, partialKontonrMatcher, '.'))}
+                    value={value || ""}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(format(e, partialKontonrMatcher, "."))}
                     error={feilmelding}
                     {...rest}
                     inputMode="numeric"
                 />
             )}
         />
-    )
-}
-
-export const RHFValutaInput = ({ name, valgfri, ...rest }: RHFProps) => {
-    const { t } = useTranslation('error')
-    const {
-        control,
-        formState: { errors },
-    } = useFormContext()
-
-    const error: FieldError = get(errors, name) as FieldError
-    const feilmelding = !!error ? t(getErrorKey(error)) : undefined
-
-    return (
-        <Controller
-            name={name}
-            control={control}
-            rules={{ required: !valgfri, pattern: /^\d[0-9\s]*$/ }}
-            render={({ field: { value, onChange } }) => (
-                <TextField id={name} value={value || ''} onChange={onChange} error={feilmelding} {...rest} />
-            )}
-        />
-    )
-}
-
-export const RHFProsentInput = ({ name, rules, ...rest }: RHFProps) => {
-    const { t } = useTranslation('error')
-    const {
-        control,
-        formState: { errors },
-    } = useFormContext()
-
-    const error: FieldError = get(errors, name) as FieldError
-    const feilmelding = !!error ? t(getErrorKey(error)) : undefined
-    const maxLength = 4
-
-    const isValid = (e: ChangeEvent<HTMLInputElement>): boolean => {
-        return (
-            e.target.value === '' ||
-            (partialProsentMatcher.test(e.target.value) && (!maxLength || e.target.value.length <= maxLength))
-        )
-    }
-
-    return (
-        <Controller
-            name={name}
-            control={control}
-            rules={{ required: true, pattern: prosentMatcher, ...rules }}
-            render={({ field: { value, onChange } }) => (
-                <TextField
-                    id={name}
-                    value={value || ''}
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                        if (isValid(e)) onChange(e)
-                    }}
-                    error={feilmelding}
-                    {...rest}
-                    inputMode="numeric"
-                />
-            )}
-        />
-    )
-}
+    );
+};
 
 export const RHFTelefonInput = ({ name, rules, valgfri, ...rest }: RHFProps) => {
-    const { t } = useTranslation('error')
+    const { t } = useTranslation("error");
     const {
         control,
         formState: { errors },
-    } = useFormContext()
+    } = useFormContext();
 
-    const error: FieldError = get(errors, name) as FieldError
-    const feilmelding = !!error ? t(getErrorKey(error)) : undefined
+    const error: FieldError = get(errors, name) as FieldError;
+    const feilmelding = !!error ? t(getErrorKey(error)) : undefined;
 
     return (
         <Controller
@@ -226,7 +112,7 @@ export const RHFTelefonInput = ({ name, rules, valgfri, ...rest }: RHFProps) => 
             render={({ field: { value, onChange } }) => (
                 <TextField
                     id={name}
-                    value={value || ''}
+                    value={value || ""}
                     onChange={onChange}
                     error={feilmelding}
                     {...rest}
@@ -234,24 +120,24 @@ export const RHFTelefonInput = ({ name, rules, valgfri, ...rest }: RHFProps) => 
                 />
             )}
         />
-    )
-}
+    );
+};
 
 export const RHFFoedselsnummerInput = ({ name, rules, valgfri, ...rest }: RHFProps) => {
-    const { t } = useTranslation('error')
+    const { t } = useTranslation("error");
     const {
         control,
         formState: { errors },
-    } = useFormContext()
+    } = useFormContext();
 
-    const error: FieldError = get(errors, name) as FieldError
-    const feilmelding = !!error ? t(getErrorKey(error)) : undefined
+    const error: FieldError = get(errors, name) as FieldError;
+    const feilmelding = !!error ? t(getErrorKey(error)) : undefined;
 
     const isValid = (e: ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value
+        const value = e.target.value;
 
-        return !isNaN(Number(value)) && value.length <= 11
-    }
+        return !isNaN(Number(value)) && value.length <= 11;
+    };
 
     return (
         <Controller
@@ -259,16 +145,16 @@ export const RHFFoedselsnummerInput = ({ name, rules, valgfri, ...rest }: RHFPro
             control={control}
             rules={{
                 required: !valgfri,
-                validate: (value) => !value || fnr(value).status === 'valid',
+                validate: (value) => !value || fnr(value).status === "valid",
                 ...rules,
             }}
             render={({ field: { value, onChange } }) => (
                 <TextField
                     id={name}
                     type="tel"
-                    value={value || ''}
+                    value={value || ""}
                     onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                        if (isValid(e)) onChange(e)
+                        if (isValid(e)) onChange(e);
                     }}
                     error={feilmelding}
                     {...rest}
@@ -276,18 +162,18 @@ export const RHFFoedselsnummerInput = ({ name, rules, valgfri, ...rest }: RHFPro
                 />
             )}
         />
-    )
-}
+    );
+};
 
 export const RHFIbanInput = ({ name, ...rest }: RHFProps) => {
-    const { t } = useTranslation('error')
+    const { t } = useTranslation("error");
     const {
         control,
         formState: { errors },
-    } = useFormContext()
+    } = useFormContext();
 
-    const error: FieldError = get(errors, name) as FieldError
-    const feilmelding = !!error ? t(getErrorKey(error)) : undefined
+    const error: FieldError = get(errors, name) as FieldError;
+    const feilmelding = !!error ? t(getErrorKey(error)) : undefined;
 
     return (
         <Controller
@@ -297,25 +183,25 @@ export const RHFIbanInput = ({ name, ...rest }: RHFProps) => {
             render={({ field: { value, onChange } }) => (
                 <TextField
                     id={name}
-                    value={value || ''}
+                    value={value || ""}
                     onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e.target.value.toUpperCase())}
                     error={feilmelding}
                     {...rest}
                 />
             )}
         />
-    )
-}
+    );
+};
 
 export const RHFBicInput = ({ name, ...rest }: RHFProps) => {
-    const { t } = useTranslation('error')
+    const { t } = useTranslation("error");
     const {
         control,
         formState: { errors },
-    } = useFormContext()
+    } = useFormContext();
 
-    const error: FieldError = get(errors, name) as FieldError
-    const feilmelding = !!error ? t(getErrorKey(error)) : undefined
+    const error: FieldError = get(errors, name) as FieldError;
+    const feilmelding = !!error ? t(getErrorKey(error)) : undefined;
 
     return (
         <Controller
@@ -325,32 +211,32 @@ export const RHFBicInput = ({ name, ...rest }: RHFProps) => {
             render={({ field: { value, onChange } }) => (
                 <TextField
                     id={name}
-                    value={value || ''}
+                    value={value || ""}
                     onChange={(e: ChangeEvent<HTMLInputElement>) => onChange(e.target.value.toUpperCase())}
                     error={feilmelding}
                     {...rest}
                 />
             )}
         />
-    )
-}
+    );
+};
 
 export const RHFNumberInput = ({ name, minLength, maxLength, required = true, label, ...rest }: RHFProps) => {
-    const { t } = useTranslation('error')
+    const { t } = useTranslation("error");
     const {
         control,
         formState: { errors },
-    } = useFormContext()
+    } = useFormContext();
 
-    const error: FieldError = get(errors, name) as FieldError
-    const feilmelding = !!error ? t(getErrorKey(error)) : undefined
+    const error: FieldError = get(errors, name) as FieldError;
+    const feilmelding = !!error ? t(getErrorKey(error)) : undefined;
 
-    const re = /^[0-9\b]+$/
+    const re = /^[0-9\b]+$/;
     const isValid = (e: ChangeEvent<HTMLInputElement>): boolean => {
-        return e.target.value === '' || (re.test(e.target.value) && (!maxLength || e.target.value.length <= maxLength))
-    }
+        return e.target.value === "" || (re.test(e.target.value) && (!maxLength || e.target.value.length <= maxLength));
+    };
 
-    const labelWithOptional = `${label} ${required ? '' : `(${t('optional', { ns: 'common' })})`}`
+    const labelWithOptional = `${label} ${required ? "" : `(${t("optional", { ns: "common" })})`}`;
 
     return (
         <Controller
@@ -360,10 +246,10 @@ export const RHFNumberInput = ({ name, minLength, maxLength, required = true, la
             render={({ field: { value, onChange } }) => (
                 <TextField
                     id={name}
-                    value={value || ''}
+                    value={value || ""}
                     label={labelWithOptional}
                     onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                        if (isValid(e)) onChange(e)
+                        if (isValid(e)) onChange(e);
                     }}
                     error={feilmelding}
                     inputMode="numeric"
@@ -371,5 +257,5 @@ export const RHFNumberInput = ({ name, minLength, maxLength, required = true, la
                 />
             )}
         />
-    )
-}
+    );
+};
