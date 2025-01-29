079e75a2a118b510e149550b7c6b51d67f0a2cb1
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-29 15:09:44 +0100

    wtf?

--- apps/barnepensjon-ui/src/components/application/summary/fragments/SummaryAboutDeceasedParent.tsx
+++ apps/barnepensjon-ui/src/components/application/summary/fragments/SummaryAboutDeceasedParent.tsx
@@ -1,31 +1,31 @@
-import { Panel, Tag } from '@navikt/ds-react'
-import { isEmpty } from 'lodash'
-import { memo } from 'react'
-import { v4 as uuid } from 'uuid'
-import { IDeceasedParent } from '../../../../context/application/application'
-import useTranslation from '../../../../hooks/useTranslation'
-import { StepLabelKey, StepPath } from '../../../../utils/steps'
-import FormElement from '../../../common/FormElement'
-import { AccordionItem } from '../AccordionItem'
-import { TextGroup, TextGroupJaNeiVetIkke } from '../TextGroup'
-import PersonInfoSummary from './PersonInfoSummary'
+import { Panel, Tag } from "@navikt/ds-react";
+import { isEmpty } from "lodash";
+import { memo } from "react";
+import { v4 as uuid } from "uuid";
+import { IDeceasedParent } from "../../../../context/application/application";
+import useTranslation from "../../../../hooks/useTranslation";
+import { StepLabelKey, StepPath } from "../../../../utils/steps";
+import FormElement from "../../../common/FormElement";
+import { AccordionItem } from "../AccordionItem";
+import { TextGroup, TextGroupJaNeiVetIkke } from "../TextGroup";
+import PersonInfoSummary from "./PersonInfoSummary";
 
 interface Props {
-    aboutTheParent: IDeceasedParent
-    pathPrefix: string
+    aboutTheParent: IDeceasedParent;
+    pathPrefix: string;
 }
 
 export const SummaryAboutDeceasedParent = memo(({ aboutTheParent, pathPrefix }: Props) => {
-    const { t } = useTranslation('aboutTheDeceased')
-    if (!aboutTheParent || isEmpty(aboutTheParent)) return null
-    const isVergeOrBarn = pathPrefix === 'verge' || pathPrefix === 'barn'
+    const { t } = useTranslation("aboutTheDeceased");
+    if (!aboutTheParent || isEmpty(aboutTheParent)) return null;
+    const isVergeOrBarn = pathPrefix === "verge" || pathPrefix === "barn";
 
     return (
         <AccordionItem
-            title={t('singleParentTitle')}
+            title={t("singleParentTitle")}
             path={`/skjema/${pathPrefix}/${isVergeOrBarn ? StepPath.AboutTheParents : StepPath.AboutTheDeceased}`}
             pathText={t(isVergeOrBarn ? StepLabelKey.AboutTheParents : StepLabelKey.AboutTheDeceased, {
-                ns: 'summary',
+                ns: "summary",
             })}
         >
             <FormElement>
@@ -36,32 +36,37 @@ export const SummaryAboutDeceasedParent = memo(({ aboutTheParent, pathPrefix }: 
                     dateOfBirth={aboutTheParent.dateOfBirth}
                     citizenship={aboutTheParent.citizenship}
                 />
-                <TextGroup title={t('dateOfDeath')} content={aboutTheParent.dateOfDeath} />
-                <TextGroupJaNeiVetIkke title={t('occupationalInjury')} content={aboutTheParent.occupationalInjury} />
+                <TextGroup title={t("dateOfDeath")} content={aboutTheParent.dateOfDeath.toString()} />
+                <TextGroupJaNeiVetIkke title={t("occupationalInjury")} content={aboutTheParent.occupationalInjury} />
                 <TextGroupJaNeiVetIkke
-                    title={t('didTheDeceasedLiveAbroad')}
+                    title={t("didTheDeceasedLiveAbroad")}
                     content={aboutTheParent.staysAbroad.hasStaysAbroad}
                 />
                 {aboutTheParent.staysAbroad.abroadStays?.map((stay) => (
                     <Panel key={uuid()}>
-                        <Tag variant={'neutral-moderate'}>{`Opphold i ${stay.country}`}</Tag>
-                        <TextGroup title={t('abroadInWhichCountry')} content={stay.country} />
-                        <TextGroup title={t('livedOrWorkedAbroad')} content={stay.type?.map((item) => ` ${t(item)}`)} />
-                        {stay.toDate && <TextGroup title={t('stayedAbroadToDate')} content={stay.toDate} />}
-                        {stay.fromDate && <TextGroup title={t('stayedAbroadFromDate')} content={stay.fromDate} />}
+                        <Tag variant={"neutral-moderate"}>{`Opphold i ${stay.country}`}</Tag>
+                        <TextGroup title={t("abroadInWhichCountry")} content={stay.country} />
+                        <TextGroup
+                            title={t("livedOrWorkedAbroad")}
+                            content={stay.type?.map((item) => ` ${t(item)}`).toString()}
+                        />
+                        {stay.toDate && <TextGroup title={t("stayedAbroadToDate")} content={stay.toDate.toString()} />}
+                        {stay.fromDate && (
+                            <TextGroup title={t("stayedAbroadFromDate")} content={stay.fromDate.toString()} />
+                        )}
                         <TextGroupJaNeiVetIkke
-                            title={t('deceasedWasMemberOfFolketrygdenAbroad')}
+                            title={t("deceasedWasMemberOfFolketrygdenAbroad")}
                             content={stay.medlemFolketrygd}
                         />
 
-                        {stay.pension.amount && <TextGroup title={t('amountAbroad')} content={stay.pension.amount} />}
+                        {stay.pension.amount && <TextGroup title={t("amountAbroad")} content={stay.pension.amount} />}
 
                         {stay.pension.currency && (
-                            <TextGroup title={t('chooseCurrency', { ns: 'common' })} content={stay.pension.currency} />
+                            <TextGroup title={t("chooseCurrency", { ns: "common" })} content={stay.pension.currency} />
                         )}
                     </Panel>
                 ))}
             </FormElement>
         </AccordionItem>
-    )
-})
+    );
+});
