aab64e524cfb071543a3003d6556229bb0691dab
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-29 14:55:17 +0100

    Fikk ikke med

--- apps/barnepensjon-ui/cypress/support/index.d.ts
+++ apps/barnepensjon-ui/cypress/support/index.d.ts
@@ -1,14 +1,14 @@
 declare namespace Cypress {
     // biome-ignore lint/suspicious/noExplicitAny: gamle tester, gidder ikke fikse
     interface Chainable<Subject = any> {
-        startApplication(userFixture: string): Chainable<null>;
-        agreeToTerms(gotoNext?: boolean): Chainable<null>;
-        useScenario(role: string, situation?: string, gotoNext?: boolean): Chainable<null>;
-        acceptInformationAboutYou(): Chainable<null>;
-        useSimpleLiving(gotoNext?: boolean): Chainable<null>;
-        useSimpleDeceased(gotoNext?: boolean): Chainable<null>;
-        useAdvancedDeceased(gotoNext?: boolean): Chainable<null>;
-        addChild(gotoNext?: boolean): Chainable<null>;
-        clickBtn(btn: string, index?: number): Chainable<null>;
+        startApplication(userFixture: string): Chainable<null>
+        agreeToTerms(gotoNext?: boolean): Chainable<null>
+        useScenario(role: string, situation?: string, gotoNext?: boolean): Chainable<null>
+        acceptInformationAboutYou(): Chainable<null>
+        useSimpleLiving(gotoNext?: boolean): Chainable<null>
+        useSimpleDeceased(gotoNext?: boolean): Chainable<null>
+        useAdvancedDeceased(gotoNext?: boolean): Chainable<null>
+        addChild(gotoNext?: boolean): Chainable<null>
+        clickBtn(btn: string, index?: number): Chainable<null>
     }
 }
