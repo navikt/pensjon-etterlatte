efa237e57fd51d028e6e26bdd1ea92c62fd1cf14
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-30 09:45:02 +0100

    Revert test

--- apps/barnepensjon-ui/src/components/application/about-children/ChildInfocard.tsx
+++ apps/barnepensjon-ui/src/components/application/about-children/ChildInfocard.tsx
@@ -1,26 +1,25 @@
-import { DeleteFilled, EditFilled } from '@navikt/ds-icons'
-import { BodyShort, Box, Button, Heading, Tag, VStack } from '@navikt/ds-react'
-import { memo } from 'react'
-import { JaNeiVetIkke } from '../../../api/dto/FellesOpplysninger'
-import ikon from '../../../assets/barn1.svg'
-import useTranslation from '../../../hooks/useTranslation'
-import { Infocard, InfocardHeader, InformationBox, InformationBoxContent } from '../../common/card/InfoCard'
-import { IChild } from '../../../types/person'
-import FormElement from '../../common/FormElement'
-import { format } from 'date-fns'
+import { DeleteFilled, EditFilled } from "@navikt/ds-icons";
+import { BodyShort, Box, Button, Heading, Tag, VStack } from "@navikt/ds-react";
+import { format } from "date-fns";
+import { memo } from "react";
+import { JaNeiVetIkke } from "../../../api/dto/FellesOpplysninger";
+import ikon from "../../../assets/barn1.svg";
+import useTranslation from "../../../hooks/useTranslation";
+import { IChild } from "../../../types/person";
+import FormElement from "../../common/FormElement";
+import { Infocard, InfocardHeader, InformationBox, InformationBoxContent } from "../../common/card/InfoCard";
 
 interface Props {
-    child: IChild
-    index: number
-    remove: (index: number) => void
-    setActiveChildIndex: () => void
+    child: IChild;
+    index: number;
+    remove: (index: number) => void;
+    setActiveChildIndex: () => void;
 }
 
 const ChildInfoCard = memo(({ child, index, remove, setActiveChildIndex }: Props) => {
-    const { t } = useTranslation('aboutChildren')
+    const { t } = useTranslation("aboutChildren");
 
-    const foedselsnummer = child.fnrDnr?.replace(/(\d{6})(.*)/, '$1 $2')
-
+    const foedselsnummer = child.fnrDnr?.replace(/(\d{6})(.*)/, "$1 $2");
     return (
         <Infocard>
             <InfocardHeader>
@@ -29,42 +28,42 @@ const ChildInfoCard = memo(({ child, index, remove, setActiveChildIndex }: Props
 
             <InformationBox>
                 <InformationBoxContent>
-                    <Heading size={'small'}>
+                    <Heading size={"small"}>
                         {child.firstName} {child.lastName}
                     </Heading>
                 </InformationBoxContent>
                 <Box paddingBlock="2">
                     {foedselsnummer && (
                         <>
-                            <BodyShort>{t('infoCard_fnr')}</BodyShort>
+                            <BodyShort>{t("infoCard_fnr")}</BodyShort>
                             <BodyShort spacing>{foedselsnummer}</BodyShort>
                         </>
                     )}
                     {child.dateOfBirth && (
                         <>
-                            <BodyShort>{t('infoCard_dob')}</BodyShort>
-                            <BodyShort spacing>{format(child.dateOfBirth, 'dd.MM.yyyy')}</BodyShort>
+                            <BodyShort>{t("infoCard_dob")}</BodyShort>
+                            <BodyShort spacing>{format(child.dateOfBirth, "dd.MM.yyyy")}</BodyShort>
                         </>
                     )}
 
-                    <BodyShort>{t('infoCard_citizenship')}</BodyShort>
+                    <BodyShort>{t("infoCard_citizenship")}</BodyShort>
                     <BodyShort spacing>{child.citizenship}</BodyShort>
 
-                    <BodyShort>{t('infoCard_residence')}</BodyShort>
+                    <BodyShort>{t("infoCard_residence")}</BodyShort>
                     <BodyShort spacing>
-                        {t('livesIn')}&nbsp;
+                        {t("livesIn")}&nbsp;
                         {child.staysAbroad?.answer === JaNeiVetIkke.JA
                             ? child.staysAbroad?.country
-                            : t('norway', { ns: 'common' })}
+                            : t("norway", { ns: "common" })}
                     </BodyShort>
 
                     {!!child.appliesForChildrensPension ? (
                         <FormElement>
-                            <Tag variant={'success'}>{t('childAppliedForPension')}</Tag>
+                            <Tag variant={"success"}>{t("childAppliedForPension")}</Tag>
                         </FormElement>
                     ) : (
                         <FormElement>
-                            <Tag variant={'warning'}>{t('childNotApplyingForPension')}</Tag>
+                            <Tag variant={"warning"}>{t("childNotApplyingForPension")}</Tag>
                         </FormElement>
                     )}
                 </Box>
@@ -75,7 +74,7 @@ const ChildInfoCard = memo(({ child, index, remove, setActiveChildIndex }: Props
                         onClick={setActiveChildIndex}
                         variant="tertiary"
                     >
-                        {t('editButton', { ns: 'btn' })}
+                        {t("editButton", { ns: "btn" })}
                     </Button>
                     <Button
                         type="button"
@@ -83,12 +82,12 @@ const ChildInfoCard = memo(({ child, index, remove, setActiveChildIndex }: Props
                         onClick={() => remove(index)}
                         variant="tertiary"
                     >
-                        {t('removeChildButton')}
+                        {t("removeChildButton")}
                     </Button>
                 </VStack>
             </InformationBox>
         </Infocard>
-    )
-})
+    );
+});
 
-export default ChildInfoCard
+export default ChildInfoCard;
