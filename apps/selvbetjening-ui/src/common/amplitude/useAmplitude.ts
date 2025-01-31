f574e160a50e7d7585a3598e3884f48468e35e21
Emil Schrøder <emil.schroder@nav.no>
2025-01-31 09:19:59 +0100

    Ta i bruk dekoratoren sin amplitude - Selvbetjening (#1744)
    * Ta i bruk dekoratoren sin amplitude
    
    * Fiks linting

--- apps/selvbetjening-ui/src/common/amplitude/useAmplitude.ts
+++ apps/selvbetjening-ui/src/common/amplitude/useAmplitude.ts
@@ -1,5 +1,5 @@
-import * as amplitude from '@amplitude/analytics-browser'
-import { useEffect } from 'react'
+import { getAmplitudeInstance } from '@navikt/nav-dekoratoren-moduler'
+import { useCallback } from 'react'
 
 // Felles taksonomi for analytics https://github.com/navikt/analytics-taxonomy
 export enum LogEvents {
@@ -7,40 +7,15 @@ export enum LogEvents {
     INGEN_OMS_SAK = 'ingen oms sak',
 }
 
-const getAmplitudeKey = () => {
-    if (window.location.href.includes('dev.nav.no')) return 'b0ea5ed50acc6bdf505e3f6cdf76b99d' // dev
-    if (window.location.href.includes('nav.no')) return '10798841ebeba333b8ece6c046322d76' // prod
-    return '119ddb1e1aa52564d90038ac65926a7d' // other e.g. localhost
-}
-
 export const useAmplitude = () => {
-    useEffect(() => {
-        amplitude.init(getAmplitudeKey(), '', {
-            serverUrl: 'https://amplitude.nav.no/collect-auto',
-            ingestionMetadata: {
-                sourceName: window.location.toString(),
-            },
-            autocapture: {
-                attribution: false,
-                pageViews: true,
-                sessions: true,
-                formInteractions: false,
-                fileDownloads: false,
-            },
-        })
-    }, [])
+    const track = getAmplitudeInstance('dekoratoren')
 
-    const logEvent = (eventName: LogEvents, eventData: object): void => {
-        setTimeout(() => {
-            try {
-                if (amplitude) {
-                    amplitude.logEvent(eventName, eventData)
-                }
-            } catch (error) {
-                console.error(error)
-            }
-        }, 0)
-    }
+    const logEvent = useCallback(
+        <T extends object>(eventName: string, eventData: T = {} as T) => {
+            track(eventName, eventData).catch((error) => console.error(error))
+        },
+        [track]
+    )
 
     return { logEvent }
 }
