f33ebb93e3c6d8b5d410a23cf8dbe4e94abfcbc7
Emil Schrøder <emil.schroder@nav.no>
2025-01-31 08:21:57 +0100

    Fiks linting

--- apps/selvbetjening-ui/src/common/amplitude/useAmplitude.ts
+++ apps/selvbetjening-ui/src/common/amplitude/useAmplitude.ts
@@ -1,21 +1,21 @@
-import { getAmplitudeInstance } from "@navikt/nav-dekoratoren-moduler";
-import { useCallback } from "react";
+import { getAmplitudeInstance } from '@navikt/nav-dekoratoren-moduler'
+import { useCallback } from 'react'
 
 // Felles taksonomi for analytics https://github.com/navikt/analytics-taxonomy
 export enum LogEvents {
-    ALDER = "alder",
-    INGEN_OMS_SAK = "ingen oms sak",
+    ALDER = 'alder',
+    INGEN_OMS_SAK = 'ingen oms sak',
 }
 
 export const useAmplitude = () => {
-    const track = getAmplitudeInstance("dekoratoren");
+    const track = getAmplitudeInstance('dekoratoren')
 
     const logEvent = useCallback(
         <T extends object>(eventName: string, eventData: T = {} as T) => {
-            track(eventName, eventData).catch((error) => console.error(error));
+            track(eventName, eventData).catch((error) => console.error(error))
         },
-        [track],
-    );
+        [track]
+    )
 
-    return { logEvent };
-};
+    return { logEvent }
+}
