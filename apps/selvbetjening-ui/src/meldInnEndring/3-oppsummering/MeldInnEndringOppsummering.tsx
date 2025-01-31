03c8bd09a3803984b37cb84e7deeaba590473402
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-01-31 14:50:58 +0100

    Legg til scroll til topp ved oppsummering

--- apps/selvbetjening-ui/src/meldInnEndring/3-oppsummering/MeldInnEndringOppsummering.tsx
+++ apps/selvbetjening-ui/src/meldInnEndring/3-oppsummering/MeldInnEndringOppsummering.tsx
@@ -1,5 +1,5 @@
 import { BodyLong, FormSummary, GuidePanel, HStack, VStack } from '@navikt/ds-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { SideLaster } from '../../common/SideLaster.tsx'
 import { apiURL, poster } from '../../common/api/api.ts'
@@ -35,6 +35,10 @@ export const MeldInnEndringOppsummering = () => {
     if (innholdError) {
         throw innholdError
     }
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+    }, [])
 
     const sendInnEndring = async () => {
         setLaster(true)
