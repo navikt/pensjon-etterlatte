d27cc205feb0e86e54e1df6285db30eb120105d3
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-02-25 08:36:55 +0100

    Amplitude tracking på lenker i selvbetjening

--- apps/selvbetjening-ui/src/common/sanity/SanityRikTekst.tsx
+++ apps/selvbetjening-ui/src/common/sanity/SanityRikTekst.tsx
@@ -1,5 +1,6 @@
 import { Heading, Link, List } from '@navikt/ds-react'
 import { PortableText, PortableTextComponents } from '@portabletext/react'
+import { EventType, LogEvents, useAmplitude } from '../amplitude/useAmplitude.ts'
 
 interface PortableTextBlock {
     children?: Array<{
@@ -20,52 +21,59 @@ interface PortableTextBlock {
     _key: string
 }
 
-const portableTextTilAkselKomponenter: PortableTextComponents = {
-    marks: {
-        a: ({ children, value }) => (
-            <Link href={value.href} target="_blank" rel="noopener noreferrer">
-                {children}
-            </Link>
-        ),
-    },
-    block: {
-        h1: ({ children }) => (
-            <Heading size="xlarge" level="1">
-                {children}
-            </Heading>
-        ),
-        h2: ({ children }) => (
-            <Heading size="large" level="2">
-                {children}
-            </Heading>
-        ),
-        h3: ({ children }) => (
-            <Heading size="medium" level="3">
-                {children}
-            </Heading>
-        ),
-        h4: ({ children }) => (
-            <Heading size="small" level="4">
-                {children}
-            </Heading>
-        ),
-        h5: ({ children }) => (
-            <Heading size="xsmall" level="5">
-                {children}
-            </Heading>
-        ),
-        h6: ({ children }) => <h6>{children}</h6>,
-    },
-    list: {
-        bullet: ({ children }) => <List as="ul">{children}</List>,
-        number: ({ children }) => <List as="ol">{children}</List>,
-    },
-    listItem: {
-        bullet: ({ children }) => <List.Item>{children}</List.Item>,
-        number: ({ children }) => <List.Item>{children}</List.Item>,
-    },
-}
-
 export const SanityRikTekst = ({ text }: { text: Array<PortableTextBlock> | undefined }) => {
+    const { logEvent } = useAmplitude()
+
+    const portableTextTilAkselKomponenter: PortableTextComponents = {
+        marks: {
+            a: ({ children, value }) => (
+                <Link
+                    href={value.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={() => logEvent(LogEvents.KLIKK, { type: EventType.LENKE_KLIKK, href: value.href })}
+                >
+                    {children}
+                </Link>
+            ),
+        },
+        block: {
+            h1: ({ children }) => (
+                <Heading size="xlarge" level="1">
+                    {children}
+                </Heading>
+            ),
+            h2: ({ children }) => (
+                <Heading size="large" level="2">
+                    {children}
+                </Heading>
+            ),
+            h3: ({ children }) => (
+                <Heading size="medium" level="3">
+                    {children}
+                </Heading>
+            ),
+            h4: ({ children }) => (
+                <Heading size="small" level="4">
+                    {children}
+                </Heading>
+            ),
+            h5: ({ children }) => (
+                <Heading size="xsmall" level="5">
+                    {children}
+                </Heading>
+            ),
+            h6: ({ children }) => <h6>{children}</h6>,
+        },
+        list: {
+            bullet: ({ children }) => <List as="ul">{children}</List>,
+            number: ({ children }) => <List as="ol">{children}</List>,
+        },
+        listItem: {
+            bullet: ({ children }) => <List.Item>{children}</List.Item>,
+            number: ({ children }) => <List.Item>{children}</List.Item>,
+        },
+    }
+
     return !!text && <PortableText value={text} components={portableTextTilAkselKomponenter} />
 }
