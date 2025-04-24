34f37ad2ee29669ed8ebe5177f2f1cf49a2ba7a6
Emil Elton Nilsen <emil.nilsen9888@gmail.com>
2025-04-24 10:12:08 +0200

    Fjern react-cookie fra OMS

--- apps/omstillingsstoenad-ui/src/hooks/useLanguage.tsx
+++ apps/omstillingsstoenad-ui/src/hooks/useLanguage.tsx
@@ -1,15 +1,10 @@
-import { useEffect } from 'react'
-import i18next from '../i18n'
-import { useSoknadContext } from '../context/soknad/SoknadContext'
-import { useCookies } from 'react-cookie'
 import { setParams } from '@navikt/nav-dekoratoren-moduler'
-
-const dekoratorLanguageCookieName = 'decorator-language'
+import { useEffect } from 'react'
+import { useSoknadContext } from '../context/soknad/SoknadContext'
+import i18next from '../i18n'
 
 export const useLanguage = () => {
     const { state: soeknadState } = useSoknadContext()
-    const [cookies, setCookie] = useCookies([dekoratorLanguageCookieName])
-    const { [dekoratorLanguageCookieName]: dekoratørSpraak } = cookies
 
     useEffect(() => {
         i18next.changeLanguage(soeknadState.spraak, (err, t) => {
@@ -17,10 +12,5 @@ export const useLanguage = () => {
             t('key')
         })
         setParams({ language: soeknadState.spraak }).then()
-        setCookie(dekoratorLanguageCookieName, soeknadState.spraak)
     }, [soeknadState.spraak])
-
-    useEffect(() => {
-        if (!dekoratørSpraak) setCookie(dekoratorLanguageCookieName, soeknadState.spraak)
-    }, [])
 }
