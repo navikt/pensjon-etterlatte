6e5bc82a8612918078abdb8f7c53e354b7561482
Emil Elton Nilsen <emil.elton.nilsen@proton.me>
2025-06-27 09:50:22 +0200

    Ikke gjem unleash bak selvbetjening sjekk

--- apps/etterlatte-node-server/src/routers/unleashRouter.ts
+++ apps/etterlatte-node-server/src/routers/unleashRouter.ts
@@ -14,44 +14,43 @@ export enum FeatureToggleStatus {
     HENTING_FEILET = 'HENTING_FEILET',
 }
 
-if (config.env.isSelvbetjeningUIApp) {
-    unleashRouter.post('/', express.json(), (req: Request, res: Response) => {
-        const featureTogglesNavn: string[] = req.body
+unleashRouter.post('/', express.json(), (req: Request, res: Response) => {
+    const featureTogglesNavn: string[] = req.body
 
-        const statusPaaFeatureToggle = (name: string): FeatureToggleStatus => {
-            const context: Context = {
-                appName: config.featureToggle.applicationName,
-                environment: process.env.NAIS_CLUSTER_NAME,
-            }
-            if (!unleash) {
-                return FeatureToggleStatus.UDEFINERT
-            }
-            try {
-                if (unleash.isEnabled(name, context)) {
-                    return FeatureToggleStatus.PAA
-                } else {
-                    return FeatureToggleStatus.AV
-                }
-            } catch (e) {
-                logger.error({
-                    message: `Fikk feilmelding fra Unleash for toggle ${sanitize(name)}, bruker defaultverdi false`,
-                    stack_trace: JSON.stringify(e),
-                })
-                return FeatureToggleStatus.HENTING_FEILET
-            }
+    const statusPaaFeatureToggle = (name: string): FeatureToggleStatus => {
+        const context: Context = {
+            appName: config.featureToggle.applicationName,
+            environment: process.env.NAIS_CLUSTER_NAME,
         }
-
-        res.json(
-            featureTogglesNavn.map((toggle) => {
-                const status = statusPaaFeatureToggle(toggle)
-
-                logger.info(`${sanitize(toggle)} enabled: ${status}`)
-
-                return {
-                    name: toggle,
-                    status,
-                }
+        if (!unleash) {
+            return FeatureToggleStatus.UDEFINERT
+        }
+        try {
+            if (unleash.isEnabled(name, context)) {
+                return FeatureToggleStatus.PAA
+            } else {
+                return FeatureToggleStatus.AV
+            }
+        } catch (e) {
+            logger.error({
+                message: `Fikk feilmelding fra Unleash for toggle ${sanitize(name)}, bruker defaultverdi false`,
+                stack_trace: JSON.stringify(e),
             })
-        )
-    })
-}
+            return FeatureToggleStatus.HENTING_FEILET
+        }
+    }
+
+    res.json(
+        featureTogglesNavn.map((toggle) => {
+            const status = statusPaaFeatureToggle(toggle)
+
+            logger.info(`${sanitize(toggle)} enabled: ${status}`)
+
+            return {
+                name: toggle,
+                status,
+            }
+        })
+    )
+})
+
