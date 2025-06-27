6144c8b7a6fa7a1249e14b77f8b93c2e53e522e6
Emil Elton Nilsen <emil.elton.nilsen@proton.me>
2025-06-27 09:19:46 +0200

    Ta i bruk context for feature toggle

--- apps/omstillingsstoenad-ui/src/context/featureToggle/FeatureToggleContext.tsx
+++ apps/omstillingsstoenad-ui/src/context/featureToggle/FeatureToggleContext.tsx
@@ -1,3 +1,6 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react'
+import { hentFeatureToggles } from '~api/api'
+
 export enum FeatureToggleStatus {
     PAA = 'PAA',
     AV = 'AV',
@@ -7,4 +10,36 @@ export enum FeatureToggleStatus {
 
 export enum FeatureToggleNavn {
     OMS_SOEKNAD_NYTT_INNTEKT_STEG = 'oms-soeknad-nytt-inntekt-steg'
+}
+
+export interface FeatureToggle {
+    name: FeatureToggleNavn
+    status: FeatureToggleStatus
+}
+
+const initialFeatureTogglesState: Array<FeatureToggle> = []
+
+const featureTogglesContext = createContext<Array<FeatureToggle>>(initialFeatureTogglesState)
+
+export const ProvideFeatureToggleContext = ({ children }: { children: ReactNode | Array<ReactNode>}) => {
+    const [featureToggles, setFeatureToggles] = useState<Array<FeatureToggle>>(initialFeatureTogglesState)
+
+    useEffect(() => {
+        hentFeatureToggles().then(setFeatureToggles)
+    }, [])
+
+    return <featureTogglesContext.Provider value={featureToggles}>{children}</featureTogglesContext.Provider>
+}
+
+export const useFeatureToggle = (featureToggleNavn: FeatureToggleNavn): FeatureToggle => {
+    const funnetFeatureToggle = useContext(featureTogglesContext).find((toggle) => toggle.name === featureToggleNavn)
+
+    if (!!funnetFeatureToggle) {
+        return funnetFeatureToggle
+    } else {
+        return {
+            name: featureToggleNavn,
+            status: FeatureToggleStatus.UDEFINERT,
+        }
+    }
 }
