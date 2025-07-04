053d28d5b8f45b38529836859061bf61a5f282f0
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-07-04 09:35:46 +0200

    Litt mindre spacing mellom header

--- apps/omstillingsstoenad-ui/src/components/soknad/6-inntekten-din/InntektenDin.tsx
+++ apps/omstillingsstoenad-ui/src/components/soknad/6-inntekten-din/InntektenDin.tsx
@@ -93,10 +93,11 @@ const InntektenDin = ({ neste, forrige }: SoknadSteg) => {
                 {omsSoeknadNyttInntektStegFeatureToggle.status === FeatureToggleStatus.PAA ? (
                     <VStack gap="12" paddingBlock="0 12">
                         <SkalGaaAvMedAlderspensjon />
+                        <VStack gap="6">
+                            <Heading size="medium">Fyll inn inntektene dine</Heading>
 
-                        <Heading size="medium">Fyll inn inntektene dine</Heading>
-
-                        <InntektFremTilDoedsallet />
+                            <InntektFremTilDoedsallet />
+                        </VStack>
 
                         <ForventetInntektIAar />
 
