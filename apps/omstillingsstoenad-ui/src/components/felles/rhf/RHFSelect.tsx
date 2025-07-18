bf4299bda5fe6625d10a5ad355a894ceda7e5247
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-07-18 10:28:21 +0200

    Styled bort fra RHF select

--- apps/omstillingsstoenad-ui/src/components/felles/rhf/RHFSelect.tsx
+++ apps/omstillingsstoenad-ui/src/components/felles/rhf/RHFSelect.tsx
@@ -1,17 +1,12 @@
+import { Box, Select, SelectProps } from '@navikt/ds-react'
+import { get } from 'lodash'
 import React from 'react'
-import { Select, SelectProps } from '@navikt/ds-react'
 import { Controller, FieldError, useFormContext } from 'react-hook-form'
 import { FieldPath, FieldValues } from 'react-hook-form/dist/types'
-import { get } from 'lodash'
 import { RegisterOptions } from 'react-hook-form/dist/types/validator'
 import { useTranslation } from 'react-i18next'
 import { v4 as uuid } from 'uuid'
 import { getTransKey } from '../../../utils/translation'
-import styled from 'styled-components'
-
-const SelectSize = styled.div<{$size?: number}>`
-  max-width: ${props => props.$size ? `${props.$size}rem`  : '22rem'};
-`
 
 interface SelectOption {
     value?: string
@@ -22,11 +17,10 @@ interface RHFProps extends Omit<SelectProps, 'name' | 'children'> {
     name: FieldPath<FieldValues>
     selectOptions: SelectOption[]
     rules?: Omit<RegisterOptions<FieldValues, FieldPath<FieldValues>>, 'required'>
-    bredde?: number
     valgfri?: boolean
 }
 
-export const RHFSelect = ({ name, label, selectOptions, rules, bredde, valgfri = false, ...rest }: RHFProps) => {
+export const RHFSelect = ({ name, label, selectOptions, rules, valgfri = false, ...rest }: RHFProps) => {
     const { t } = useTranslation()
 
     const {
@@ -38,7 +32,7 @@ export const RHFSelect = ({ name, label, selectOptions, rules, bredde, valgfri =
     const feilmelding = !!error ? t(getTransKey(error)) : undefined
 
     return (
-        <SelectSize $size={bredde} id={name}>
+        <Box maxWidth="22rem">
             <Controller
                 name={name}
                 control={control}
@@ -60,6 +54,6 @@ export const RHFSelect = ({ name, label, selectOptions, rules, bredde, valgfri =
                     </Select>
                 )}
             />
-        </SelectSize>
+        </Box>
     )
 }
