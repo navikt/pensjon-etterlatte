36473d88c3d39c98726e56810c57c55cc344858b
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-07-30 12:22:50 +0200

    Fjern fra språk velger

--- apps/barnepensjon-ui/src/components/common/LanguageSelect.tsx
+++ apps/barnepensjon-ui/src/components/common/LanguageSelect.tsx
@@ -1,17 +1,10 @@
-import { Select } from '@navikt/ds-react'
-import styled from 'styled-components'
+import { Box, Select, VStack } from '@navikt/ds-react'
 import { useApplicationContext } from '../../context/application/ApplicationContext'
 import { ActionTypes } from '../../context/application/application'
 import { useLanguageContext } from '../../context/language/LanguageContext'
 import { Language } from '../../context/language/language'
 import { LogEvents, useAnalytics } from '../../hooks/useAnalytics'
 import useTranslation from '../../hooks/useTranslation'
-
-const SelectWrapper = styled.div`
-    max-width: 200px;
-    text-align: center;
-    margin: 0 auto;
-`
 
 export default function LanguageSelect() {
     const { language, updateLanguage } = useLanguageContext()
@@ -27,12 +20,18 @@ export default function LanguageSelect() {
     }
 
     return (
-        <SelectWrapper>
-            <Select onChange={(e) => update(e.target.value as Language)} value={language} label={t('chooseLanguage')}>
-                <option value={Language.BOKMAAL}>Bokmål</option>
-                <option value={Language.NYNORSK}>Nynorsk</option>
-                <option value={Language.ENGELSK}>English</option>
-            </Select>
-        </SelectWrapper>
+        <VStack align="center">
+            <Box width="12rem">
+                <Select
+                    onChange={(e) => update(e.target.value as Language)}
+                    value={language}
+                    label={t('chooseLanguage')}
+                >
+                    <option value={Language.BOKMAAL}>Bokmål</option>
+                    <option value={Language.NYNORSK}>Nynorsk</option>
+                    <option value={Language.ENGELSK}>English</option>
+                </Select>
+            </Box>
+        </VStack>
     )
 }
