ade422d8be4ea5d3c5eb3ccb968aa9e12738f608
Emil Elton Nilsen <45180092+perkynades@users.noreply.github.com>
2025-09-01 10:44:14 +0200

    Bytt ut feature toggle for inntekt neste år med dato sjekk (#1958)
    * Bytt ut feature toggle for inntekt neste år med dato sjekk
    
    * Fjern feature toggle for meld inn endring skjema

--- apps/selvbetjening-ui/src/meldInnEndring/MeldInnEndringOutlet.tsx
+++ apps/selvbetjening-ui/src/meldInnEndring/MeldInnEndringOutlet.tsx
@@ -1,27 +1,15 @@
-import { Heading } from '@navikt/ds-react'
 import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
-import {
-    FeatureToggleNavn,
-    FeatureToggleStatus,
-    useFeatureToggle,
-} from '../common/featureToggles/FeatureTogglesContext.tsx'
 import { ProvideMeldInnEndringContext } from './components/meldInnEndringContext/MeldInnEndringContext.tsx'
 
 export const MeldInnEndringOutlet = () => {
-    const omsMeldInnEndringerSkjemaFeatureToggle = useFeatureToggle(FeatureToggleNavn.OMS_MELD_INN_ENDRING_SKJEMA)
-
     useEffect(() => {
         document.title = 'Meld inn endring'
     }, [])
 
-    return omsMeldInnEndringerSkjemaFeatureToggle.status === FeatureToggleStatus.PAA ? (
+    return (
         <ProvideMeldInnEndringContext>
             <Outlet />
         </ProvideMeldInnEndringContext>
-    ) : (
-        <Heading size="large" level="2">
-            Skjema ikke tilgjengelig
-        </Heading>
     )
 }
