56a381d3cdecfed5f04051809bfb95264b223618
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-09-01 12:11:14 +0200

    Fra withdrawing til drawing

--- apps/omstillingsstoenad-ui/src/locales/en.ts
+++ apps/omstillingsstoenad-ui/src/locales/en.ts
@@ -14,7 +14,7 @@ export default {
     'radiobuttons.ja': 'Yes',
     'radiobuttons.nei': 'No',
     'radiobuttons.vetIkke': 'Don’t know',
-    'radiobuttons.tarAlleredeUtAlderspensjon': 'Have already started withdrawing retirement pension',
+    'radiobuttons.tarAlleredeUtAlderspensjon': 'Have already started drawing retirement pension',
     'knapp.tilbake': 'Back',
     'knapp.neste': 'Next',
     'knapp.avbryt': 'Cancel',
@@ -347,11 +347,11 @@ export default {
     'inntektenDin.skalGaaAvMedAlderspensjon.naarKanJegTaUtAlderspensjon.tittel':
         'When can I start to receive my retirement pension?',
     'inntektenDin.skalGaaAvMedAlderspensjon.naarKanJegTaUtAlderspensjon.innhold':
-        'You can start to receive retirement pension in the month after you turn 67.\n\n If you have high enough earnings, you can start withdrawing from the age of 62.\n\n It is important for us to know if you are planning to retire on a retirement pension so we can ensure correct payments. If you have not yet decided, remember to notify us if you wish to retire using your retirement pension.',
+        'You can start to receive retirement pension in the month after you turn 67.\n\n If you have high enough earnings, you can start drawing from the age of 62.\n\n It is important for us to know if you are planning to retire on a retirement pension so we can ensure correct payments. If you have not yet decided, remember to notify us if you wish to retire using your retirement pension.',
     'inntektenDin.skalGaaAvMedAlderspensjon.datoForAaGaaAvMedAlderspensjon':
         'When do you plan to withdraw your retirement pension?',
     'inntektenDin.skalGaaAvMedAlderspensjon.datoForAaGaaAvMedAlderspensjon.tarAlleredeUtAlderspensjon':
-        'When did you start withdrawing your retirement pension?',
+        'When did you start drawing your retirement pension?',
     'feil.skalGaaAvMedAlderspensjon.datoForAaGaaAvMedAlderspensjon.required':
         'You must fill in the date of retirement pension (mm.yy)',
     'inntektenDin.alderspensjonOgOmstillingsstoenad.tittel': 'Retirement pension and adjustment allowance',
