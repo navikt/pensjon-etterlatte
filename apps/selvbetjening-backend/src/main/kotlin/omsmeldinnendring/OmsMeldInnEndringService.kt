60e681b57d8a8a55400e7cf9b8b322e04477438c
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-10-15 09:59:01 +0200

    Logg feil

--- apps/selvbetjening-backend/src/main/kotlin/omsmeldinnendring/OmsMeldInnEndringService.kt
+++ apps/selvbetjening-backend/src/main/kotlin/omsmeldinnendring/OmsMeldInnEndringService.kt
@@ -3,23 +3,31 @@ package no.nav.etterlatte.omsendringer
 import no.nav.etterlatte.libs.common.omsmeldinnendring.OmsMeldtInnEndring
 import no.nav.etterlatte.libs.common.omsmeldinnendring.OmsMeldtInnEndringStatus
 import no.nav.etterlatte.libs.common.person.Foedselsnummer
+import org.slf4j.LoggerFactory
 import java.util.UUID
+import kotlin.jvm.java
 
 class OmsMeldInnEndringService(
     private val repository: OmsMeldInnEndringRepository,
 ) {
+    private val logger = LoggerFactory.getLogger(OmsMeldInnEndringService::class.java)
+
     fun lagreEndringer(
         fnr: Foedselsnummer,
         request: OmsMeldtInnEndringRequest,
     ) {
-        repository.lagreEndringer(
-            OmsMeldtInnEndring(
-                fnr = fnr,
-                endring = request.endring,
-                beskrivelse = request.beskrivelse,
-                forventetInntektTilNesteAar = request.forventetInntektTilNesteAar,
-            ),
-        )
+        try {
+            repository.lagreEndringer(
+                OmsMeldtInnEndring(
+                    fnr = fnr,
+                    endring = request.endring,
+                    beskrivelse = request.beskrivelse,
+                    forventetInntektTilNesteAar = request.forventetInntektTilNesteAar,
+                ),
+            )
+        } catch (e: Error) {
+            logger.error("Kunne ikke lagre meldt inn endring", e)
+        }
     }
 
     fun hentEndringerMedStatus(lagret: OmsMeldtInnEndringStatus) = repository.hentEndringerMedStatus(lagret)
