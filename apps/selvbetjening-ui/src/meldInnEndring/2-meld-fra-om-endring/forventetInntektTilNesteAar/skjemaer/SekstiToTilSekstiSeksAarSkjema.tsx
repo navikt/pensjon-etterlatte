c6bddb323c14e416e8cb457b33a08bad4997b8c0
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-10-31 14:17:28 +0100

    Lagt til alt for å få inn sanity

--- apps/selvbetjening-ui/src/meldInnEndring/2-meld-fra-om-endring/forventetInntektTilNesteAar/skjemaer/SekstiToTilSekstiSeksAarSkjema.tsx
+++ apps/selvbetjening-ui/src/meldInnEndring/2-meld-fra-om-endring/forventetInntektTilNesteAar/skjemaer/SekstiToTilSekstiSeksAarSkjema.tsx
@@ -1,20 +1,39 @@
 import { Box, Radio, ReadMore, TextField, VStack } from '@navikt/ds-react'
 import { useFormContext } from 'react-hook-form'
+import { KomponentLaster } from '../../../../common/KomponentLaster.tsx'
 import { ControlledMaanedVelger } from '../../../../common/maanedVelger/ControlledMaanedVelger.tsx'
 import { ControlledRadioGruppe } from '../../../../common/radioGruppe/ControlledRadioGruppe.tsx'
+import { SanityRikTekst } from '../../../../common/sanity/SanityRikTekst.tsx'
+import { useSanityInnhold } from '../../../../common/sanity/useSanityInnhold.ts'
+import { useSpraak } from '../../../../common/spraak/SpraakContext.tsx'
 import { ControlledInntektTextField } from '../../../../inntektsjustering/components/controlledInntektTextField/ControlledInntektTextField.tsx'
 import { Alder, MeldtInnEndring, SkalGaaAvMedAlderspensjon } from '../../../../types/meldInnEndring.ts'
 import { IInnloggetBruker } from '../../../../types/person.ts'
+import { MeldInnEndringMeldFra } from '../../../sanity.types.ts'
 import { SumAvOppgittInntekt } from './SumAvOppgittInntekt.tsx'
 import { forventetInntektTilNesteAarSkjemaValuesTilValues } from './utils.ts'
 
 export const SekstiToTilSekstiSeksAarSkjema = ({ innloggetBruker }: { innloggetBruker: IInnloggetBruker }) => {
+    const spraak = useSpraak()
+
     const {
         control,
         register,
         watch,
         formState: { errors },
     } = useFormContext<MeldtInnEndring>()
+
+    const { innhold, error, isLoading } = useSanityInnhold<MeldInnEndringMeldFra>('*[_type == "meldInnEndringMeldFra"]')
+
+    if (isLoading) {
+        return <KomponentLaster />
+    }
+    if (error) {
+        throw error
+    }
+    if (!innhold?.forventetInntektTilNesteAarSkjemer?.sekstiToTilSekstiSeksAarSkjema) {
+        throw Error('Finner ikke sanity innhold for skjema 62-66 år')
+    }
 
     const skalViseAfpPrivat = (innloggetBruker: IInnloggetBruker): boolean => {
         return innloggetBruker.foedselsaar! < 1963
@@ -24,120 +43,181 @@ export const SekstiToTilSekstiSeksAarSkjema = ({ innloggetBruker }: { innloggetB
     const foersteDagNesteAar = new Date(nesteAar, 0, 1)
     const sisteDagNesteAar = new Date(nesteAar, 11, 31)
 
+    const {
+        skalGaaAvMedAlderspensjon,
+        datoForAaGaaAvMedAlderspensjon,
+        omsTarSlutt,
+        inntekterSomSkalMeldesInn,
+        arbeidsinntekt,
+        naeringsinntekt,
+        afpInntekt,
+        afpTjenestepensjonordning,
+        inntektFraUtland,
+        sumAvInntekt,
+    } = innhold.forventetInntektTilNesteAarSkjemer.sekstiToTilSekstiSeksAarSkjema
+
     return (
-        <>
-            <VStack gap="2">
-                <ControlledRadioGruppe
-                    name={'forventetInntektTilNesteAar.skalGaaAvMedAlderspensjon'}
-                    control={control}
-                    legend={'Skal du gå av med alderspensjon?'}
-                    description={'Beskrivelse jauda'}
-                    errorVedTomInput="hei her var det mye feil"
-                    radios={
-                        <>
-                            <Radio value={SkalGaaAvMedAlderspensjon.JA}>Ja</Radio>
-                            <Radio value={SkalGaaAvMedAlderspensjon.NEI}>Nei</Radio>
-                            <Radio value={SkalGaaAvMedAlderspensjon.VET_IKKE}>Vet ikke</Radio>
-                        </>
-                    }
-                />
-                <ReadMore header={'Skal du gå av'}>Masse info om hvis man skal gå av</ReadMore>
-            </VStack>
-            {watch('forventetInntektTilNesteAar.skalGaaAvMedAlderspensjon') !== undefined && (
-                <>
-                    {watch('forventetInntektTilNesteAar.skalGaaAvMedAlderspensjon') ===
-                        SkalGaaAvMedAlderspensjon.JA && (
-                        <>
-                            <VStack gap="2">
-                                <ControlledMaanedVelger
-                                    name={'forventetInntektTilNesteAar.datoForAaGaaAvMedAlderspensjon'}
-                                    control={control}
-                                    label={'Når skal du gå av med alterspensjon'}
-                                    description="God beskrivelse er hja"
-                                    errorVedTomInput="enda mer som er feil"
-                                    fromDate={foersteDagNesteAar}
-                                    toDate={sisteDagNesteAar}
-                                />
-                                <ReadMore header={'Mer info om å gå av med alderspensjon'}>Enda mer info</ReadMore>
-                            </VStack>
-                            <div>Oms vil ta slutt om litt</div>
-                        </>
-                    )}
-
-                    {watch('forventetInntektTilNesteAar.skalGaaAvMedAlderspensjon') === SkalGaaAvMedAlderspensjon.JA ? (
-                        <div>Tekst om Ja til skal gå av med alderspensjon</div>
-                    ) : (
-                        <div>Tekst om Nei/Vet ikke til skal gå av med alderspensjon</div>
-                    )}
-
-                    <VStack gap="2">
-                        <ControlledInntektTextField
-                            name={'forventetInntektTilNesteAar.arbeidsinntekt'}
-                            control={control}
-                            label={'Arbeidsinntekt'}
-                            description={'Bekrivelse'}
-                        />
-                        <ReadMore header={'Mer om arbeidsinntekt'}>Masse mer om arbeidsinntekt</ReadMore>
-                    </VStack>
-
-                    <VStack gap="2">
-                        <ControlledInntektTextField
-                            name={'forventetInntektTilNesteAar.naeringsinntekt'}
-                            control={control}
-                            label={'Næringsinntekt'}
-                            description="Bekrivelse"
-                        />
-                        <ReadMore header={'Næringsinntekt'}>Næsdgpoj</ReadMore>
-                    </VStack>
-
-                    <ControlledInntektTextField
-                        name={'forventetInntektTilNesteAar.afpInntekt'}
+        !!innhold && (
+            <>
+                <VStack gap="2">
+                    <ControlledRadioGruppe
+                        name={'forventetInntektTilNesteAar.skalGaaAvMedAlderspensjon'}
                         control={control}
-                        label={skalViseAfpPrivat(innloggetBruker) ? 'Afp privat' : 'Afp privat eller offentlig'}
-                        description={'spiogj'}
+                        legend={skalGaaAvMedAlderspensjon?.legend?.[spraak]}
+                        description={skalGaaAvMedAlderspensjon?.description?.[spraak]}
+                        errorVedTomInput={skalGaaAvMedAlderspensjon?.errorVedTomInput?.[spraak]}
+                        radios={
+                            <>
+                                <Radio value={SkalGaaAvMedAlderspensjon.JA}>
+                                    {skalGaaAvMedAlderspensjon?.radios?.ja?.[spraak]}
+                                </Radio>
+                                <Radio value={SkalGaaAvMedAlderspensjon.NEI}>
+                                    {skalGaaAvMedAlderspensjon?.radios?.nei?.[spraak]}
+                                </Radio>
+                                <Radio value={SkalGaaAvMedAlderspensjon.VET_IKKE}>
+                                    {skalGaaAvMedAlderspensjon?.radios?.vetIkke?.[spraak]}
+                                </Radio>
+                            </>
+                        }
                     />
-                    {!!watch('forventetInntektTilNesteAar.afpInntekt') &&
-                        watch('forventetInntektTilNesteAar.afpInntekt') !== '0' && (
-                            <Box maxWidth="25rem">
-                                <TextField
-                                    {...register('forventetInntektTilNesteAar.afpTjenesteordning', {
-                                        required: {
-                                            value: true,
-                                            message: 'Error ved tom input',
-                                        },
-                                    })}
-                                    label={'AFP tjenestepensjonsordning'}
-                                    description={'Bsoigh'}
-                                    error={errors.forventetInntektTilNesteAar?.afpTjenesteordning?.message}
-                                />
-                            </Box>
-                        )}
-
-                    <VStack gap="2">
-                        <ControlledInntektTextField
-                            name={'forventetInntektTilNesteAar.inntektFraUtland'}
-                            control={control}
-                            label="Inntekt fra utland"
-                            description={'soigh'}
-                        />
-                        <ReadMore header={'Informasjon om inntekt fra utland'}>sgipjsdgp</ReadMore>
-                    </VStack>
-
-                    <SumAvOppgittInntekt
-                        forventetInntektTilNesteAar={forventetInntektTilNesteAarSkjemaValuesTilValues(
-                            watch('forventetInntektTilNesteAar')!
-                        )}
-                        alder={Alder.SEKSTI_TO_TIL_SEKSTI_SEKS}
-                    >
+                    {!!skalGaaAvMedAlderspensjon?.readMore && (
+                        <ReadMore header={skalGaaAvMedAlderspensjon.readMore?.tittel?.[spraak]}>
+                            <SanityRikTekst text={skalGaaAvMedAlderspensjon.readMore?.innhold?.[spraak]} />
+                        </ReadMore>
+                    )}
+                </VStack>
+                {watch('forventetInntektTilNesteAar.skalGaaAvMedAlderspensjon') !== undefined && (
+                    <>
                         {watch('forventetInntektTilNesteAar.skalGaaAvMedAlderspensjon') ===
-                        SkalGaaAvMedAlderspensjon.JA ? (
-                            <div>Ja til skal gå av med alderspensjon</div>
-                        ) : (
-                            <div>Nei/Vet ikke til skal gå av med alderspensjon</div>
+                            SkalGaaAvMedAlderspensjon.JA && (
+                            <>
+                                <VStack gap="2">
+                                    <ControlledMaanedVelger
+                                        name="forventetInntektTilNesteAar.datoForAaGaaAvMedAlderspensjon"
+                                        control={control}
+                                        label={datoForAaGaaAvMedAlderspensjon?.label?.[spraak]}
+                                        description={datoForAaGaaAvMedAlderspensjon?.description?.[spraak]}
+                                        errorVedTomInput={datoForAaGaaAvMedAlderspensjon?.errorVedTomInput?.[spraak]}
+                                        fromDate={foersteDagNesteAar}
+                                        toDate={sisteDagNesteAar}
+                                    />
+                                    {!!datoForAaGaaAvMedAlderspensjon?.readMore && (
+                                        <ReadMore header={datoForAaGaaAvMedAlderspensjon.readMore.tittel?.[spraak]}>
+                                            <SanityRikTekst
+                                                text={datoForAaGaaAvMedAlderspensjon.readMore.innhold?.[spraak]}
+                                            />
+                                        </ReadMore>
+                                    )}
+                                </VStack>
+                                <div>
+                                    <SanityRikTekst text={omsTarSlutt?.[spraak]} />
+                                </div>
+                            </>
                         )}
-                    </SumAvOppgittInntekt>
-                </>
-            )}
-        </>
+
+                        <VStack gap="2">
+                            {watch('forventetInntektTilNesteAar.skalGaaAvMedAlderspensjon') ===
+                            SkalGaaAvMedAlderspensjon.JA ? (
+                                <SanityRikTekst
+                                    text={inntekterSomSkalMeldesInn?.skalGaaAvMedAlderspensjon?.ja?.[spraak]}
+                                />
+                            ) : (
+                                <SanityRikTekst
+                                    text={inntekterSomSkalMeldesInn?.skalGaaAvMedAlderspensjon?.neiVetIkke?.[spraak]}
+                                />
+                            )}
+                        </VStack>
+
+                        <VStack gap="2">
+                            <ControlledInntektTextField
+                                name="forventetInntektTilNesteAar.arbeidsinntekt"
+                                control={control}
+                                label={arbeidsinntekt?.label?.[spraak]}
+                                description={arbeidsinntekt?.description?.[spraak]}
+                            />
+
+                            {!!arbeidsinntekt?.readMore && (
+                                <ReadMore header={arbeidsinntekt?.readMore?.tittel?.[spraak]}>
+                                    <SanityRikTekst text={arbeidsinntekt?.readMore?.innhold?.[spraak]} />
+                                </ReadMore>
+                            )}
+                        </VStack>
+
+                        <VStack gap="2">
+                            <ControlledInntektTextField
+                                name="forventetInntektTilNesteAar.naeringsinntekt"
+                                control={control}
+                                label={naeringsinntekt?.label?.[spraak]}
+                                description={naeringsinntekt?.description?.[spraak]}
+                            />
+                            {!!naeringsinntekt?.readMore && (
+                                <ReadMore header={naeringsinntekt?.readMore?.tittel?.[spraak]}>
+                                    <SanityRikTekst text={naeringsinntekt?.readMore?.innhold?.[spraak]} />
+                                </ReadMore>
+                            )}
+                        </VStack>
+
+                        <ControlledInntektTextField
+                            name="forventetInntektTilNesteAar.afpInntekt"
+                            control={control}
+                            label={
+                                skalViseAfpPrivat(innloggetBruker)
+                                    ? afpInntekt?.label?.afpPrivat?.[spraak]
+                                    : afpInntekt?.label?.afpOffentligEllerPrivat?.[spraak]
+                            }
+                            description={afpInntekt?.description?.[spraak]}
+                        />
+
+                        {!!watch('forventetInntektTilNesteAar.afpInntekt') &&
+                            watch('forventetInntektTilNesteAar.afpInntekt') !== '0' && (
+                                <Box maxWidth="25rem">
+                                    <TextField
+                                        {...register('forventetInntektTilNesteAar.afpTjenesteordning', {
+                                            required: {
+                                                value: true,
+                                                message:
+                                                    afpTjenestepensjonordning?.errorVedTomInput?.[spraak] ??
+                                                    'Må settes',
+                                            },
+                                        })}
+                                        label={afpTjenestepensjonordning?.label?.[spraak]}
+                                        description={afpTjenestepensjonordning?.description?.[spraak]}
+                                        error={errors.forventetInntektTilNesteAar?.afpTjenesteordning?.message}
+                                    />
+                                </Box>
+                            )}
+
+                        <VStack gap="2">
+                            <ControlledInntektTextField
+                                name="forventetInntektTilNesteAar.inntektFraUtland"
+                                control={control}
+                                label={inntektFraUtland?.label?.[spraak]}
+                                description={inntektFraUtland?.description?.[spraak]}
+                            />
+
+                            {!!inntektFraUtland?.readMore && (
+                                <ReadMore header={inntektFraUtland?.readMore?.tittel?.[spraak]}>
+                                    <SanityRikTekst text={inntektFraUtland?.readMore?.innhold?.[spraak]} />
+                                </ReadMore>
+                            )}
+                        </VStack>
+
+                        <SumAvOppgittInntekt
+                            forventetInntektTilNesteAar={forventetInntektTilNesteAarSkjemaValuesTilValues(
+                                watch('forventetInntektTilNesteAar')!
+                            )}
+                            alder={Alder.SEKSTI_TO_TIL_SEKSTI_SEKS}
+                        >
+                            {watch('forventetInntektTilNesteAar.skalGaaAvMedAlderspensjon') ===
+                            SkalGaaAvMedAlderspensjon.JA ? (
+                                <SanityRikTekst text={sumAvInntekt?.skalGaaAvMedAlderspensjon?.ja?.[spraak]} />
+                            ) : (
+                                <SanityRikTekst text={sumAvInntekt?.skalGaaAvMedAlderspensjon?.neiVetIkke?.[spraak]} />
+                            )}
+                        </SumAvOppgittInntekt>
+                    </>
+                )}
+            </>
+        )
     )
 }
