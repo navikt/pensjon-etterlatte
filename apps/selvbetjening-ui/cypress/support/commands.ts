63dfbb2711c681252b1e6dce67a8ccf3e3cf3af9
Hanne Olsen <hanne.olsen@nav.no>
2026-04-28 15:40:55 +0200

    Legger på fixtures (hardkodede tekster) slik at vi ikke trenger at Sanity er oppe for at testene kjører

--- apps/selvbetjening-ui/cypress/support/commands.ts
+++ apps/selvbetjening-ui/cypress/support/commands.ts
@@ -13,54 +13,55 @@ Cypress.Commands.add('lastInnledning', () => {
         body: { harOMSSak: true },
     }).as('harSakIGjenny')
 
-    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22meldInnEndringInnledning%22%5D`).as(
-        'fellesKomponenterInnhold'
-    )
+    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22meldInnEndringInnledning%22%5D`, {
+        fixture: 'innledning',
+    }).as('innledning')
 
     cy.intercept(
         'GET',
-        `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22skjemaHeader%22+%26%26+dokumentTittel+%3D%3D+%22meld-inn-endring%22%5D`
+        `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22skjemaHeader%22+%26%26+dokumentTittel+%3D%3D+%22meld-inn-endring%22%5D`,
+        {
+            fixture: 'skjemaHeader',
+        }
     ).as('skjemaHeader')
 
-    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22behandlingAvInformasjonAccordion%22%5D`).as(
-        'behandlingAvInformasjonAccordion'
-    )
+    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22behandlingAvInformasjonAccordion%22%5D`, {
+        fixture: 'behandlingAvInformasjon',
+    }).as('behandlingAvInformasjon')
 
-    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22spraakVelger%22%5D`).as('spraakVelger')
+    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22spraakVelger%22%5D`, {
+        fixture: 'spraakVelger',
+    }).as('spraakVelger')
 
     cy.visit('http://localhost:5173/omstillingsstonad/skjema/meld-inn-endring/innledning')
 
-    cy.wait([
-        '@fellesKomponenterInnhold',
-        '@harSakIGjenny',
-        '@skjemaHeader',
-        '@behandlingAvInformasjonAccordion',
-        '@spraakVelger',
-    ])
+    cy.wait(['@innledning', '@harSakIGjenny', '@skjemaHeader', '@behandlingAvInformasjon', '@spraakVelger'])
 })
 
 Cypress.Commands.add('lastMeldFraOmEndring', () => {
     cy.lastInnledning()
 
-    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22meldInnEndringMeldFra%22%5D`).as(
-        'meldInnEndringMeldFra'
-    )
+    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22meldInnEndringMeldFra%22%5D`, {
+        fixture: 'meldFra',
+    }).as('meldFra')
 
-    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22sammendragAvSkjemaFeil%22%5D`).as(
-        'sammendragAvSkjemafeil'
-    )
-    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22navigasjonMeny%22%5D`).as('navigasjonsMeny')
+    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22sammendragAvSkjemaFeil%22%5D`, {
+        fixture: 'skjemaFeil',
+    }).as('sammendragAvSkjemafeil')
+    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22navigasjonMeny%22%5D`, {
+        fixture: 'navigasjonsMeny',
+    }).as('navigasjonsMeny')
 
     cy.visit('http://localhost:5173/omstillingsstonad/skjema/meld-inn-endring/meld-fra-om-endring')
 
-    cy.wait(['@meldInnEndringMeldFra', '@sammendragAvSkjemafeil', '@navigasjonsMeny'])
+    cy.wait(['@meldFra', '@sammendragAvSkjemafeil', '@navigasjonsMeny'])
 })
 
 Cypress.Commands.add('lastOppsummering', () => {
     cy.intercept('POST', `${apiUrl}/api/oms/meld_inn_endringer`, { statusCode: 200 })
-    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22meldInnEndringOppsummering%22%5D`).as(
-        'meldInnEndringOppsummering'
-    )
+    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22meldInnEndringOppsummering%22%5D`, {
+        fixture: 'oppsummering',
+    }).as('oppsummering')
 
     cy.lastInnledning()
     cy.lastMeldFraOmEndring()
@@ -69,18 +70,18 @@ Cypress.Commands.add('lastOppsummering', () => {
 
     cy.findByRole('button', { name: 'Neste steg' }).click()
 
-    cy.wait(['@meldInnEndringOppsummering'])
+    cy.wait(['@oppsummering'])
 })
 
 Cypress.Commands.add('lastKvittering', () => {
-    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22meldInnEndringKvittering%22%5D`).as(
-        'meldInnEndringKvittering'
-    )
+    cy.intercept('GET', `${apiUrl}/sanity?sanityQuery=*%5B_type+%3D%3D+%22meldInnEndringKvittering%22%5D`, {
+        fixture: 'kvittering',
+    }).as('kvittering')
 
     cy.lastOppsummering()
     cy.findByRole('button', { name: 'Send til Nav' }).click()
 
     cy.url().should('include', 'meld-inn-endring/kvittering')
 
-    cy.wait(['@meldInnEndringKvittering'])
+    cy.wait(['@kvittering'])
 })
