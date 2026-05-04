3dcd9aa7945861eae3d1def5ba93acfb25ff688d
Hanne Olsen <hanne.olsen@nav.no>
2026-05-04 15:04:04 +0200

    Oppdaterer vite, fjerner pluginen vite-tsconfig-paths siden det nå gjøres default

--- apps/selvbetjening-ui/vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import { defineConfig } from 'vitest/config'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-})
