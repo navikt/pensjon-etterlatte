e58e980fa3ecbcbcb2e1d21464a78a7382bcd21c
Miina Lervik <miina.elisabeth.lervik@nav.no>
2026-08-25 14:20:00 +0200

    Lagt på midlertidig logging HUSK Å FJERN (pluss oppdatert ktlint versjon til 1.8.0)

--- apps/innsendt-soeknad/src/main/kotlin/JsonUtils.kt
+++ apps/innsendt-soeknad/src/main/kotlin/JsonUtils.kt
@@ -1,10 +1,15 @@
 package no.nav.etterlatte
 
+import org.slf4j.LoggerFactory
 import tools.jackson.databind.DeserializationFeature
 import tools.jackson.databind.cfg.DateTimeFeature
 import tools.jackson.databind.cfg.EnumFeature
 import tools.jackson.module.kotlin.jacksonMapperBuilder
 import tools.jackson.module.kotlin.readValue
+
+// TODO: Fjern etter Jackson 3-migrering er verifisert
+private val jacksonMigLog = LoggerFactory.getLogger("JacksonMigrering")
+private val jacksonMigSikkerLogg = LoggerFactory.getLogger("sikkerLogg")
 
 val mapper =
     jacksonMapperBuilder()
@@ -12,7 +17,38 @@ val mapper =
         .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
         .disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS)
         .build()
+        .also { m ->
+            // TODO: Fjern etter Jackson 3-migrering er verifisert
+            jacksonMigLog.info(
+                "[Jackson3-verifisering] innsendt-soeknad mapper initialisert:" +
+                    " versjon=${m.version()}," +
+                    " moduler=${m.registeredModuleIds}," +
+                    " FAIL_ON_UNKNOWN_PROPERTIES=${m.deserializationConfig.isEnabled(
+                        DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+                    )}," +
+                    " READ_UNKNOWN_ENUM_VALUES_AS_NULL=${m.deserializationConfig.isEnabled(
+                        EnumFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL,
+                    )}," +
+                    " WRITE_DATES_AS_TIMESTAMPS=${m.serializationConfig.isEnabled(
+                        DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS,
+                    )}",
+            )
+        }
 
-fun Any.toJson(): String = mapper.writeValueAsString(this)
+fun Any.toJson(): String =
+    mapper.writeValueAsString(this).also { json ->
+        // TODO: Fjern etter Jackson 3-migrering er verifisert
+        jacksonMigLog.debug(
+            "[Jackson3-verifisering] toJson() OK: type=${this::class.simpleName}, lengde=${json.length}",
+        )
+        jacksonMigSikkerLogg.debug("[Jackson3-verifisering] toJson() innhold (type=${this::class.simpleName}):\n$json")
+    }
 
-inline fun <reified T> deserialize(value: String): T = mapper.readValue(value)
+inline fun <reified T> deserialize(value: String): T =
+    mapper.readValue<T>(value).also {
+        // TODO: Fjern etter Jackson 3-migrering er verifisert
+        jacksonMigLog.debug("[Jackson3-verifisering] deserialize() OK: måltype=${T::class.simpleName}")
+        jacksonMigSikkerLogg.debug(
+            "[Jackson3-verifisering] deserialize() input (måltype=${T::class.simpleName}):\n$value",
+        )
+    }
--- apps/innsendt-soeknad/src/main/kotlin/common/JsonUtils.kt
+++ apps/innsendt-soeknad/src/main/kotlin/common/JsonUtils.kt
@@ -1,10 +1,15 @@
 package no.nav.etterlatte.common
 
+import org.slf4j.LoggerFactory
 import tools.jackson.databind.DeserializationFeature
-import tools.jackson.databind.cfg.EnumFeature
 import tools.jackson.databind.cfg.DateTimeFeature
+import tools.jackson.databind.cfg.EnumFeature
 import tools.jackson.module.kotlin.jacksonMapperBuilder
 import tools.jackson.module.kotlin.readValue
+
+// TODO: Fjern etter Jackson 3-migrering er verifisert
+private val jacksonMigLog = LoggerFactory.getLogger("JacksonMigrering")
+private val jacksonMigSikkerLogg = LoggerFactory.getLogger("sikkerLogg")
 
 val objectMapper =
     jacksonMapperBuilder()
@@ -12,10 +17,43 @@ val objectMapper =
         .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
         .disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS)
         .build()
+        .also { m ->
+            // TODO: Fjern etter Jackson 3-migrering er verifisert
+            jacksonMigLog.info(
+                "[Jackson3-verifisering] innsendt-soeknad common objectMapper initialisert:" +
+                    " versjon=${m.version()}," +
+                    " moduler=${m.registeredModuleIds}," +
+                    " FAIL_ON_UNKNOWN_PROPERTIES=${m.deserializationConfig.isEnabled(
+                        DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+                    )}," +
+                    " READ_UNKNOWN_ENUM_VALUES_AS_NULL=${m.deserializationConfig.isEnabled(
+                        EnumFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL,
+                    )}," +
+                    " WRITE_DATES_AS_TIMESTAMPS=${m.serializationConfig.isEnabled(
+                        DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS,
+                    )}",
+            )
+        }
 
-fun Any.toJson(): String = objectMapper.writeValueAsString(this)
+fun Any.toJson(): String =
+    objectMapper.writeValueAsString(this).also { json ->
+        // TODO: Fjern etter Jackson 3-migrering er verifisert
+        jacksonMigLog.debug(
+            "[Jackson3-verifisering] common.toJson() OK: type=${this::class.simpleName}, lengde=${json.length}",
+        )
+        jacksonMigSikkerLogg.debug(
+            "[Jackson3-verifisering] common.toJson() innhold (type=${this::class.simpleName}):\n$json",
+        )
+    }
 
 inline fun <reified T : Any> mapJsonToAny(
     json: String,
     failonunknown: Boolean = false,
-): T = objectMapper.readValue(json)
+): T =
+    objectMapper.readValue<T>(json).also {
+        // TODO: Fjern etter Jackson 3-migrering er verifisert
+        jacksonMigLog.debug("[Jackson3-verifisering] mapJsonToAny() OK: måltype=${T::class.simpleName}")
+        jacksonMigSikkerLogg.debug(
+            "[Jackson3-verifisering] mapJsonToAny() input (måltype=${T::class.simpleName}):\n$json",
+        )
+    }
