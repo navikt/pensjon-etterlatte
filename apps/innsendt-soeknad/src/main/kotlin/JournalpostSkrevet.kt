5d5c4f034876b6bc06d98dd5ed3a5c7214503e74
olekvernberg <ole.christian.kvernberg@nav.no>
2026-08-25 13:16:28 +0200

    Oppgraderer rapidsandrivers og etterlatte-common, for å ta i bruk versjon 3 av jackson i avhengigheter

--- apps/innsendt-soeknad/src/main/kotlin/JournalpostSkrevet.kt
+++ apps/innsendt-soeknad/src/main/kotlin/JournalpostSkrevet.kt
@@ -1,6 +1,6 @@
 package no.nav.etterlatte
 
-import com.fasterxml.jackson.databind.JsonNode
+import tools.jackson.databind.JsonNode
 import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
 import com.github.navikt.tbd_libs.rapids_and_rivers.River
 import com.github.navikt.tbd_libs.rapids_and_rivers.isMissingOrNull
